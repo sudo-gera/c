eaf276c6bf6e7401d5341f1dd056d5ea67c89b5a
sudo-gera <gera.tatarinov.999@gmail.com>
2020-09-20 13:53:09 +0300

    hw

--- api.py
+++ api.py
@@ -11,20 +11,40 @@ from traceback import format_exc as fo
 from os import popen
 from webbrowser import open as webopen
 from sys import argv
+from pathlib import Path
 
-url=open('url').read()
+url=open(str(Path.home())+'/url').read()
 token=url.split('#')[1].split('&')[0].split('=')[1]
 
-def api(path,data=''):
- if path and path[-1] not in '&?':
-  if '?' in path:
-   path+='&'
-  else:
-   path+='?'
- sleep(1/6)
- data=data.encode()
- global token
- ret= loads(urlopen('https://api.vk.com/method/'+path+'v=5.101&access_token='+token,data=data).read().decode())
- return ret
+def items(q):
+	if type(q) == type(dict()):
+		if set(q.keys()) == set(['count', 'items']):
+			return items(q['items'])
+		else:
+			for w in q:
+				q[w] = items(q[w])
+			return q
+	elif type(q) == type(list()):
+		return [items(w) for w in q]
+	else:
+		return q
 
+def api(path,data=''):
+	if path and path[-1] not in '&?':
+		if '?' in path:
+			path+='&'
+		else:
+			path+='?'
+	sleep(1/6)
+	data=data.encode()
+	global token
+	ret= loads(urlopen('https://api.vk.com/method/'+path+'v=5.101&access_token='+token,data=data).read().decode())
+	try:
+		return items(ret['response'])
+	except:
+		pass
+	try:
+		print(ret['error']['error_msg'])
+	except:
+		print(ret)
 
