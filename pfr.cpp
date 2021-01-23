cb0ee79a01cc172ee928488f63caa1eba429ca3d
sudo-gera <gera.tatarinov.999@gmail.com>
2021-01-23 23:22:51 +0300

    receiveing...

--- pfr.cpp
+++ pfr.cpp
@@ -1,75 +1,56 @@
 #include <bits/stdc++.h>
-using namespace std;
-#define vect(type,n) (type*)calloc(sizeof(type),n)
-auto knt(int n,int m,int* a,int* z){
-	int n,m,w,h,j;
-	auto s=vect(int*,m+1);
-	auto d=vect(int*,m+1);
-	for (j=0;j<m+1;j++){
-		s[j]=vect(int,n+1);
-		d[j]=vect(int,n+1);
+#define int int64_t
+int scan(){int q;scanf("%li",&(q));return q;}
+#define printn(q) printf("%li\n",(q));
+#define print(q) printf("%li",(q));
+#define prints(q) printf("%li ",(q));
+#define ffff(w,a,o,e) for(int w=a;w<o;w+=e)
+#define fffb(w,a,o,e) for(int w=a;w>o;w+=e)
+#define fff(w,a,o) for(int w=a;w<o;w++)
+#define ff(w,o) for(int w=0;w<o;w++)
+#define vect(t,n) auto n=(t*)malloc(0);int n##_size=0;
+#define push(n,v) n=(decltype(n))realloc(n,(n##_size+1)*sizeof(decltype(*n)));n[n##_size++]=v;
+
+#define 
+
+decltype(0) main(){
+	int n=scan(),m=scan();
+	vect(int,a);
+	vect(int,b);
+	vect(int,x);
+	vect(int,c);
+	ff(w,n){
+		push(a,scan());
+		push(b,scan());
 	}
-	for (h=0;h<n+1;h++){
-		for (j=0;j<m+1;j++){
-			if (h==0){
-				s[j][h]=0;
-				d[j][h]=0;
-			}else{
-				s[j][h]=s[j][h-1];
-				d[j][h]=d[j][h-1];
-				if (j>=a[h-1]){
-					if (s[j][h]<s[j-a[h-1]][h-1]+z[h-1]){
-						d[j][h]=d[j-a[h-1]][h-1]+1;
-						s[j][h]=s[j-a[h-1]][h-1]+z[h-1];
-					}else if (s[j][h]==s[j-a[h-1]][h-1]+z[h-1]){
-						if (d[j][h]>d[j-a[h-1]][h-1]+z[h-1]){
-							d[j][h]=d[j-a[h-1]][h-1]+1;
-						}
-					}
-				}
-			}
+	ff(w,m){
+		push(x,scan());
+		push(c,0);
+	}
+	vect(int,e);
+	ff(w,a_size){
+		push(e,(a[w]<<32)+(1<<30));
+	}
+	ff(w,b_size){
+		push(e,(b[w]<<32)+(3<<30));
+	}
+	ff(w,x_size){
+		push(e,(x[w]<<32)+w+(2<<30));
+	}
+	std::sort(e,e+e_size);
+	int o=0;
+	ff(w,e_size){
+		if ((e[w]&((1l<<32)-(1l<<30)))==1){
+			o++;
+		}
+		if ((e[w]&((1l<<32)-(1l<<30)))==3){
+			o--;
+		}
+		if ((e[w]&((1l<<32)-(1l<<30)))==2){
+			c[(e[w]&((1l<<30)-(1l<<0)))]=o;
 		}
 	}
-	return s;	
-}
-int main(){
-	int s;
-	cin>>s;
-	int n;
-	cin>>n;
-	auto p=vect(int,n);
-	int w,t;
-	for (w=0;w<n;w++){
-		cin>>p[w];
+	ff(w,c_size){
+		printn(c[w]);
 	}
-	int m;
-	cin>>m;
-	auto q=vect(int,m);
-	for (w=0;w<m;w++){
-		cin>>q[w];
-	}
-	set <int> l;
-	set <int> a;
-	l.insert(0);
-	for (t=0;t<n;t++){
-		for (w : l){
-			l.insert(w+p[t]);
-		}
-	}
-	a.insert(s);
-	for (t=0;t<m;t++){
-		for (w : a){
-			a.insert(w+q[t]);
-		}
-	}
-	vector<int> i;
-	set_intersection(l.begin(),l.end(),a.begin(),a.end(),back_inserter(i));
-	if (i.size()==0){
-		cout<<'Impossible'<<endl;
-		return 0;
-	}
-	int v=i[0];
-	int **g=knt(n,v,p,p);
-	int **h=knt(m,v-s,q,q);
-	
 }
