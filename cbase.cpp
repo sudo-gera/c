a3ea7428ba4b8c57324ec4e78389ab17460f35e6
sudo-gera <gera.tatarinov.999@gmail.com>
2021-01-31 23:55:37 +0300

    receiveing...

--- cbase.cpp
+++ cbase.cpp
@@ -1,20 +1,50 @@
+#pragma GCC optimize("Ofast")
 #include <stdio.h>
 #include <stdlib.h>
+#include <type_traits>
+#include <vector>
 #define int int64_t
-#define scan(q) scanf("%li",&(q));
-#define print(q) printf("%li\n",q);
-#define vect(type,name) auto name=(type*)calloc(sizeof(type),0);int name##_size=0;
-#define push(name,q) name=(decltype(name))realloc(name,(name##_size+1)*sizeof(*name));name[name##_size++]=q;
-#define ff(w,q) for(int w=0;w<q;w++)
+#define print(q) printf("%li\n",int64_t(q));
+#define prints(q) printf("%li ",int64_t(q));
+#define printt(q) printf("%li\t",int64_t(q));
+#define printn(q) printf("%li",int64_t(q));
+#define printc(q) printf("%c",char(c));
+#define _o(name) (((int*)(name))-1)
+#define _c(type,name) ((type)(((int*)(name))+1))
+#define _si (sizeof(int))
+#if 1
+#define _dt(name) std::remove_reference<decltype(name)>::type
+#else
+#define _dt(name) decltype(name)
+#endif
+#define vect(vtype) (_c(vtype*,calloc(1,_si)))
+#define len(name) (*_o(name))
+#define push(name,val) (name)=_c(_dt(name),realloc(_o(name),_si+(len(name)+1)*sizeof(*name)));(name)[len(name)++]=(val);
+int scan(){int64_t q;scanf("%li",&(q));return q;}
+#define ff(w,q) for(int w=0;w<(q);++w)
+#define fd(w,q) for(int w=0;w>(q);--w)
+#define fff(w,e,q) for(int w=(e);w<(q);++w)
+#define ffd(w,e,q) for(int w=(e);w>(q);--w)
+#define ffff(w,e,q,h) for(int w=(e);w<(q);w+=(h))
+#define fffd(w,e,q,h) for(int w=(e);w>(q);w-=(h))
+#define let auto
+#define s(q,w) (q)=(w);
+#define l(q,w) auto (q)=(w);
 
 decltype(0) main(){
-	vect(int,q);
+	l(q,vect(int*))
 	ff(w,4){
-		int h;
-		scan(h);
-		push(q,h);
+		push(q,vect(int))
 	}
-	ff(w,q_size){
-		print(q[w]);
+	ff(w,len(q)){
+		ff(e,7){
+			push(q[w],w*7+e);
+		}
+	}
+	ff(w,len(q)){
+		ff(e,len(q[w])){
+			printt(q[w][e])
+		}
+		printf("\n");
 	}
 }
--- test.cpp
+++ test.cpp
@@ -1,39 +1,50 @@
+#pragma GCC optimize("Ofast")
 #include <stdio.h>
 #include <stdlib.h>
-#include <typeinfo>
-#include <iostream>
 #include <type_traits>
+#include <vector>
 #define int int64_t
-#define print(q) printf("%li\n",(q));
+#define print(q) printf("%li\n",int64_t(q));
+#define prints(q) printf("%li ",int64_t(q));
+#define printt(q) printf("%li\t",int64_t(q));
+#define printn(q) printf("%li",int64_t(q));
+#define printc(q) printf("%c",char(c));
 #define _o(name) (((int*)(name))-1)
 #define _c(type,name) ((type)(((int*)(name))+1))
 #define _si (sizeof(int))
+#if 1
+#define _dt(name) std::remove_reference<decltype(name)>::type
+#else
+#define _dt(name) decltype(name)
+#endif
 #define vect(vtype) (_c(vtype*,calloc(1,_si)))
 #define len(name) (*_o(name))
-#define push(name,val) (name)=_c(std::remove_reference<decltype(name)>::type,realloc(_o(name),_si+(len(name)+1)*sizeof(*name)));(name)[len(name)++]=(val);
+#define push(name,val) (name)=_c(_dt(name),realloc(_o(name),_si+(len(name)+1)*sizeof(*name)));(name)[len(name)++]=(val);
 int scan(){int64_t q;scanf("%li",&(q));return q;}
 #define ff(w,q) for(int w=0;w<(q);++w)
+#define fd(w,q) for(int w=0;w>(q);--w)
+#define fff(w,e,q) for(int w=(e);w<(q);++w)
+#define ffd(w,e,q) for(int w=(e);w>(q);--w)
+#define ffff(w,e,q,h) for(int w=(e);w<(q);w+=(h))
+#define fffd(w,e,q,h) for(int w=(e);w>(q);w-=(h))
 #define let auto
+#define s(q,w) (q)=(w);
+#define l(q,w) auto (q)=(w);
 
 decltype(0) main(){
-	let q=vect(int*);
-	ff(w,5){
-		push(q,vect(int));
+	l(q,vect(int*))
+	ff(w,4){
+		push(q,vect(int))
 	}
-	ff(w,5){
+	ff(w,len(q)){
 		ff(e,7){
-			push(q[w],0);
+			push(q[w],w*7+e);
 		}
 	}
-	ff(w,5){
-		ff(e,7){
-			q[w][e]=w*7+e;
+	ff(w,len(q)){
+		ff(e,len(q[w])){
+			printt(q[w][e])
 		}
-	}
-	ff(w,5){
-		ff(e,7){
-			print(q[w][e]);
-		}
-		print(9999999)
+		printf("\n");
 	}
 }
