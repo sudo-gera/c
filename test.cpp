30f0a6e8c79cc3cf72bd8708086de70cd9010272
sudo-gera <gera.tatarinov.999@gmail.com>
2021-02-08 20:40:32 +0300

    receiveing...

--- test.cpp
+++ test.cpp
@@ -1,11 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct f{
- int g[444];};
-int main(){
- f h;
- h.g[4]=5;
- f v=h;
- cout<<v.g[4];
- v.g[4]=2;
- cout<<h.g[4];}
+int main(int argc,char ** argv){
+	vector<string> arg(argv,argv+argc);
+	for(auto j:arg){
+		cout<<j<<'\n';
+	}
+}
