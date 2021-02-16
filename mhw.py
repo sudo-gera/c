152acc3afee4dc52bd31e41c3ca5d0a490edffbd
sudo-gera <gera.tatarinov.999@gmail.com>
2021-02-17 01:16:29 +0300

    receiveing...

--- mhw.py
+++ mhw.py
@@ -1,9 +1,21 @@
-q=input()
-from re import *
-e=r'\s*'
-v=r'[\-!-(:-~]\S*'
-n=r'\d+'
-if fullmatch(e+v+e+':='+e+v+e,q) or fullmatch(e+v+e+':='+e+n+e,q):
-	print('YES')
-else:
-	print('NO')
+from functools import reduce
+t=0
+g=open('g.txt')
+g=[w.split() for w in g]
+g=[[w[0],reduce(lambda a,s:a[:-1]+[a[-1]+[int(s)]]if a and len(a[-1])==1 else a+[[s]],w[1:],[])] for w in g]
+g=[[w[0],{'pos':dict(w[1]),'unt':8*60}] for w in g]
+g=dict(g)
+f=open('f.txt')
+r=open('r.txt','w')
+ct=8*60
+for c in f:
+	c=c.split()
+	p=[g[w] for w in g if g[w]['unt']<=ct and c[1] in g[w]['pos']]
+	if not p:
+		p=min([g[w] for w in g if c[1] in g[w]['pos']],key=lambda a:a['unt'])
+		ct=p['unt']
+	else:
+		p=p[0]
+	t=ct+p['pos'][c[1]]
+	print(c[0],' ',t//60,':',t%60,sep='',file=r)
+	p['unt']=t
--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-from tkinter import filedialog
-from tkinter import *
-Tk().withdraw()
-s=filedialog.askopenfilename()
-print(s)
+from functools import reduce
+w=[1,2,3,4,5,6]
+w=[reduce(lambda a,s:a[:-1]+[a[-1]+[s]] if a and len(a[-1])<2 else a+[[s]],w,[])]
+print(w)
