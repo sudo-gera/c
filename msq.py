43ab3b60e4f4a98f328ecfce385812c426d79009
gera <gera@Air-tatarinov.mshome.net>
2021-03-12 00:07:50 +0300

    receiveing...

--- msq.py
+++ msq.py
@@ -1,8 +1,5 @@
-#from sys import argv
 n=int(input())
-a=[]
 for y in range(n):
-	a.append([])
 	for x in range(n):
 		m=(x+y-n//2)
 		b=(x-y-n//2)-m*(n-3)
@@ -12,11 +9,6 @@ for y in range(n):
 			b+=n
 		b//=2
 		e=m*n+b+1
-		a[-1].append(e)
 		print(e,end='\t')
-#		print('\x1b[9'+str(m%7+1)+'m',m%n,b%n,end='\t')
 	print()
 
-#	s=[sum(w) for w in a]+[sum(w) for w in zip(*a)]+[sum([a[w][w] for w in range(len(a))])]+[sum([a[w][len(a)-1-w] for w in range(len(a))])]
-#	if len(set(s))!=1:
-#		print(n)
