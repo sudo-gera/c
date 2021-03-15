d658da3e07266b42584251f729a7d4c5109d4422
gera <gera@Air-tatarinov.mshome.net>
2021-03-15 23:55:33 +0300

    receiveing...

--- ege.py
+++ ege.py
@@ -1,9 +1,13 @@
-for x in range(1,100000):
-	ox=x
-	a = 1
-	while x > 0:
-	 a *= x % 11
-	 x = x // 11
-	if a==120:
-		print(ox)
-		exit()
+a=[0]*1000
+a[1]=1
+for w in range(2,len(a)):
+	p=[w-1,w-2]
+	if w%3==0:
+		p.append(w//3)
+	if w>8:
+		p=[e for e in p if e>=8]
+	for e in p:
+		a[w]+=a[e]
+	a[10]=0
+	a[11]=0
+print(a[27])
