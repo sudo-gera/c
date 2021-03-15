f3af1bdd29ab051ed148db58a9aa7e587cdf1eb9
gera <gera@Air-tatarinov.mshome.net>
2021-03-15 22:28:37 +0300

    receiveing...

--- ege.py
+++ ege.py
@@ -1,15 +1,5 @@
-m=[99999999999,99999999999999]
-try:
-	q=100
-	while 1:
-		q+=1
-		a='1'*q
-		while '111' in a:
-			a=a.replace('111','22',1)
-			a=a.replace('222','11',1)
-		print([a.count('1'),q])
-		m=min([m,[a.count('1'),q]])
-except:
-	from traceback import *
-	print(format_exc())
-	print(m)
+def d(a,s):
+	return a%s==0
+for a in range(1,91):
+	if all([d(90,a) and (  d(x,a) or ( not d(x,15) or not d(x,20) ) ) for x in range(1,1000000)]):
+		print(a)
