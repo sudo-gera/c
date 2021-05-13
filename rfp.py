eaa5982e66f50ba5866001846cf0e8b39413bac7
sudo-gea <gera.tatarinov.999@gmail.com>
2021-05-13 23:41:33 +0300

    *interesting commit message*

--- rfp.py
+++ rfp.py
@@ -1,5 +1,7 @@
 from urllib import request
 
-r=request.Request('/')
-r.set_proxy('192.168.44.94','http')
+r=request.Request('http://vk.com/')
+# r.set_proxy('127.0.0.1:8080','http')
+# r.set_proxy('localhost:8080','http')
+r.set_proxy('192.168.44.94:8765','http')
 print(request.urlopen(r).read())
