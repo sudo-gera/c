097cc41ba63026e48cc2934da64fd245ff15120b
sudo-gea <gera.tatarinov.999@gmail.com>
2021-05-22 22:02:53 +0300

    *interesting commit message*

--- sth.py
+++
@@ -1,8 +0,0 @@
-n=int(input())
-q=[list(map(int,input().split())) for w in range(n)]
-a=[[(w[0]+e[0],w[1]+e[1]),[w,e]] for w in q for e in q]
-s={}
-for w in a:
-	s[w[0]]=[]
-for w in a:
-	s[w[0]].append(w[1])
