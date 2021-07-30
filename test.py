32ffd07a398ca114e50a1dd0e0b7a027078059f0
sudo-gea <gera.tatarinov.999@gmail.com>
2021-07-30 07:16:05 +0300

    *interesting commit message*

---
+++ test.py
@@ -0,0 +1,50 @@
+from sys import setrecursionlimit
+setrecursionlimit(1000000)
+from functools import lru_cache
+@lru_cache(maxsize=None)
+def c(n,k):
+	if k*2>n:
+		return c(n,n-k)
+	if k<0:
+		return 0
+	if k==0:
+		return 1
+	return c(n,k-1)*(n-k+1)//k
+# колчиество графов на a вершинах, где ровно b вершин не связаны ни с чем
+@lru_cache(maxsize=None)
+def f(a,b):
+	if b>a:
+		return 0
+	if a==b==0:
+		return 1
+	if a<0 or b<0:
+		print('error',a,b)
+		return 0
+	if b:
+		return c(a,b)*f(a-b,0)
+	else:
+		return sum([2**c(a,2)]+[-f(a,w) for w in range(1,a+1)])
+def solve(n):
+	return f(n,1)
+
+from fractions import Fraction
+@lru_cache(maxsize=None)
+def fr(q):
+	return Fraction(q).limit_denominator()
+def froot(x,n):
+	b=fr(0)
+	e=fr(x+1)
+	for w in range(64):
+		c=(b+e)/2
+		d=c**n
+		if d<x:
+			b=c
+		if d>x:
+			e=c
+		if d==x:
+			b=e=c
+	return (b+e)/2
+
+def fpow(a,s):
+	s=fr(s)
+	return froot(fr(a)**s.numerator,s.denominator)
--- test.py.cpp
+++ test.py.cpp
@@ -1,41 +1,18 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-vector<int64_t> _100000001_0_____0_100000002_0____0list_L_int_J;
 vector<int64_t> q0____0list_L_int_J;
-int64_t w0____0int;
-int64_t e0____0int;
-int64_t __python__q;
-vector<int64_t> _100000001_0____0callable_L__J(){
-				_100000001_0_____0_100000002_0____0list_L_int_J=vector<int64_t>({});
-            for (auto iterator:vector<int64_t>({int64_t(1),int64_t(2),int64_t(3),int64_t(4)})){
-                w0____0int=iterator;
-                for (auto iterator:q0____0list_L_int_J){
-                    e0____0int=iterator;
-                     /*
-Call(
-    func=Attribute(
-        value=Name(id='_100000002_', ctx=Load()),
-        attr='append',
-        ctx=Load()),
-    args=[
-        BinOp(
-            left=Name(id='w', ctx=Load()),
-            op=Add(),
-            right=BinOp(
-                left=Name(id='e', ctx=Load()),
-                op=Mult(),
-                right=Constant(value=4)))],
-    keywords=[])
-*/ ;
-                }            }				return _100000001_0_____0_100000002_0____0list_L_int_J;
-}
 int main(){
-	q0____0list_L_int_J=vector<int64_t>({int64_t(0)});
-	q0____0list_L_int_J=_100000001_0____0callable_L__J();
-	for (auto iterator:q0____0list_L_int_J){
-		w0____0int=iterator;
-		__python__q=w0____0int;
-		cout<<__python__q<<endl;;
-	}
+	q0____0list_L_int_J=vector<int64_t>({int64_t(1)});
+	 /*
+Call(
+	func=Attribute(
+		value=Name(id='q', ctx=Load()),
+		attr='append',
+		ctx=Load()),
+	args=[
+		Constant(value=1)],
+	keywords=[])
+*/ ;
+
 }
