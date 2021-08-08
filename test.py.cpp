597bca99ff47a9b85ee6b2b02ea9fab0a25b09fd
sudo-gea <gera.tatarinov.999@gmail.com>
2021-08-08 15:39:39 +0300

    *interesting commit message*

--- test.py.cpp
+++ test.py.cpp
@@ -1,18 +1,28 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-vector<int64_t> q0____0list_L_int_J;
+#define u32string string
+
+				#define to_u8
+				#define to_u32
+				#define chr
+				#define ord
+			
+
+			
+
+vector<int64_t> q____list_L_int_J;
+int64_t w____int;
+bool any____callable_L__J(vector<int64_t> arg0_____list_L_int_J){
+							q____list_L_int_J=arg0_____list_L_int_J;
+							for (auto iterator:q____list_L_int_J){
+								w____int=iterator;
+								if (bool(w____int)){
+									return true;
+								}else{
+								}							}							return false;
+}
 int main(){
-	q0____0list_L_int_J=vector<int64_t>({int64_t(1)});
-	 /*
-Call(
-	func=Attribute(
-		value=Name(id='q', ctx=Load()),
-		attr='append',
-		ctx=Load()),
-	args=[
-		Constant(value=1)],
-	keywords=[])
-*/ ;
+	cout<<to_u8(to_u32((any____callable_L__J(vector<int64_t>({int64_t(0),int64_t(0)})))?"True":"False"))<<"\n";
 
 }
