6590193765c663b3fc43fb75350cf2e2974a50e1
sudo-gera <gera.tatarinov.999@gmail.com>
2021-09-08 00:07:06 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,15 +1,4 @@
 #include <bits/stdc++.h>
 using namespace std;
-<<<<<<< HEAD
-int s();
-int a();
-int a(){
-	return s();}
-int s(){
-	return a();}
-int main(){}
-=======
-
-int main(){
-}
->>>>>>> 958edbffff45f5181ed6079b76bc214c68e40101
+void run(int*q){
+	
--- test.py
+++ test.py
@@ -1,32 +1,24 @@
-<<<<<<< HEAD
-from server_example import *
-a=Server()
-print(get_ips())
-print(a.port)
-while 1:
-	s=a.get()
-	try:
-		a.put(open(s[0],'rb').read())
-	except:
-		from traceback import format_exc
-		print(format_exc())
-=======
-<<<<<<< HEAD
-from functools import total_ordering
-@total_ordering
-class a:
- def __init__(s,q):
-  s.q=q
- def __lt__(s,o):
-  return s.q<o.q
-print(a(1)<a(2))
-print(a(1)==a(2))
-print(a(1)>a(2))
-print(a(1)>=a(2))
-print(a(1)!=a(2))
-print(a(1)<=a(2))
+c=[]
+def run(q,w):
+ global c
+ for e in range(w):
+  while 1:
+   while len(c)==0:
+    c+=input().split()
+   r=c[0]
+   c=c[1:]
+   try:
+   	r=int(r)
+   except:
+    print('cannot read:',r)
+   else:
+    break
+  if len(q)>e:
+   q[e]=r
+  else:
+   q.append(r)
 
-=======
-from lancom import *
->>>>>>> 958edbffff45f5181ed6079b76bc214c68e40101
->>>>>>> 25b0d80edd7bf374a8909a347803a8099de62da4
+q=[1,1,1]
+run(q,6)
+print(q)
+
