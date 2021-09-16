9e1c682091321147e92f02849d65571bc753def3
sudo-gera <gera.tatarinov.999@gmail.com>
2021-09-16 10:12:41 +0300

    *interesting commit message*

--- 103bn.cpp
+++ 103bn.cpp
@@ -1,50 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include "bn.h"
+#include "bn.h"
 
-// #define debug
+#define debug
 
-// #ifdef debug
-// #include <bits/stdc++.h>
-// using namespace std;
-// #endif
-
-// struct bn_s{
-// 	char sign;
-// 	size_t size;
-// 	int32_t *vect;
-// };
-
-// bn *bn_new(){
-// 	bn q={0,0,0};
-// 	// printf("%i %zu %zu\n",q.sign,q.size,size_t(q.vect));
-// }
-
-
-#define qq 0
-
-void run(int64_t q){
-	q%=1000;
-#if qq
-		int*a=(int*)malloc(sizeof(int)*q);
-		free(a);
-#else
-		int a[q];
+#ifdef debug
+#include <bits/stdc++.h>
+using namespace std;
 #endif
+
+struct bn_s{
+	char sign;
+	size_t size;
+	int32_t *vect;
+};
+
+bn *bn_new(){
+	bn *q=(bn*)calloc(sizeof(bn));
+	return q;
+}
+
+bn *bn_init(bn const*orig){
+	if(!orig){
+		return nullptr;
+	}
+	bn *q=(bn*)malloc(sizeof(bn));
+
+	// bn *q=calloc(sizeof(bn));
+	// return q;
 }
 
 
 int main(){
-	int q;
-	scanf("%i",&q);
-	srand(q);
-#if qq
-	printf("malloc\n");
-#else
-	printf("new\n");
-#endif
-	for (int64_t q=0;q<1'000'000'00;++q){
-		run(q+rand());
-	}
+	bn *q = bn_new();
 }
 
