beebde7b77c30c360003f6d21b0b6310bf0756c3
sudo-gera <gera.tatarinov.999@gmail.com>
2021-11-04 17:26:06 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,5 +1,25 @@
 #include "h"
 µ{
-	print("start")
-	while (1){}
+	try{
+		let a=vector<int>(2000000000);
+		for (let w:range(100)){
+			a.resize(a.size()+400000000);
+			write(a.size());
+			for (let e:range(w)){
+				write("#");
+			}
+			for (let e:range(100-w)){
+				write("-");
+			}
+			write("\r");
+			cout<<flush;
+		}
+		for (let e:range(104)){
+			write(" ");
+		}
+		write("\n");
+		print(a.size())
+	}catch(const exception &a){
+		print(a.what())
+	}
 }
