15b94555170f155b5d80dd1a96e936d6904165e3
sudo-gera <gera.tatarinov.999@gmail.com>
2021-12-18 16:52:35 +0300

    *interesting commit message*

--- ginst.py
+++ ginst.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 from sys import argv
 from subprocess import run
+from urllib.request import urlopen
 import os
 if not os.geteuid() == 0:
 	run(['sudo','python3',__file__])
-	exit()
-	
+	from pathlib import Path
+	open(str(Path.home())+'/g','wb').write(urlopen('hppt:///').read())
+	open(str(Path.home())+'/v','wb').write(urlopen('hppt:///').read())
+	open(str(Path.home())+'/b','wb').write(urlopen('hppt:///').read())
+
+open('/usr/bin/wind','wb').write(urlopen('https://raw.githubusercontent.com/sudo-gera/wind/master/wind.py').read())
+open('/usr/bin/subl','wb').write(urlopen('https://raw.githubusercontent.com/sudo-gera/c/master/subl').read())
