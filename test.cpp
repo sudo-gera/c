0ce36dc1c052752b72fa4842f651d2bfa4155dce
sudo-gera <gera.tatarinov.999@gmail.com>
2022-01-13 23:53:44 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -4,7 +4,6 @@
 #include <algorithm>
 
 int main(){
-<<<<<<< HEAD
 	size_t n;
 	scanf("%zi",&n);
 	auto a=new size_t[n];
@@ -16,7 +15,4 @@ int main(){
 	q=1.0*(clock()-q)/CLOCKS_PER_SEC;
 	printf("%ld\n",q);
 	printf("%zi\n",a[rand()%n]);
-=======
-	size_t q;
->>>>>>> a26a58cdce6d132344630f26cf2b9b1c4e85f8ad
 }
