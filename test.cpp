5b01ffc6293bd08ff52039136f0d8a05b51b22d2
sudo-gera <gera.tatarinov.999@gmail.com>
2022-02-06 21:21:47 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,3 +1,12 @@
 int main(){
-	
+	// auto q=clock();
+	auto e=perf();
+	let a=vector<int>();
+	for (auto w:range(9999999)){
+		a.push_back(w);
+	}
+	sort(a.begin(),a.end());
+	// auto w=clock();
+	// ic((w-q)/1.0/CLOCKS_PER_SEC)
+	ic(e)
 }
