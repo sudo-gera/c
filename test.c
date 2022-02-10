c89151eb4b8b6c522bcab368a9abd32a580bd209
sudo-gera <gera.tatarinov.999@gmail.com>
2022-02-10 15:02:46 +0300

    *interesting commit message*

--- test.c
+++ test.c
@@ -5,11 +5,23 @@
 #include <string.h>
 #include <stdbool.h>
 
-int run(int q=1){
-    return q;
+void gen(char*p,int ed,int start){
+    if (ed==0){
+        printf("%s\n",p);
+        return;
+    }
+    for (int i=start;i<8;++i){
+        p[i]='1';
+        gen(p,ed-1,i+1);
+        p[i]='0';
+    }
 }
 
 int main(){
-    printf("%i",run());
-    printf("%i",run(9));
+    char a[9];
+    for (size_t w=0;w<8;++w){
+        a[w]='0';
+    }
+    a[8]=0;
+    gen(a,3,0);
 }
