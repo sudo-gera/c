6791c3c56506e462510f6347ea8418151a03fd55
sudo-gera <gera.tatarinov.999@gmail.com>
2022-02-12 19:20:25 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,18 +1,2 @@
 from h import *
-def root(q):
-	p = 1
-	while p<q:
-		p *= 4
-	r = 0
-	while p:
-		u=r+p
-		if u<=q:
-			q -= u
-			r += p
-			r += p
-		r //= 2
-		p //= 4
-	return r
-
-
-print(all([root(w) == floor_root(w) for w in range(-9999,9999)]))
+a=StringIO()
