a0b5132499dadaa0bb96fd303db7a01b74061d0a
sudo-gera <gera.tatarinov.999@gmail.com>
2022-02-14 06:08:08 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-from h import *
-a=StringIO()
+# from h import *
+
+
+for a1 in range(2):[[
+	print(a1*16+a2*8+a3*4+a4*2+a5)
+			for a4 in range(2)
+		for a5 in range(2)]
+	for a2 in range(2)
+for a3 in range(2)]
