d87bfc09a24d018bed9b6fa5125166506d535462
sudo-gera <gera.tatarinov.999@gmail.com>
2022-02-15 16:00:59 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,15 +1,6 @@
 ima{
-	inf z=1;
-	inf x=1;
-	printf("{");
-	for (size_t w=0;w<1000'000'000;++w){
-		if (w%1000==0){
-				printf("{%s,%s},",z.to_string().c_str(),x.to_string().c_str());
-				fprintf(stderr, "%zu\n", w);
-		}
-		inf c=z+x;
-		z=x;
-		x=c;
-	}
-	printf("}\n");
+	long double q=45.56;
+	print(bin_repr(q))
+	q/=1<<1;
+	print(bin_repr(q))
 }
