5d118d0255cc55dc131b72a68896e5a99d42b9cd
sudo-gera <gera.tatarinov.999@gmail.com>
2022-02-16 17:30:49 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,6 +1,5 @@
+import(fibonacci,test)
+
 ima{
-	long double q=45.56;
-	print(bin_repr(q))
-	q/=1<<1;
-	print(bin_repr(q))
+	print(fibonacci(6));
 }
--- test.py
+++ test.py
@@ -1,14 +1,1 @@
 from h import *
-
-def aout(q):
-	return int(popen('./a.out '+str(q)).read())
-
-if len(argv)>1:
-	print(fibonacci(int(argv[1])))
-	exit()
-q=0
-while 1:
-	q+=1
-	if aout(q)!=fibonacci(q):
-		print(q)
-		break
