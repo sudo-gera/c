ce402be16924a6f15247777b49396153b9b46ecc
sudo-gera <gera.tatarinov.999@gmail.com>
2022-02-20 09:35:59 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,26 +1,9 @@
-#include "d"
-#define bit_get(a,s)   (((a)[(s)/8/sizeof((a)[0])]>>(s)%(8*sizeof((a)[0])))&1)
-#define bit_set(a,s,d) {(a)[(s)/8/sizeof((a)[0])]&=~(1<<(s)%(8*sizeof((a)[0])));(a)[(s)/8/sizeof((a)[0])]+=(d)<<(s)%(8*sizeof((a)[0]));}
+#include <algorithm>
 
-
-ima{
-	auto q=new uint8_t[4]();
-	size_t w=1;
-	cin>>w;
-	while (w){
-		if (w==1){
-			size_t e,r;
-			cin>>e>>r;
-			bit_set(q,e,r);
-		}else{
-			size_t e;
-			cin>>e;
-			print(bit_get(q,e))
-		}
-		// for (size_t w=0;w<4;++w){
-		// 	write(bin_repr(q[w]));
-		// }
-		// print()
-		cin>>w;
+int main(){
+	int*a=new int[9];
+	for (int w=0;w<9;++w){
+		a[w]=-w;
 	}
+	std::sort(a,a+9);
 }
