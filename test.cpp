c4437171cd1e08c831fc14a24c714fa5019ca89f
sudo-gera <gera.tatarinov.999@gmail.com>
2022-02-23 02:45:36 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 
 int main(){
-	system("arch");
-	int q;
-	scanf("%i",&q);
-	int w=12/q;
-	printf("%i\n",w);
+	// system("arch");
+	// int q;
+	// scanf("%i",&q);
+	// int w=12/q;
+	// printf("%i\n",w);
+	// long double q=0;
+	// q=0/q;
+	// print(q,bin_repr(q));
 }
