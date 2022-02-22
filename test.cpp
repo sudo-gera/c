7f2db97fa8b71c051f68ecc7a6caa2f2744006b1
sudo-gera <gera.tatarinov.999@gmail.com>
2022-02-22 22:27:01 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-#include <bits/stdc++.h>
-#include <float.h>
-using namespace std;
+#include <stdio.h>
+
 int main(){
-	cout<<sizeof(FLT_RADIX)<<endl;
-=======
-<<<<<<< HEAD
-int main(){
-	long double q=1;
-	size_t c=0;
-	while (1!=q+1){
-		print(bin_repr(q),bin_repr(q+1))
-		q/=2;
-		c+=1;
-	}
-	print(c)
-=======
-ima{
-	float w=45.56;
-	let q=bin_repr(w);
-	print(str(q.begin(),q.begin()+1),str(q.begin()+1,q.begin()+25),str(q.begin()+25,q.end()))
-	print(inf(q,2).to_string(16))
->>>>>>> edd1a72a29bdf481718ea43e7194f3dbec21102a
->>>>>>> 1b548a9042163c7b39905594b6f4a431ef097f70
+	int q;
+	scanf("%i",&q);
+	int w=12/q;
+	printf("%i\n",w);
 }
