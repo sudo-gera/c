ef6cfe857c8cb5137d3a936f3341be636618b6d3
sudo-gera <gera.tatarinov.999@gmail.com>
2022-02-22 08:37:49 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
-ima{
-	float w=45.56;
-	let q=bin_repr(w);
-	print(str(q.begin(),q.begin()+1),str(q.begin()+1,q.begin()+25),str(q.begin()+25,q.end()))
-	print(inf(q,2).to_string(16))
+#include <bits/stdc++.h>
+#include <float.h>
+using namespace std;
+int main(){
+	cout<<sizeof(FLT_RADIX)<<endl;
 }
