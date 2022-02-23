a7bfd79b90e2b3e1de0a09ba2a98c11d095039ca
sudo-gera <gera.tatarinov.999@gmail.com>
2022-02-24 00:56:04 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,15 +1,14 @@
 from h import *
 def root(q):
-	# x=Fraction(q)
-	# for w in range(9):
-	# 	x=(x*x+q)/2/x
-	# 	print(float(x))
-	# return x
-	xn=q
-	xd=1
-	for w in range(1):
-		xn=xn*xn+xd*xd*q
-		xd=xd*xn*2
-		print(xn,xd)
-		print(xn/xd)
-	return Fraction(xn,xd)
+	x=Fraction(q)
+	for w in range(10):
+		x=(x+q/x)/2
+	return x
+	# xn=q
+	# xd=1
+	# for w in range(30):
+	# 	xn,xd=xn*xn+xd*xd*q,xd*xn*2
+	# 	t=gcd(xn,xd)
+	# 	xn//=t
+	# 	xd//=t
+	# return Fraction(xn,xd)
