91fd050d2183aff71dd23bc4f5eb75b3ba8a2775
sudo-gera <gera.tatarinov.999@gmail.com>
2022-02-25 10:00:03 +0300

    *interesting commit message*

--- subl_open.py
+++ subl_open.py
@@ -2,10 +2,10 @@ from os.path import *
 from subprocess import *
 from time import *
 from sys import *
-while exists(expanduser('~/ubuntu/.subl_open')):
-	for w in open(expanduser('~/ubuntu/.subl_open')).read().splitlines():
-		w=w.replace('/home/gera/','~/ubuntu/')
+while exists(expanduser('~/ubuntux/.subl_open')):
+	for w in open(expanduser('~/ubuntux/.subl_open')).read().splitlines():
+		w=w.replace('/home/gera/','~/ubuntux/')
 		Popen(['/opt/local/bin/subl',w])
-	open(expanduser('~/ubuntu/.subl_open'),'w')
+	open(expanduser('~/ubuntux/.subl_open'),'w')
 	sleep(1)
 
