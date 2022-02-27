85a1f5698019db8e8047e90c54558556c5b099f8
sudo-gera <gera.tatarinov.999@gmail.com>
2022-02-27 16:53:01 +0300

    *interesting commit message*

--- sort.h
+++ sort.h
@@ -3,13 +3,13 @@
 template <typename T>
 void Sort(T *begin, T *end) {
   if (begin < end) {
-    T *pivot_value = begin + (end - begin) / 2;
+    T pivot_value = *(begin + (end - begin) / 2);
     T *i = begin, *j = end - 1;
     while (!(j < i)) {
-      while (*i < *pivot_value) {
+      while (*i < pivot_value) {
         ++i;
       }
-      while (*pivot_value < *j) {
+      while (pivot_value < *j) {
         --j;
       }
       if (!(j < i)) {
@@ -20,8 +20,15 @@ void Sort(T *begin, T *end) {
         --j;
       }
     }
-    Sort(begin, j + 1);
-    Sort(i, end);
+    if (begin<j and pivot_value < *j){
+      j--;
+    }
+    if (j+1<end){
+      Sort(begin, j + 1);
+    }
+    if (begin<j+1){
+      Sort(j+1, end);
+    }
   }
 }
 
