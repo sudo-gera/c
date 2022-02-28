158e42862ddf176619ce52c75731bccff9f7ae7e
sudo-gera <gera.tatarinov.999@gmail.com>
2022-02-28 17:34:42 +0300

    *interesting commit message*

--- subl_u.py
+++ subl_u.py
@@ -2,7 +2,7 @@ from os.path import *
 from subprocess import *
 from time import *
 from sys import *
-a=open(expanuser('~/.subl_open'),'a')
+a=open(expanduser('~/.subl_open'),'a')
 for w in argv[1:]:
 	w=abspath(w)
 	a.write(w+'\n')
