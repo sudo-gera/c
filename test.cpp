05311b0da6a00a5618c8fefe543b1c20953fc024
sudo-gera <gera.tatarinov.999@gmail.com>
2022-02-28 22:29:52 +0500

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-#define bit_get(a,s)   (((a)[(s)/8/sizeof((a)[0])]>>(s)%(8*sizeof((a)[0])))&1)
-
-#include <stdio.h>
-
-int main(){
-	for (long double f=1;f+1!=f;f*=2){
-//	for (long double f=-9;f<9;f++){
-		f+=1;
-		unsigned*p=(unsigned*)&f;
-		for (long w=0;w<sizeof(f)*8;++w){
-			printf("%i",bit_get(p,w));
-		}
-		printf(" %Lf\n",f);
-		f-=1;
-	}
-=======
 #include <stdio.h>
 
 int main(){
@@ -22,5 +5,4 @@ int main(){
 	scanf("%i",&q);
 	int w=12/q;
 	printf("%i\n",w);
->>>>>>> 7f2db97fa8b71c051f68ecc7a6caa2f2744006b1
 }
