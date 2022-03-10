f4da93dc1edc5afbf287299a95afffbb84eb4d64
sudo-gera <gera.tatarinov.999@gmail.com>
2022-03-10 04:17:32 +0300

    *interesting commit message*

--- create_input.py
+++ create_input.py
@@ -3,8 +3,9 @@ def rand(q=2**64):
 	assert q
 	return randint(0,q-1)
 
-n=rand(9999)
+n=rand(4)
 print(n)
-a=[rand(9999) for w in range(n)]
-a.sort()
+a=[rand(2**30) for w in range(n)]
+# a.sort()
+# a=a[::-1]
 print(*a)
