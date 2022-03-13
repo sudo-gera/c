48771c73adda419b421c16750d89c991418e85d5
sudo-gera <gera.tatarinov.999@gmail.com>
2022-03-13 18:14:45 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -6,7 +6,96 @@
 using namespace std;
 #include <bits/stdc++.h>
 using namespace std;
-#include "pointer.hpp"
+#include <bits/stdc++.h>
+using namespace std;
+template<typename T>
+class pointer{
+	int64_t *count=nullptr;
+	T *value=nullptr;
+	void __init__(const T&q){
+		value=new T(q);
+		count=new int64_t(1);
+	}
+	void __init__(const pointer& o){
+		value=o.value;
+		count=o.count;
+		if (count){
+			*count+=1;
+		}
+	}
+public:
+	pointer(){
+	}
+	pointer(const T&q){
+		this->__init__(q);
+	}
+	pointer(const pointer& o){
+		this->__init__(o);
+	}
+	pointer(const T*o){
+		if(o){
+			this->__init__(*o);
+		}
+	}
+	pointer&operator=(const T& q){
+		this->clear();
+		this->__init__(q);
+		return *this;
+	}
+	pointer&operator=(const pointer&q){
+		this->clear();
+		this->__init__(q);
+		return *this;
+	}
+	pointer&operator=(const T*q){
+		this->clear();
+		if(q){
+			this->__init__(*q);
+		}
+		return *this;
+	}
+	~pointer(){
+		this->clear();
+	}
+	operator bool()const{
+		return count;
+	}
+	void clear(){
+		if(count){
+			*count-=1;
+			if (*count==0){
+				delete count;
+				count=nullptr;
+				delete value;
+				value=nullptr;
+			}
+		}
+	}
+	template <typename Y>
+	bool operator<(const pointer<Y> o)const{
+		return (*value)<(*o.value);
+	}
+	T &operator*(){
+		return *value;
+	}
+	T *operator->()const{
+		return value;
+	}
+	operator T*(){
+		return value;
+	}
+	friend ostream&operator<<(ostream&q,const pointer&w){
+		q<<w.value;
+		return q;
+	}
+	friend istream&operator>>(istream&q,pointer&w){
+		T e;
+		q>>e;
+		w=e;
+		return q;
+	}
+};
+
 
 
 int qq=0;
@@ -483,6 +572,12 @@ int main(){
 	// 	swap(*w,*e);
 	// }
 	// assert(a.begin()<a.begin()+1);
-	sort(aa.begin(),aa.end());
+	// sort(aa.begin(),aa.end());
 	// ic(aa)
+	auto a=treap<long>({5,4,3,2,1});
+	___mergesort(a.begin(),a.end());
+	for (auto w:a){
+		cout<<w<<' ';
+	}
+	cout<<endl;
 }
