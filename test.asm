d9464d372cb4c849337825f36455ed043b156247
sudo-gera <gera.tatarinov.999@gmail.com>
2022-03-23 17:25:11 +0500

    *interesting commit message*

--- test.asm
+++ test.asm
@@ -3,17 +3,28 @@
 
 start
 
-mov ebx,17
-mov eax,4
-mov ecx,19
-mov edx,20
+; mov ebx,17
+; mov eax,4
+; mov ecx,19
+; mov edx,20
 
-regout
-_divmod ebx,eax,ebx,eax
-regout
+; regout
+; _divmod eax,bx,bx,al
+; regout
 
-UNSINT ebx
-PUTCHAR 10
+; push ah
+
+
+
+; push word 256+2
+; push word 256*3+4
+; mov eax,0
+; mov al,[esp]
+
+
+
+; UNSINT ebx
+; PUTCHAR 10
 UNSINT eax
 PUTCHAR 10
 
