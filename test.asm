749587e67247d94b51bbfb7101d1feec81505ed9
sudo-gera <gera.tatarinov.999@gmail.com>
2022-03-25 04:07:37 +0500

    *interesting commit message*

--- test.asm
+++ test.asm
@@ -5,23 +5,21 @@ darray aa
 start
 GETUN eax
 dmov ebx,2
-_1:
 dmov ecx,_2
+_1:
 dule eax,1,ecx
 	dumod ecx,eax,ebx
-	cmp ecx,0
-	je _3
-		dadd ebx,ebx,1
+	deq ecx,0,_3
+		dadd ebx,1
 		jmp _4
 	_3:
 		dapush aa,ebx
-		dudiv eax,eax,ebx
+		dudiv eax,ebx
 	_4:
 jmp _1
 _2:
-dmov eax,0
 _5:
-duge eax,[len_aa],_6
+duge 0,[len_aa],_6
 	dapop ebx,aa
 	UNSINT ebx
 	PUTCHAR 10
