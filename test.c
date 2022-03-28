b624e0327a8ab95849c2692b943e7403ca407993
sudo-gera <gera.tatarinov.999@gmail.com>
2022-03-28 23:05:49 +0300

    *interesting commit message*

--- test.c
+++ test.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+
+int run(unsigned int q){
+	return q+1;
+}
+
+int aaa(long t){
+	return t;
+}
+
+int bbb(){
+	return 0;
+}
+
+
+
 int main(){
-	unsigned q=({printf("--");0;});
-	scanf("%i",&q);
-	q*=q;
-	printf("%i",q);
+	int (*p)();
+	p=run;
+	printf("%i",p(9));
+	p=aaa;
+	printf("%i",p(48));
+	p=bbb;
 }
