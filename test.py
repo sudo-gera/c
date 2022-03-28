a4e4d5d67e45ad3ac7cf791606ffcedb08bda2dc
sudo-gera <gera.tatarinov.999@gmail.com>
2022-03-28 22:52:46 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,13 +1,10 @@
 from h import *
 
-chdir('../Downloads/Rushia')
+# chdir('../Downloads/Rushia')
 a=listdir()
-t=getctime(a[0])
-s=[]
-s+=['%02i'%(t%60)]
-t//=60
-s+=['%02i'%(t%60)]
-t//=60
-s+=['%02i:'%(t%24)]
-t//=24
-print(a[0],s)
+a=[w for w in a if w[0]!='.']
+a=[[stat(w).st_birthtime,w] for w in a]
+a.sort()
+a=[w[1] for w in a]
+for q,w in enumerate(a):
+	rename(w,('%03i'%q)+w)
