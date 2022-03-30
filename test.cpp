46379d4792d46da73f987a9a90af31c3c3a634ef
sudo-gera <gera.tatarinov.999@gmail.com>
2022-03-31 00:13:53 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,112 +1,48 @@
-#include <assert.h>
+#include <cassert>
 #include <iomanip>
 #include <iostream>
-struct point {
-  long x;
-  long y;
+// auto ppp=perf();
+
+struct Point {
+  int64_t x;
+  int64_t y;
 };
 
-bool is_less(point q, point w) {
+bool IsLess(Point q, Point w) {
   if (q.x != w.x) {
     return q.x < w.x;
   }
   return q.y < w.y;
 }
 
-bool is_eq(point q, point w) { return not is_less(q, w) and not is_less(w, q); }
-
-auto rotate(point A, point B, point C) {
-  return (B.x - A.x) * (C.y - B.y) - (B.y - A.y) * (C.x - B.x);
+bool IsEq(Point q, Point w) {
+  return !IsLess(q, w) && !IsLess(w, q);
 }
 
-auto grahamscan(point *A, long n, long *S) {
-  auto P = new long[n];
-  for (long w = 0; w < n; ++w) {
-    P[w] = w;
-  }
-  for (auto i = 1; i < n; ++i) {
-    if (A[P[i]].x < A[P[0]].x) {
-      std::swap(P[i], P[0]);
-    }
-  }
-  for (auto i = 2; i < n; i++) {
-    auto j = i;
-    while (j > 1 and (rotate(A[P[0]], A[P[j - 1]], A[P[j]]) < 0)) {
-      std::swap(P[j], P[j - 1]);
-      j -= 1;
-    }
-  }
-  S[0] = P[0];
-  S[1] = P[1];
-  long ls = 2;
-  for (auto i = 2; i < n; ++i) {
-    while (rotate(A[S[ls - 2]], A[S[ls - 1]], A[P[i]]) < 0) {
-      ls--;
-    }
-    S[ls++] = P[i];
-  }
-  delete[] P;
-  return ls;
+int64_t VecProdDot(Point a, Point b, Point c) {
+  return (b.x - a.x) * (c.y - b.y) - (b.y - a.y) * (c.x - b.x);
 }
 
-auto vd(point q, point e) { return q.x * e.y - q.y * e.x; }
+int64_t *pppp;
 
-auto vect(point _x, point _y) {
-  auto x = _y.x - _x.x;
-  auto y = _y.y - _x.y;
-  return point{x, y};
+Point *aaaa;
+
+bool AnLess(int64_t b, int64_t c) {
+  return (VecProdDot(aaaa[pppp[0]], aaaa[b], aaaa[c]) >= 0);
 }
 
-auto area(point *vertices, long n) {
-  long res = 0;
-  for (long w = 2; w < n; ++w) {
-    res += vd(vect(vertices[0], vertices[w - 1]),
-              vect(vertices[w - 1], vertices[w]));
-  }
-  return abs(res) / 2.;
-}
-
-auto unique(point *first, point *last) {
-  if (first == last)
-    return last;
-
-  auto result = first;
-  while (++first != last) {
-    if (!is_eq(*result, *first) && ++result != first) {
-      *result = *first;
-    }
-  }
-  return ++result;
-}
-
-void rotate(point *first, point *n_first, point *last) {
-  if (first == n_first)
-    return;
-  if (n_first == last)
-    return;
-
-  auto read = n_first;
-  auto write = first;
-  auto next_read = first;
-
-  while (read != last) {
-    if (write == next_read) {
-      next_read = read;
-    }
-    std::swap(*(write++), *(read++));
-  }
-}
-
-template <typename T> void PushHeap(T *begin, T *end) {
+template <typename T>
+void PushHeapA(T *begin, T *end) {
   int i = end - begin;
-  while (1 < i && is_less(begin[i / 2 - 1], begin[i - 1])) {
+  while (1 < i && AnLess(begin[i / 2 - 1], begin[i - 1])) {
     T t = begin[i - 1];
     begin[i - 1] = begin[i / 2 - 1];
     begin[i / 2 - 1] = t;
     i /= 2;
   }
 }
-template <typename T> void PopHeap(T *begin, T *end) {
+template <typename T>
+void PopHeapA(T *begin, T *end) {
   int i = 0, n = end - begin;
   if (1 < n) {
     T t = *(end - 1);
@@ -122,10 +58,10 @@ template <typename T> void PopHeap(T *begin, T *end) {
       }
       left = 2 * i;
       right = 2 * i + 1;
-      if (left - 1 < n && is_less(begin[i - 1], begin[left - 1])) {
+      if (left - 1 < n && AnLess(begin[i - 1], begin[left - 1])) {
         c = left;
       }
-      if (right - 1 < n && is_less(begin[c - 1], begin[right - 1])) {
+      if (right - 1 < n && AnLess(begin[c - 1], begin[right - 1])) {
         c = right;
       }
       if (i < c) {
@@ -136,35 +72,167 @@ template <typename T> void PopHeap(T *begin, T *end) {
     }
   }
 }
-template <typename T> void heapsort(T *begin, T *end) {
-  for (T *w = begin; w != end; ++w) {
-    PushHeap(begin, w + 1);
+template <typename T>
+void HeapsortA(T *begin, T *end) {
+  for (int64_t i = 0; i != end - begin; ++i) {
+    PushHeapA(begin, begin + i + 1);
   }
-  for (T *w = end; w != begin; --w) {
-    PopHeap(begin, w);
+  for (int64_t i = end - begin; i != 0; --i) {
+    PopHeapA(begin, begin + i);
   }
 }
 
-auto run(point *a, long n) {
-  heapsort(a, a + n);
-  n = unique(a, a + n) - a;
-  auto aa = new long[n];
-  auto laa = grahamscan(a, n, aa);
-  auto s = new point[n];
-  long ls = 0;
-  for (auto w = 0; w < laa; ++w) {
+int64_t Grahamscan(Point *a, int64_t n, int64_t *s) {
+  auto *p = new int64_t[n];
+  for (int64_t w = 0; w < n; ++w) {
+    p[w] = w;
+  }
+  for (int64_t i = 1; i < n; ++i) {
+    if (a[p[i]].x < a[p[0]].x) {
+      std::swap(p[i], p[0]);
+    }
+  }
+  pppp = p;
+  aaaa = a;
+  HeapsortA(p, p + n);
+
+  s[0] = p[0];
+  s[1] = p[1];
+  int64_t ls = 2;
+  for (int64_t i = 2; i < n; ++i) {
+    while (VecProdDot(a[s[ls - 2]], a[s[ls - 1]], a[p[i]]) < 0) {
+      ls--;
+    }
+    s[ls++] = p[i];
+  }
+  delete[] p;
+  return ls;
+}
+
+int64_t VecProd(Point q, Point e) {
+  return q.x * e.y - q.y * e.x;
+}
+
+Point Diff(Point xx, Point yy) {
+  int64_t x = yy.x - xx.x;
+  int64_t y = yy.y - xx.y;
+  return Point{x, y};
+}
+
+double Area(Point *arr, int64_t n) {
+  int64_t res = 0;
+  for (int64_t w = 2; w < n; ++w) {
+    res += VecProd(Diff(arr[0], arr[w - 1]), Diff(arr[w - 1], arr[w]));
+  }
+  return abs(res) / 2.;
+}
+
+Point *Unique(Point *first, Point *last) {
+  if (first == last) {
+    return last;
+  }
+
+  Point *result = first;
+  while (++first != last) {
+    if (!IsEq(*result, *first) && ++result != first) {
+      *result = *first;
+    }
+  }
+  return ++result;
+}
+
+void Rotate(Point *first, Point *r_to, Point *last) {
+  if (first == r_to) {
+    return;
+  }
+  if (r_to == last) {
+    return;
+  }
+
+  Point *read = r_to;
+  Point *write = first;
+  Point *next_read = first;
+
+  while (read != last) {
+    if (write == next_read) {
+      next_read = read;
+    }
+    std::swap(*(write++), *(read++));
+  }
+
+  Rotate(write, next_read, last);
+}
+
+template <typename T>
+void PushHeap(T *begin, T *end) {
+  int i = end - begin;
+  while (1 < i && IsLess(begin[i / 2 - 1], begin[i - 1])) {
+    T t = begin[i - 1];
+    begin[i - 1] = begin[i / 2 - 1];
+    begin[i / 2 - 1] = t;
+    i /= 2;
+  }
+}
+template <typename T>
+void PopHeap(T *begin, T *end) {
+  int i = 0, n = end - begin;
+  if (1 < n) {
+    T t = *(end - 1);
+    *(end - 1) = *begin;
+    *begin = t;
+    --n;
+    int left, right, c = 1;
+    while (i < c) {
+      if (0 < c) {
+        i = c;
+      } else {
+        c = i;
+      }
+      left = 2 * i;
+      right = 2 * i + 1;
+      if (left - 1 < n && IsLess(begin[i - 1], begin[left - 1])) {
+        c = left;
+      }
+      if (right - 1 < n && IsLess(begin[c - 1], begin[right - 1])) {
+        c = right;
+      }
+      if (i < c) {
+        T t = begin[i - 1];
+        begin[i - 1] = begin[c - 1];
+        begin[c - 1] = t;
+      }
+    }
+  }
+}
+template <typename T>
+void Heapsort(T *begin, T *end) {
+  for (int64_t i = 0; i != end - begin; ++i) {
+    PushHeap(begin, begin + i + 1);
+  }
+  for (int64_t i = end - begin; i != 0; --i) {
+    PopHeap(begin, begin + i);
+  }
+}
+
+Point *Run(Point *a, int64_t n) {
+  Heapsort(a, a + n);
+  n = Unique(a, a + n) - a;
+  auto *aa = new int64_t[n];
+  int64_t laa = Grahamscan(a, n, aa);
+  auto *s = new Point[n];
+  int64_t ls = 0;
+  for (int64_t w = 0; w < laa; ++w) {
     s[ls++] = a[aa[w]];
   }
   delete[] aa;
   delete[] a;
   a = s;
   n = ls;
-  long nn = n;
-  for (auto w = 0; w < nn; ++w) {
-    auto s = new point[n];
-    long ls = 0;
-    for (auto e = 0; e < n; e++) {
-      if (ls < 2 or vd(vect(s[ls - 1], a[e]), vect(s[ls - 1], s[ls - 2]))) {
+  for (int64_t w = 0; w < 3; ++w) {
+    auto *s = new Point[n];
+    int64_t ls = 0;
+    for (int64_t e = 0; e < n; e++) {
+      if (ls < 2 || VecProd(Diff(s[ls - 1], a[e]), Diff(s[ls - 1], s[ls - 2]))) {
         s[ls++] = a[e];
       } else {
         s[ls - 1] = a[e];
@@ -173,26 +241,40 @@ auto run(point *a, long n) {
     n = ls;
     delete[] a;
     a = s;
-    rotate(a, a + 1, a + n);
+    Rotate(a, a + 1, a + n);
   }
-  auto d=area(a, n);
-  std::sort(a,a+n,is_less);
+  Rotate(a, a + n - 3, a + n);
+  double d = Area(a, n);
   std::cout << n << std::endl;
-  for (auto w = 0; w < n; ++w) {
-    std::cout << a[w].x << ' ' << a[w].y << std::endl;
+  int64_t perf_index = 0;
+  for (int64_t w = 0; w < n; ++w) {
+    bool mp = false;
+    if (a[w].x < a[perf_index].x) {
+      mp = true;
+    }
+    if (a[w].x == a[perf_index].x && a[w].y < a[perf_index].y) {
+      mp = true;
+    }
+    if (mp) {
+      perf_index = w;
+    }
+  }
+  Rotate(a, a + perf_index, a + n);
+  for (int64_t w = n; w > 0; --w) {
+    std::cout << a[w % n].x << ' ' << a[w % n].y << std::endl;
   }
   std::cout << std::fixed << std::setprecision(1) << d << std::endl;
   return a;
 }
 
 int main() {
-  long n;
+  int64_t n;
   std::cin >> n;
-  auto a = new point[n];
-  for (auto w = 0; w < n; ++w) {
+  auto *a = new Point[n];
+  for (int64_t w = 0; w < n; ++w) {
     std::cin >> a[w].x;
     std::cin >> a[w].y;
   }
-  auto s=run(a, n);
+  Point *s = Run(a, n);
   delete[] s;
 }
--- test.py
+++ test.py
@@ -1,10 +1,69 @@
 from h import *
 
-# chdir('../Downloads/Rushia')
-a=listdir()
-a=[w for w in a if w[0]!='.']
-a=[[stat(w).st_birthtime,w] for w in a]
-a.sort()
-a=[w[1] for w in a]
-for q,w in enumerate(a):
-	rename(w,('%03i'%q)+w)
+@dataclass(order=1)
+class point:
+  x:int
+  y:int
+
+def rotate(A,B,C):
+  return (B.x-A.x)*(C.y-B.y)-(B.y-A.y)*(C.x-B.x)
+
+def grahamscan(A):
+  n = len(A)
+  P = list(range(n))
+  for i in range(1,n):
+    if A[P[i]].x<A[P[0]].x:
+      P[i], P[0] = P[0], P[i]
+  for i in range(2,n):
+    j = i
+    while j>1 and (rotate(A[P[0]],A[P[j-1]],A[P[j]])<0): 
+      P[j], P[j-1] = P[j-1], P[j]
+      j -= 1
+  S = [P[0],P[1]]
+  for i in range(2,n):
+    while rotate(A[S[-2]],A[S[-1]],A[P[i]])<0:
+      S.pop()
+    S.append(P[i])
+  return S
+
+
+def vd(q,e):
+  return q.x*e.y-q.y*e.x;
+
+def vect(_x,_y):
+  x=_y.x-_x.x
+  y=_y.y-_x.y
+  return point(x,y)
+
+def area(vertices):
+  res=0;
+  for w in range(2,len(vertices)):
+    res+=vd(vect(vertices[0],vertices[w-1]),vect(vertices[w-1],vertices[w]));
+  return abs(res)/2;
+
+def run(a):
+  n=len(a)
+  a.sort()
+  a=reduce(lambda a,s:a+[s] if s not in a else a, a, [])
+  aa=grahamscan(a)
+  a=[a[w] for w in aa]
+  for w in range(len(a)):
+    a=reduce(lambda _a,_s:_a+[_s] if len(_a)<2 or vd(vect(_a[-1],_s),vect(_a[-1],_a[-2])) else _a[:-1]+[_s], a, [])
+    a=a[1:]+a[:1]
+  r=[]
+
+  for w in a:
+    r.append(w)
+
+  r=[len(a)]+r
+  r.append(round(area(a),1))
+  return r
+
+if __name__ == '__main__':
+  n=scan(int)
+  a=[point(scan(int),scan(int)) for w in range(n)]
+  a=run(a)
+  print(a[0])
+  for w in a[1:-1]:
+    print(w.x,w.y)
+  print(a[-1])
