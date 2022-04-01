78e2af667556b2853d0ad9472855404018a1c04b
sudo-gera <gera.tatarinov.999@gmail.com>
2022-04-01 06:37:10 +0500

    *interesting commit message*

--- test.asm
+++ test.asm
@@ -1,8 +1,25 @@
 %include "st_io.inc"
 %include "macro.inc"
+start
 
+def factor
+	dmov ebx,2
+	while ebx,<=,65536
+		dmod edx,eax,ebx
+		while edx,==,0
+			ddiv eax,ebx
+			print ebx
+			dmod edx,eax,ebx
+		end
+		dinc ebx
+	end
+	if eax,>,1
+		print eax
+	end
+end
 
-
+dget eax
+call0 factor,eax
 
 
 stop
