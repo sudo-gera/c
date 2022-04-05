4d797172abb61bf61d85c81e5d0dd7c5f2be38cb
sudo-gera <gera.tatarinov.999@gmail.com>
2022-04-05 22:50:32 +0000

    *interesting commit message*

--- treap.cpp
+++ treap.cpp
@@ -1,280 +1,301 @@
-#include <bits/stdc++.h>
-#include "pointer.hpp"
+#define private public
+#define class struct
+#include "treap.hpp"
+// #include <bits/stdc++.h>
 using namespace std;
 
-template <typename t>
-struct el{
-	t v;
-	int64_t w;
-	pointer<el> _z=nullptr;
-	pointer<el> _x=nullptr;
-	int64_t s=1;
-	el(t _v){
-		v=_v;
-		w=rand();
-	}
-
-	pointer<el> z(){
-		return _z;
-	}
-	template<typename y>
-	void z(y q){
-		_z=q;
-		update(this);
-	}
-
-	pointer<el> x(){
-		return _x;
-	}
-	template<typename y>
-	void x(y q){
-		_x=q;
-		update(this);
-	}
-};
-
 template<typename T>
-pointer<el<T>> get_by_index(pointer<el<T>> s,int64_t n){
-	if (!s){
-		cout<<__LINE__<<" ValueError"<<endl;
-		return (pointer<el<T>>)(nullptr);
+void check(T q,T e){
+	vector<T> a;
+	for (auto w=q;w!=e;++w){
+		a.push_back(w);
 	}
-	if (!(s->z())){
-		if (n==0){
-			return s;
-		}
-		else{
-			return get_by_index(s->x(),n-1);
-		}
-	}
-	else{
-		if (n==s->z()->s){
-			return s;
-		}
-		if (n<s->z()->s){
-			return get_by_index(s->z(),n);
-		}
-		if (n>s->z()->s){
-			return get_by_index(s->x(),n-s->z()->s-1);
+	a.push_back(e);
+	for (size_t w=0;w<a.size();++w){
+		for (size_t e=0;e<a.size();++e){
+			long d=long(w)-long(e);
+			assert(a[w]-a[e]==d);
+			assert(w>=e or  a[w]<a[e]);
+			assert(w<=e or  a[w]>a[e]);
+			assert(w==e or a[w]!=a[e]);
+			assert(w>e  or a[w]<=a[e]);
+			assert(w<e  or a[w]>=a[e]);
+			assert(w!=e or a[w]==a[e]);
+			assert(a[w]+(-d)==a[e]);
+			assert(a[w]-(d)==a[e]);
 		}
 	}
-	cout<<__LINE__<<" unexpected exit"<<endl;
-	return (pointer<el<T>>)(nullptr);
+	auto s=a;
+	s.clear();
+	for (auto w=e;w!=q;--w){
+		s.push_back(w);
+	}
+	s.push_back(q);
+	reverse(s.begin(),s.end());
+	assert(a==s);
 }
 
+struct B {int d=0; B(B&&){} explicit B(int f):d(f){} };
 
-template <typename T>
-void update(el<T>*t){
-	if (!(t->z()) and !(t->x())){
-		t->s=1;
+int main(){
+	treap<long> e({1,2,3,4,5});
+	// auto q=treap<string>({"1","2","3"});
+	// auto w=q;
+	// assert(q==w);
+	// assert(vector<string>(q)==vector<string>({"1","2","3"}));
+	// assert(q.pop_back()=="3");
+	// assert(vector<string>(q)==vector<string>({"1","2"}));
+	// assert(q.pop_front()=="1");
+	// assert(vector<string>(q)==vector<string>({"2"}));
+	// assert(q.pop_front()=="2");
+	// assert(vector<string>(q)==vector<string>({}));
+	// q.push_back("2");
+	// assert(vector<string>(q)==vector<string>({"2"}));
+	// w.push_back("4");
+	// assert(vector<string>(w)==vector<string>({"1","2","3","4"}));
+	// w.push_front("0");
+	// assert(vector<string>(w)==vector<string>({"0","1","2","3","4"}));
+	// assert(w<q);
+	// assert(treap<char>(string("12"))>treap<char>(string("1")));
+	// w.insert(0,"-1");
+	// assert(vector<string>(w)==vector<string>({"-1","0","1","2","3","4"}));
+	// w.insert(w.size(),"6");
+	// assert(vector<string>(w)==vector<string>({"-1","0","1","2","3","4","6"}));
+	// w.insert(w.size()-1,"5");
+	// assert(vector<string>(w)==vector<string>({"-1","0","1","2","3","4","5","6"}));
+	// assert(vector<string>(w)==vector<string>({"-1","0","1","2","3","4","5","6"}));
+	// q.erase(0);
+	// assert(q.empty());
+	// swap(q,w);
+	// assert(w.empty());
+	// assert(vector<string>(q)==vector<string>({"-1","0","1","2","3","4","5","6"}));
+	// swap(q,w);
+	// q.insert(0,"1");
+	// q.insert(0,"0");
+	// q.insert(2,"2");
+	// swap(q,w);
+	// assert(vector<string>(q)==vector<string>({"-1","0","1","2","3","4","5","6"}));
+	// assert(vector<string>(w)==vector<string>({"0","1","2"}));
+	// check(e.begin(),e.end());
+	// assert(vector<int>(e)==vector<int>({1,2,3,4,5}));
+	// reverse(e.begin(),e.end());
+	// assert(vector<int>(e)==vector<int>({5,4,3,2,1}));
+	// sort(e.begin(),e.end());
+	// assert(vector<int>(e)==vector<int>({1,2,3,4,5}));
+	// check(e.begin()-20,e.end()+20);
+	// check(e.rbegin(),e.rend());
+	// sort(e.rbegin(),e.rend());
+	// assert(vector<int>(e)==vector<int>({5,4,3,2,1}));
+	// reverse(e.begin(),e.end());
+	// assert(vector<int>(e)==vector<int>({1,2,3,4,5}));
+	// check(e.rbegin()-20,e.rend()+20);
+	// e.clear();
+	auto y=e;
+	// auto z=e.begin();
+	// auto x=e.end();
+	// assert(z==x);
+	// for (size_t w=0;w<102400;++w){
+	// 	e.push_back(w);
+	// }
+	// for (size_t w=0;w<e.size();++w){
+	// 	assert(w==e[w]);
+	// }
+	// for (size_t w=0;w<102400;++w){
+	// 	assert(w==e.pop_front());
+	// }
+	// assert(e.empty());
+	// for (size_t w=0;w<102400;++w){
+	// 	e.push_front(w);
+	// }
+	// reverse(e.begin(),e.end());
+	// for (size_t w=0;w<e.size();++w){
+	// 	assert(w==e[w]);
+	// }
+	// reverse(e.rbegin(),e.rend());
+	// for (size_t w=0;w<102400;++w){
+	// 	assert(w==e.pop_back());
+	// }
+	// e.resize(102400);
+	// assert(e.size()==102400);
+	// e.resize(102001);
+	// assert(e.size()==102001);
+	// for (size_t w=1;w<102001;++w){
+	// 	size_t r=1;
+	// 	size_t t=w; 
+	// 	for (size_t y=1;y<102001;y*=2){
+	// 		if (y&(102001-2)){
+	// 			r*=t;
+	// 			r%=102001;
+	// 		}
+	// 		t*=t;
+	// 		t%=102001;
+	// 	}
+	// 	e[r]=w;
+	// }
+	// for (size_t w=1;w<102001;++w){
+	// 	size_t r=1;
+	// 	size_t t=w; 
+	// 	for (size_t y=1;y<102001;y*=2){
+	// 		if (y&(102001-2)){
+	// 			r*=t;
+	// 			r%=102001;
+	// 		}
+	// 		t*=t;
+	// 		t%=102001;
+	// 	}
+	// 	assert(e[w]==r);
+	// }
+	// for (size_t w=1;w<102001;++w){
+	// 	assert(w*e[w]%102001==1);
+	// }
+	// z=e.begin();
+	// x=e.end();
+	// for (size_t w=0;w<102400-102001;++w){
+	// 	e.insert(w*250+1,w);
+	// }
+	// assert(vector<int>(z,x)==vector<int>(e));
+	// y=e;
+	// for (size_t w=0;w<102400;++w){
+	// 	swap(y,e);
+	// }
+	// swap(y,y);
+	// assert(y==e);
+	// e.resize(102001);
+	// y.resize(e.size());
+	// assert(y==e);
+	// copy(w.begin(),w.end(),inserter(q,q.begin()+4));
+	// assert(vector<string>(q)==vector<string>({"-1","0","1","2","0","1","2","3","4","5","6"}));
+	// q=q;
+	// assert(vector<string>(q)==vector<string>({"-1","0","1","2","0","1","2","3","4","5","6"}));
+	// treap<B> r;
+	// r.emplace_back(1);
+	// r.emplace_front(2);
+	// auto t=r.cut_left(1);
+	// assert(t[0].d==2);
+	// assert(r[0].d==1);
+	// r.clear();
+	// t.clear();
+	// for (size_t w=0;w<102001;++w){
+	// 	r.emplace_back(w);
+	// 	t.emplace_back(w);
+	// }
+	// for (size_t w=0;w<102001;++w){
+	// 	assert(r[w].d==t[w].d);
+	// 	assert(r[w].d==w);
+	// }
+	// for (size_t w=0;w<y.size();++w){
+	// 	auto q=r.cut_right(51001);
+	// 	r.add_left(q);
+	// 	assert(r.size()==t.size());
+	// 	assert(q.empty());
+	// }
+	// for (size_t w=0;w<y.size();++w){
+	// 	auto q=t.cut_left(51001);
+	// 	t.add_right(q);
+	// 	assert(t.size()==r.size());
+	// 	assert(q.empty());
+	// }
+	// for (size_t w=0;w<102001;++w){
+	// 	assert(r[w].d==t[w].d);
+	// 	assert(r[w].d==w);
+	// }
+	// e.resize(102400);
+	// e.resize(102001);
+	// y=e;
+	// for (size_t w=0;w<y.size();++w){
+	// 	auto q=y.cut_right(51001);
+	// 	y.add_left(q);
+	// 	assert(y.size()==e.size());
+	// 	assert(q.empty());
+	// }
+	// assert(e==y);
+	// for (size_t w=0;w<y.size();++w){
+	// 	auto q=e.cut_left(51001);
+	// 	e.add_right(q);
+	// 	assert(e.size()==y.size());
+	// 	assert(q.empty());
+	// }
+	// assert(e==y);
+	// for (size_t w=0;w<y.size();++w){
+	// 	y.insert(lower_bound(y.begin(),y.begin()+w,y[w]),y[w]);
+	// 	y.erase(w+1);
+	// }
+	// sort(e.begin(),e.end());
+	// assert(e==y);
+	// assert(q.cut(4,7)==w);
+	// assert(vector<string>(q)==vector<string>({"-1","0","1","2","3","4","5","6"}));
+	// assert(vector<string>(w)==vector<string>({"0","1","2"}));
+	// sort(w.rbegin(),w.rend());
+	// assert(vector<string>(w)==vector<string>({"2","1","0"}));
+	// q.insert(q.begin(),w);
+	// assert(vector<string>(q)==vector<string>({"2","1","0","-1","0","1","2","3","4","5","6"}));
+	// assert(vector<string>(w)==vector<string>({}));
+	ic(CLOCKS_PER_SEC)
+	e.resize(10240);
+	for (size_t w=0;w<e.size();++w){
+		e[w]=w;
 	}
-	else if (!(t->z())){
-		t->s=t->x()->s+1;
+	e.pr(e.e);
+	print()
+	print()
+	print()
+	print()
+	print()
+	print()
+	vector<pair<size_t,treap<long>>> a(e.size());
+	for (size_t w=0;w<e.size();++w){
+		auto t=clock();
+		auto q=e.cut_left(w);
+		a[w].first=clock()-t;
+		e.add_left(q);
+		e[w]=a[w].first;
 	}
-	else if (!(t->x())){
-		t->s=t->z()->s+1;
-	}else{
-		t->s=t->z()->s+t->x()->s+1;
+	for (size_t w=0;e.size();++w){
+		auto q=e.cut_left(1);
+		a[w].second=move(q);
 	}
-}
-
-template<typename T>
-vector<T> to_list(pointer<el<T>> q){
-	if (!q){
-		return vector<T>();
+	assert(e.empty());
+	sort(a.begin(),a.end());
+	for (size_t w=0;w<a.size();++w){
+		e.add_right(a[w].second);
 	}
-	auto r=to_list(q->z());
-	auto t=vector({q->v});
-	r.insert(r.end(),t.begin(),t.end());
-	t=to_list(q->x());
-	r.insert(r.end(),t.begin(),t.end());
-	return r;
-}
-
-// template<typename T>
-// void print(pointer<el<T>> q){
-// 	auto w=to_list(q);
-// 	for (auto w:w){
-// 		cout<<w<<" ";
-// 	}
-// 	cout<<endl;
-// }
-
-template<typename T>
-pointer<el<T>> merge(pointer<el<T>> t1,pointer<el<T>> t2){
-
-	if (!t1){
-		return t2;
-	}
-	if (!t2){
-		return t1;
-	}
-	if (t1->w<t2->w){
-		t2->z(merge(t1,t2->z()));
-		return t2;
-	}else{
-		t1->x(merge(t1->x(),t2));
-		return t1;
-	}
-}
-
-template <typename T>
-pair<pointer<el<T>>,pointer<el<T>>> split(pointer<el<T>> t,int64_t n){
-	if (!t){
-		return {(pointer<el<T>>)(nullptr),(pointer<el<T>>)(nullptr)};
-	}
-	else if (!(t->z())){
-		if (n<1){
-			return {(pointer<el<T>>)(nullptr),t};
+	e.pr(e.e);
+	print()
+	print()
+	print()
+	print()
+	print()
+	print()
+	for (size_t w=0;w<20;++w){
+		// y=e;
+		for (size_t w=0;w<e.size();++w){
+			auto q=e.cut_left(w);
+			auto s=e.cut_left(1);
+			e.add_left(q);
+			e.add_right(s);
+			auto t=clock();
+			s=e.cut_right(1);
+			a[w].first=clock()-t;
+			q=e.cut_left(w);
+			e.add_left(s);
+			e.add_left(q);
+			e[w]=a[w].first;
 		}
-		else{
-			auto tmp=split(t->x(),n-1);
-			t->x(tmp.first);
-			auto t2=tmp.second;
-			return {t,t2};
+		// assert(y==e);
+		for (size_t w=0;e.size();++w){
+			auto q=e.cut_left(1);
+			a[w].second=move(q);
 		}
-	}else if (t->z()->s==n){
-		auto t1=t->z();
-		t->z((pointer<el<T>>)(0));
-		return {t1,t};
-	}else if (t->z()->s+1==n){
-		auto t2=t->x();
-		t->x((pointer<el<T>>)(nullptr));
-		return {t,t2};
-	}else if (t->z()->s+1<n){
-		auto tmp=split(t->x(),n-t->z()->s-1);
-		t->x(tmp.first);
-		auto t2=tmp.second;
-		return {t,t2};
-	}else if (t->z()->s>n){
-		auto tmp=split(t->z(),n);
-		auto t1=tmp.first;
-		t->z(tmp.second);
-		return {t1,t};
-	}
-	cout<<__LINE__<<" unexpected exit"<<endl;
-	return {(pointer<el<T>>)(0),(pointer<el<T>>)(0)};
-}
-
-
-template <typename T>
-struct treap{
-	pointer<el<T>> e=nullptr;
-	template <typename y=vector<T>>
-	treap(y l=vector<T>()){
-		for (auto w:l){
-			e=merge(e,pointer(el(w)));
+		assert(e.empty());
+		sort(a.begin(),a.end());
+		for (size_t w=0;w<a.size();++w){
+			e.add_right(a[w].second);
 		}
+		e.pr(e.e);
+		print()
+		print()
+		print()
+		print()
+		print()
 	}
-	treap(int64_t l){
-		for (int64_t w=0;w<l;++w){
-			e=merge(e,pointerel(T()));
-		}
-	}
-	treap(const treap&l){
-		for (int64_t w=0;w<l.size();++w){
-			e=merge(e,pointer(el(l[w])));
-		}
-	}
-	void operator=(const treap&l){
-		e=nullptr;
-		for (int64_t w=0;w<l.size();++w){
-			e=merge(e,pointer(el(l[w])));
-		}
-	}
-	
-	int64_t size()const{
-		return e?e->s:0;
-	}
-
-	T&operator[](int64_t n){
-		if (n<0){
-			n+=this->size();
-		}
-		return get_by_index(e,n)->v;
-	}
-
-	T&operator[](int64_t n)const{
-		if (n<0){
-			n+=this->size();
-		}
-		return get_by_index(e,n)->v;
-	}
-
-	friend int64_t __cmp__(treap s,treap d){
-		int64_t min_len=s.size()>d.size()?d.size():s.size();
-		for (int64_t w=0;w<min_len;++w){
-			if (s[w]<d[w]){
-				return -1;
-			}
-			if (s[w]>d[w]){
-				return -1;
-			}
-		}
-		if (s.size()>d.size()){
-			return 1;
-		}
-		if (s.size()<d.size()){
-			return -1;
-		}
-		return 0;
-	}
-
-	bool operator<(const treap o)const{
-		return __cmp__(*this,o)<0;
-	}
-
-	void operator+=(treap o){
-		e=merge(e,o.e);
-	}
-
-	void clear(){
-		e->clear();
-	}
-
-	void push_back(T a){
-		e=merge(e,pointer(el(a)));
-	}
-
-
-	treap<T> cut(int64_t n){
-		auto tmp=split(this->e,n);
-		e=tmp.second;
-		treap<T> res;
-		res.e=tmp.first;
-		return res;
-	}
-
-	friend ostream&operator<<(ostream&q,treap w){
-		auto r=to_list(w.e);
-		q<<'[';
-		int64_t c=0;
-		for (auto w:r){
-			if (c){
-				q<<", "<<w;
-			}else{
-				q<<w;
-				c=1;
-			}
-		}
-		q<<']';
-		return q;
-	}
-};
-
-
-signed main(){
-	cout<<__LINE__<<endl;
-	auto q=treap<int64_t>();
-	cout<<__LINE__<<endl;
-	q.push_back(1);
-	q.push_back(2);
-	q.push_back(3);
-	auto w=q;
-	q.push_back(1);
-	q.push_back(2);
-	q.push_back(3);
-	cout<<w<<endl;
-	cout<<q<<endl;
-	cout<<endl;
 }
---
+++ treap.hpp
@@ -0,0 +1,709 @@
+#include <inttypes.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <type_traits>
+// #include <assert.h>
+
+template <typename T>
+class treap{
+	struct el{
+		T v;
+		int64_t w;
+		el* _z=nullptr;
+		el* _x=nullptr;
+		el* _p=nullptr;
+		int64_t s=1;
+		int64_t d=1;
+		int r=0;
+		template<typename...Y>
+		el(const Y&..._v):v(_v...),w(rand()){}
+
+		el*& z(){
+			return _z;
+		}
+		template<typename y>
+		void z(y&& q){
+			_z=std::move(q);
+			update();
+		}
+
+		el*& x(){
+			return _x;
+		}
+		template<typename y>
+		void x(y&& q){
+			_x=std::move(q);
+			update();
+		}
+		void make(){
+			if (r){
+				auto t=z();
+				z(x());
+				x(t);
+				if (z()){
+					z()->r=1;
+				}
+				if (x()){
+					x()->r=1;
+				}
+				r=0;
+			}
+		}
+		void update(){
+			auto t=this;
+			if (t->z()==nullptr and t->x()==nullptr){
+				t->s=1;
+				t->d=1;
+			}
+			else if (t->z()==nullptr){
+				t->s=t->x()->s+1;
+				t->x()->_p=t;
+				t->d=t->x()->d+1;
+			}
+			else if (t->x()==nullptr){
+				t->s=t->z()->s+1;
+				t->z()->_p=t;
+				t->d=t->z()->d+1;
+			}else{
+				t->s=t->z()->s+t->x()->s+1;
+				t->x()->_p=t;
+				t->z()->_p=t;
+				t->d=(t->z()->d>t->x()->d?t->z()->d:t->x()->d)+1;
+			}
+			if (t->d>256){
+				std::cerr<<"bamboo!! "<<t->d<<std::endl;
+				exit(0);
+			}
+		}
+		int64_t nz_find_index(){
+			auto q=this;
+			auto w=q->_p;
+			if (w==nullptr){
+				return el_size(q->z());
+			}else if (w->z()==q){
+				return w->nz_find_index()-el_size(q->x())-1;
+			}else if (w->x()==q){
+				return w->nz_find_index()+el_size(q->z())+1;
+			}
+			assert(0);
+			return 0;
+		}
+	};
+
+	static int64_t el_size(el*&s){
+		return s?s->s:0;
+	}
+
+	static int64_t find_index(el*const&s){
+		return s?s->nz_find_index():0;
+	}
+
+	static el* get_by_index(el*const&s,int64_t n){
+		if (!s){
+			return nullptr;
+		}
+		if (s->z()==nullptr){
+			if (n==0){
+				return s;
+			}
+			else{
+				return get_by_index(s->x(),n-1);
+			}
+		}
+		else{
+			if (n==s->z()->s){
+				return s;
+			}
+			if (n<s->z()->s){
+				return get_by_index(s->z(),n);
+			}
+			if (n>s->z()->s){
+				return get_by_index(s->x(),n-s->z()->s-1);
+			}
+		}
+		assert(0);
+		return (el*)(nullptr);
+	}
+
+	static void to_list(el* q,std::vector<T>&a){
+		if (!q){
+			return;
+		}
+		to_list(q->z(),a);
+		a.push_back(q->v);
+		to_list(q->x(),a);
+		return;
+	}
+
+	static auto pr(el* q,int64_t n=0){
+		if (!q){
+			return;
+		}
+		pr(q->z(),n+1);
+		for (auto w=0;w<n;++w){
+			putchar('|');
+		}
+		std::cout<<q<<' '<<q->v<<' '<<q->w<<std::endl;
+		pr(q->x(),n+1);
+	}
+
+	static el* copy(el* q){
+		if (!q){
+			return nullptr;
+		}
+		el*w=new el(q->v);
+		w=merge(copy(q->z()),w);
+		w=merge(w,copy(q->x()));
+		return w;
+	}
+
+	static void del(el* q){
+		if (!q){
+			return;
+		}
+		del(q->z());
+		del(q->x());
+		delete q;
+	}
+
+	static el* merge(el* t1,el* t2){
+		if (!t1){
+			return t2;
+		}
+		if (!t2){
+			return t1;
+		}
+		if (t1->w<t2->w){
+			t2->z(merge(t1,t2->z()));
+			return t2;
+		}else{
+			t1->x(merge(t1->x(),t2));
+			return t1;
+		}
+	}
+
+	static std::pair<el*,el*> split(el* t,int64_t n){
+		if (!t){
+			return {(el*)(nullptr),(el*)(nullptr)};
+		}else if (t->z()==nullptr){
+			if (n<1){
+				return {(el*)(nullptr),t};
+			}else{
+				auto tmp=split(t->x(),n-1);
+				t->x(tmp.first);
+				auto t2=tmp.second;
+				if(t2){
+					t2->_p=nullptr;
+				}
+				return {t,t2};
+			}
+		}else if (t->z()->s==n){
+			auto t1=t->z();
+			t->z((el*)(nullptr));
+			if (t1){
+				t1->_p=nullptr;
+			}
+			return {t1,t};
+		}else if (t->z()->s+1==n){
+			auto t2=t->x();
+			t->x((el*)(nullptr));
+			if (t2){
+				t2->_p=nullptr;
+			}
+			return {t,t2};
+		}else if (t->z()->s+1<n){
+			auto tmp=split(t->x(),n-t->z()->s-1);
+			t->x(tmp.first);
+			auto t2=tmp.second;
+			if (t2){
+				t2->_p=nullptr;
+			}
+			return {t,t2};
+		}else if (t->z()->s>n){
+			auto tmp=split(t->z(),n);
+			auto t1=tmp.first;
+			t->z(tmp.second);
+			if (t1){
+				t1->_p=nullptr;
+			}
+			return {t1,t};
+		}
+		assert(0);
+		return {(el*)(0),(el*)(0)};
+	}
+
+	static el* add(el*q,int64_t n){
+		if (!q){
+			return nullptr;
+		}
+		if (n==0){
+			return q;
+		}
+		if (n>0){
+			if (el_size(q->x())>=n){
+				return get_by_index(q->x(),n-1);
+			}else{
+				auto w=q->_p;
+				if (w==nullptr){
+					return nullptr;
+				}else if (w->z()==q){
+					return add(q->_p,-el_size(q->x())-1+n);
+				}else if (w->x()==q){
+					return add(q->_p,el_size(q->z())+1+n);
+				}
+				assert(0);
+			}
+		}else if (n<0){
+			if (-el_size(q->z())<=n){
+				return get_by_index(q->z(),el_size(q->z())+n);
+			}else{
+				auto w=q->_p;
+				if (w==nullptr){
+					return nullptr;
+				}else if (w->z()==q){
+					return add(q->_p,-el_size(q->x())-1+n);
+				}else if (w->x()==q){
+					return add(q->_p,el_size(q->z())+1+n);
+				}
+				assert(0);
+			}
+		}
+		assert(0);
+		return (el*)(0);
+	}
+
+	el* e=nullptr;
+public:
+	template <typename y=std::initializer_list<T>>
+	treap(const y&l=std::initializer_list<T>(),
+		std::enable_if_t<
+			std::is_same_v<
+				typename y::value_type,T
+			> and std::is_copy_constructible_v<T>
+			and std::is_same_v<decltype(l.begin()!=l.end()),bool>
+			,int> =0){
+		for (auto&w:l){
+			e=merge(e,new auto (el(w)));
+		}
+	}
+	template <typename y=int>
+	treap(y=0,
+		std::enable_if_t<!std::is_copy_constructible_v<T>
+			,y> =0){
+	}
+	template<typename y=int64_t>
+	treap(std::enable_if_t<
+			std::is_trivially_constructible_v<T>
+		,y> l){
+		for (int64_t w=0;w<l;++w){
+			e=merge(e,new auto (el(T())));
+		}
+	}
+	treap(const treap&l){
+		e=copy(l.e);
+	}
+	treap(treap&&l){
+		std::swap(e,l.e);
+	}
+	auto&operator=(treap&&l){
+		std::swap(e,l.e);
+		return *this;
+	}
+	auto&operator=(const treap&l){
+		if (e!=l.e){
+			del(e);
+			e=copy(l.e);
+		}
+		return *this;
+	}
+	~treap(){
+		del(e);
+	}
+	int64_t size()const{
+		return e?e->s:0;
+	}
+
+	T&operator[](int64_t n){
+		if (n<0){
+			n+=this->size();
+		}
+		assert(n<size());
+		assert(n>=0);
+		return get_by_index(e,n)->v;
+	}
+
+	const T&operator[](int64_t n)const{
+		if (n<0){
+			n+=this->size();
+		}
+		assert(n<size());
+		assert(n>=0);
+		return get_by_index(e,n)->v;
+	}
+private:
+	int64_t __cmp__(const treap&d)const{
+		auto&s=*this;
+		int64_t min_len=d.size()<s.size()?d.size():s.size();
+		for (int64_t w=0;w<min_len;++w){
+			if (s[w]<d[w]){
+				return -1;
+			}
+			if (s[w]>d[w]){
+				return -1;
+			}
+		}
+		if (s.size()>d.size()){
+			return 1;
+		}
+		if (s.size()<d.size()){
+			return -1;
+		}
+		return 0;
+	}
+public:
+	void add_left(treap<T>&q){
+		e=merge(q.e,e);
+		q.e=nullptr;
+	}
+	void add_right(treap<T>&q){
+		e=merge(e,q.e);
+		q.e=nullptr;
+	}
+	treap<T> cut_left(int64_t n){
+		assert(0<=n and n<=size());
+		auto s=size();
+		auto tmp=split(e,n);
+		assert(el_size(tmp.first)+el_size(tmp.second)==s);
+		e=tmp.second;
+		auto r=treap<T>();
+		r.e=tmp.first;
+		return r;
+	}
+	treap<T> cut_right(int64_t n){
+		assert(0<=n and n<=size());
+		auto s=size();
+		auto tmp=split(e,size()-n);
+		assert(el_size(tmp.first)+el_size(tmp.second)==s);
+		e=tmp.first;
+		auto r=treap<T>();
+		r.e=tmp.second;
+		return r;
+	}
+	treap<T> cut(int64_t l,int64_t r){
+		assert(0<=l and l<=r and  r<=size());
+		auto q=cut_left(l);
+		auto w=cut_left(r-l);
+		add_left(q);
+		return w;
+	}
+	void push_back(const T&a){
+		e=merge(e,new auto(el(a)));
+	}
+	void push_front(const T&a){
+		e=merge(new auto(el(a)),e);
+	}
+	template<typename...Y>
+	void emplace_back(const Y&...a){
+		e=merge(e,new auto(el(a...)));
+	}
+	template<typename...Y>
+	void emplace_front(const Y&...a){
+		e=merge(new auto(el(a...)),e);
+	}
+	T pop_back(){
+		auto q=cut_right(1);
+		return q[0];
+	}
+	T pop_front(){
+		auto q=cut_left(1);
+		return q[0];
+	}
+	operator std::vector<T>()const{
+		auto q=std::vector<T>();
+		to_list(e,q);
+		return q;
+	}
+	friend std::ostream&operator<<(std::ostream&q,const treap&w){
+		auto r=std::vector<T>(w);
+		q<<'[';
+		int64_t c=0;
+		for (auto w:r){
+			if (c){
+				q<<", "<<w;
+			}else{
+				q<<w;
+				c=1;
+			}
+		}
+		q<<']';
+		return q;
+	}
+	void clear(){
+		del(e);
+		e=nullptr;
+	}
+	bool empty(){
+		return !e;
+	}
+	T&front(){
+		return (*this)[0];
+	}
+	const T&front()const{
+		return (*this)[0];
+	}
+	T&back(){
+		return (*this)[this->size()-1];
+	}
+	const T&back()const{
+		return (*this)[this->size()-1];
+	}
+	struct iter {
+		using difference_type = std::ptrdiff_t;
+		using value_type = T;
+		using pointer = T*;
+		using reference = T&;
+		using iterator_category = std::random_access_iterator_tag;
+		template<typename Y>
+		using is_iterator=Y;
+		using original_type=treap<T>;
+		el*e=nullptr;
+		int64_t o=0;
+		int64_t d=1;
+		T&operator*(){
+			return e->v;
+		}
+		auto&operator--(){
+			return *this-=1;
+		}
+		auto&operator++(){
+			return *this+=1;
+		}
+		auto operator--(int){
+			auto q=*this;
+			*this-=1;
+			return q;
+		}
+		auto operator++(int){
+			auto q=*this;
+			*this+=1;
+			return q;
+		}
+		auto&operator+=(long w){
+			w*=d;
+			w+=o;
+			if (!e){
+				return *this;
+			}
+			while (1){
+				auto s=w*2;
+				auto q=e;
+				q=nullptr;
+				while (!q){
+					s/=2;
+					q=add(e,s);
+				}
+				if (q==e){
+					break;
+				}
+				e=q;
+				w-=s;
+			}
+			o=w;
+			return *this;
+		}
+		auto&operator-=(long w){
+			return *this+=(-w);
+		}
+		auto&operator[](long q){
+			return *(*this+q);
+		}
+		auto& operator=(const iter&q){
+			e=q.e;
+			o=q.o;
+			d=q.d;
+			return *this;
+		}
+	};
+	auto begin(){
+		if (!e){
+			return iter{e,0};
+		}
+		auto q=e;
+		while (q->z()){
+			q=q->z();
+		}
+		return iter{q,0};
+	}
+	auto end(){
+		if (!e){
+			return iter{e,0};
+		}
+		auto q=e;
+		while (q->x()){
+			q=q->x();
+		}
+		return iter{q,1};
+	}
+	auto rbegin(){
+		auto q=end()-1;
+		q.d=-1;
+		return q;
+	}
+	auto rend(){
+		auto q=begin()-1;
+		q.d=-1;
+		return q;
+	}
+	using iterator=iter;
+	using value_type=T;
+	auto insert(int64_t n,const T&q){
+		auto w=cut_left(n);
+		w.push_back(q);
+		add_left(w);
+		return n;
+	}
+	auto erase(int64_t n){
+		auto w=cut_left(n);
+		pop_front();
+		add_left(w);
+		return n;
+	}
+	template<typename TT>
+	auto insert(TT n,const T&q)->typename TT::template is_iterator<iter>{
+		return begin()+insert(find_index(n.e),q);
+	}
+	template<typename TT>
+	auto erase(TT n,const T&q)->typename TT::template is_iterator<iter>{
+		return begin()+erase(find_index(n.e),q);
+	}
+	auto insert(int64_t n,treap<T>&q){
+		auto w=cut_left(n);
+		add_left(q);
+		add_left(w);
+		return n;
+	}
+	template<typename TT>
+	auto insert(TT n,treap<T>&q)->typename TT::template is_iterator<iter>{
+		return begin()+insert(find_index(n.e),q);
+	}
+	void resize(int64_t n){
+		for (size_t w=size();w<n;++w){
+			push_back(T());
+		}
+		cut_right(size()-n);
+	}
+	template <typename Y>
+	friend bool operator< (const treap<Y>&q,const treap<Y>&w);
+	template <typename Y>
+	friend bool operator> (const treap<Y>&q,const treap<Y>&w);
+	template <typename Y>
+	friend bool operator==(const treap<Y>&q,const treap<Y>&w);
+	template <typename Y>
+	friend bool operator<=(const treap<Y>&q,const treap<Y>&w);
+	template <typename Y>
+	friend bool operator>=(const treap<Y>&q,const treap<Y>&w);
+	template <typename Y>
+	friend bool operator!=(const treap<Y>&q,const treap<Y>&w);
+	template <typename Y>
+	friend void swap(treap&q,treap&e);
+};
+
+template <typename T>
+bool operator<(const treap<T>&q,const treap<T>&w){
+	return q.__cmp__(w)<0;
+}
+
+template <typename T>
+bool operator>(const treap<T>&q,const treap<T>&w){
+	return q.__cmp__(w)>0;
+}
+
+template <typename T>
+bool operator==(const treap<T>&q,const treap<T>&w){
+	return q.__cmp__(w)==0;
+}
+
+template <typename T>
+bool operator<=(const treap<T>&q,const treap<T>&w){
+	return q.__cmp__(w)<=0;
+}
+
+template <typename T>
+bool operator>=(const treap<T>&q,const treap<T>&w){
+	return q.__cmp__(w)>=0;
+}
+
+template <typename T>
+bool operator!=(const treap<T>&q,const treap<T>&w){
+	return q.__cmp__(w)!=0;
+}
+
+template <typename T>
+void swap(treap<T>&q,treap<T>&e){
+	auto w=q.cut_left(q.size());
+	q.add_left(e);
+	e.add_left(w);
+}
+
+template<typename TT>
+auto operator-(TT q,TT w)->typename TT::template is_iterator<ptrdiff_t>{
+	assert(q.d==w.d);
+	return ((q.e)->nz_find_index()-(w.e)->nz_find_index()+q.o-w.o)*q.d;
+}
+
+template<typename TT>
+auto operator+(TT q,long w)->typename TT::template is_iterator<TT>{
+	auto e=q;
+	e+=w;
+	return e;
+}
+
+template<typename TT>
+auto operator-(TT q,long w)->typename TT::template is_iterator<TT>{
+	auto e=q;
+	e-=w;
+	return e;
+}
+
+template<typename TT>
+auto operator+(long w,TT q)->typename TT::template is_iterator<TT>{
+	auto e=q;
+	e+=w;
+	return e;
+}
+
+template<typename TT>
+auto operator>=(TT q,TT w)->typename TT::template is_iterator<bool>{
+	return q-w>=0;
+}
+
+template<typename TT>
+auto operator<=(TT q,TT w)->typename TT::template is_iterator<bool>{
+	return q-w<=0;
+}
+
+template<typename TT>
+auto operator<(TT q,TT w)->typename TT::template is_iterator<bool>{
+	return q-w<0;
+}
+
+template<typename TT>
+auto operator>(TT q,TT w)->typename TT::template is_iterator<bool>{
+	return q-w>0;
+}
+
+template<typename TT>
+auto operator==(TT q,TT w)->typename TT::template is_iterator<bool>{
+	q+=0;
+	w+=0;
+	return q.e==w.e and q.o==w.o;
+}
+
+template<typename TT>
+auto operator!=(TT q,TT w)->typename TT::template is_iterator<bool>{
+	q+=0;
+	w+=0;
+	return q.e!=w.e or q.o!=w.o;
+}
