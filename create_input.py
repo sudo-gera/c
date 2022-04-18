909e14fcbdae00b4799e9b2ea21836b497b95044
sudo-gera <gera.tatarinov.999@gmail.com>
2022-04-18 18:21:09 +0300

    *interesting commit message*

--- create_input.py
+++ create_input.py
@@ -8,6 +8,14 @@ def rand(q=2**64,e=None):
 	assert q
 	return randint(e,q-1)
 
-for w in range(rand(100)):
-	for e in range(rand(100)):
-		print(rand(10))
+# for w in range(rand(100)):
+# 	for e in range(rand(100)):
+# 		print(rand(10))
+
+n=rand(100)+1
+print(n)
+a=[]
+for w in range(n):
+	a.append(rand(100))
+a.sort()
+print(*a)
