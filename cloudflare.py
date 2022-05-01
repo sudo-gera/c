77062160a2010c23ebf2ef79d0bd539f59e32f04
sudo-gera <gera.tatarinov.999@gmail.com>
2022-05-01 20:23:51 +0300

    *interesting commit message*

--- cloudflare.py
+++ cloudflare.py
@@ -1,3 +1,24 @@
-from subprocess import run
-run('adb connect 192.168.49.1:53142'.split())
-run('adb shell cmd statusbar click-tile com.cloudflare.onedotonedotonedotone/com.cloudflare.app.domain.quicksettingstile.QuickSettingsTileService'.split())
+from h import *
+try:
+	a=urlopen('https://api.ipify.org').read()
+	a=loads(urlopen('http://ip-api.com/batch',data=b'[{"query":"'+a+b'","fields":"isp"}]').read().decode())
+except KeyboardInterrupt:
+	a=''
+	print()
+except:
+	a=''
+if a:
+	a=a[0]['isp']
+	print('current provider is:',a)
+	try:
+		a=input('switch? ')
+	except KeyboardInterrupt:
+		print()
+	except:
+		a='n'
+	if a.lower() in ['y','']:
+		print('switching...')
+		system('adb connect 192.168.49.1:53142')
+		system('adb shell cmd statusbar click-tile com.cloudflare.onedotonedotonedotone/com.cloudflare.app.domain.quicksettingstile.QuickSettingsTileService')
+	else:
+		print('aborted.')
--- test.cpp
+++ test.cpp
@@ -10,41 +10,142 @@
 #include <string.h>
 #include <tgmath.h>
 
+struct array_s {
+	size_t mem_size;
+	size_t el_count;
+	char data[0];
+};
 
-int main(){
-	int*a=(int*)calloc(1024,sizeof(int));
-	#define left --a;
-	#define right ++a;
-	#define inc ++*a;
-	#define dec --*a;
-	#define get getchar(*a);
-	#define put putchar(*a);
-	#define begin while(*a){
-	#define end }
+static inline size_t len(void *a) {
+	if (a == NULL) {
+		return 0;
+	}
+	return ((struct array_s *)(a))[-1].el_count;
+}
 
-	inc
-	inc
-	inc
-	begin
-		right
-		inc
-		inc
-		inc
-		inc
-		left
-		dec
-	end
-	right
-	begin
-		right
-		inc
-		inc
-		inc
-		inc
-		left
-		dec
-	end
-	right
-	put
+static inline void del(void *a) {
+	if (a != NULL) {
+		free(((struct array_s *)(a)) - 1);
+	}
+}
 
+static inline struct array_s *resize_f(struct array_s **vp, size_t el_size,
+																			 size_t n) {
+	if (*vp == NULL) {
+		*vp = (struct array_s *)calloc(1, sizeof(struct array_s));
+		*vp += 1;
+	}
+	struct array_s *a = *vp - 1;
+	assert(a->data == *(char **)vp);
+	if (a->mem_size < n + 1) {
+		size_t cur_size = a->mem_size * el_size;
+		size_t new_size;
+		if (a->mem_size * 2 > n) {
+			new_size = a->mem_size * 2 * el_size;
+		} else {
+			new_size = (n + 1) * el_size;
+		}
+		a = (struct array_s *)realloc(a, sizeof(struct array_s) + new_size);
+		memset(a->data + a->mem_size * el_size, 0, new_size - cur_size);
+		a->mem_size = new_size / el_size;
+	}
+	a->el_count = n;
+	*vp = a + 1;
+	return a + 1;
+}
+/////// resize(a, n) is resize_f(&a, sizeof(a[0]), n)
+#define resize(a, ...)                                                         \
+	(resize_f((struct array_s **)&(a), sizeof((a)[0]), (__VA_ARGS__)))
+#define append(a, ...)                                                         \
+	(resize((a), len(a) + 1), (a)[len(a) - 1] = (__VA_ARGS__))
+#define pop(a) (resize((a), len(a) - 1), (a)[len(a)])
+#define back(a) (a[len(a) - 1])
+
+static inline int64_t getint() {
+	int sign = 1;
+	int c;
+	size_t res = 0;
+	while (c = getchar_unlocked(), isspace(c))
+		;
+	if (c == '-') {
+		sign = -1;
+	} else {
+		res = c - '0';
+	}
+	while (c = getchar_unlocked(), isdigit(c)) {
+		res *= 10;
+		res += c - '0';
+	}
+	return (int64_t)(res)*sign;
+}
+
+static inline void putint(uint64_t out) {
+	if (out > (1LLU << 63) - 1) {
+		putchar_unlocked('-');
+		out = 1 + ~out;
+	}
+	char data[44];
+	char *dend = data;
+	while (out) {
+		*++dend = (unsigned)('0') + out % 10;
+		out /= 10;
+	}
+	if (dend == data) {
+		putchar_unlocked('0');
+	}
+	for (; dend != data; --dend) {
+		putchar_unlocked(*dend);
+	}
+}
+
+static inline void print(uint64_t out) {
+	putint(out);
+	putchar('\n');
+}
+
+static inline void write(uint64_t out) {
+	putint(out);
+	putchar(' ');
+}
+
+#define min(a, s) ((a) < (s) ? (a) : (s))
+#define max(a, s) ((a) > (s) ? (a) : (s))
+
+typedef int (*cmp_f_t)(const void *, const void *);
+
+///////////////////////////////////////////////////end of lib
+
+#define stack_load																\
+	static char *__stack_start = 0;												\
+	static long __stack_diff = 0;												\
+	char *__stack_curr = (char *)&__stack_curr;									\
+	if (!__stack_start) {														\
+		__stack_start = (char *)&__stack_curr;									\
+	} else if (!__stack_diff and __stack_start != (char *)&__stack_curr) {		\
+		__stack_diff = __stack_start - (char *)&__stack_curr;					\
+	}																			\
+	long stack_len =															\
+			__stack_diff ? (__stack_start - __stack_curr) / __stack_diff + 1 : 1;
+
+#define stack_get(v, n)											\
+	(*(decltype(v) *)((char *)&(v) +							\
+		((n) % stack_len + stack_len) % stack_len * __stack_diff))
+
+void run(size_t c) {
+	stack_load;
+	for (long w = 0; w < stack_len; w++) {
+		write(stack_get(c, -1 - w));
+	}
+	putchar(10);
+	if (c) {
+		run(c - 1);
+	}
+}
+
+int main() {
+	run(0);
+	run(1);
+	run(2);
+	run(1);
+	run(0);
 }
