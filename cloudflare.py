654671d0147c45f295aef33df222a21689631fac
sudo-gera <gera.tatarinov.999@gmail.com>
2022-05-02 16:33:38 +0300

    *interesting commit message*

--- cloudflare.py
+++ cloudflare.py
@@ -1,21 +1,23 @@
 from h import *
 try:
-	a=urlopen('https://api.ipify.org').read()
-	a=loads(urlopen('http://ip-api.com/batch',data=b'[{"query":"'+a+b'","fields":"isp"}]').read().decode())
+	a=urlopen('https://api.ipify.org',timeout=1).read()
+	a=loads(urlopen('http://ip-api.com/batch',data=b'[{"query":"'+a+b'","fields":"isp"}]',timeout=1).read().decode())
 except KeyboardInterrupt:
 	a=''
 	print()
 except:
-	a=''
+	a='y'
 if a:
-	a=a[0]['isp']
-	print('current provider is:',a)
-	try:
-		a=input('switch? ')
-	except KeyboardInterrupt:
-		print()
-	except:
-		a='n'
+	if type(a)==dict:
+		a=a[0]['isp']
+		print('current provider is:',a)
+		try:
+			a=input('switch? ')
+		except KeyboardInterrupt:
+			a='n'
+			print()
+		except:
+			a='n'
 	if a.lower() in ['y','']:
 		print('switching...')
 		system('adb connect 192.168.49.1:53142')
