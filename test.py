6e769a2808debf1f8b1e53c561a936fc97ffd20c
sudo-gera <gera.tatarinov.999@gmail.com>
2022-05-15 00:01:31 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -2,13 +2,10 @@ from h import *
 
 o=Fraction(1)
 
-def run(a,s,d,f):
-	if d+f>14:
-		s=0
-		s+=run(a+1,s,d-1,f)*o*d/(d+f)
-		s+=run(a,s+1,d,f-1)*o*f/(d+f)
-		return s
-	else:
-		return o*a/(a+s)
-
-print(run(26,5,15,4))
+r=0
+for w in range(100):
+	print(w+1,100-w,w,99-w)
+	r+=(w)*(100-w)+(w+1)*(99-w)
+	print(w+1,99-w,w+1,99-w)
+	r+=(w+1)*(99-w)+(w+1)*(99-w)
+print(r)
