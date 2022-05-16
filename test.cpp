cf382abddf76d612187f926cc790407038e21e04
sudo-gera <gera.tatarinov.999@gmail.com>
2022-05-16 15:55:33 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,26 +1,47 @@
-#include <stdint.h>
-#include <assert.h>
-#define bisect(b, e, ...)														\
-	({																			\
-		int64_t __begin = b;													\
-		int64_t __end = e;														\
-		assert(__begin <= __end);												\
-		while (__begin + 1 < __end) {											\
-			int64_t __center = (__begin + __end) / 2;							\
-			if (__VA_ARGS__) {													\
-				__end = __center;												\
-			} else {															\
-				__begin = __center;												\
-			}																	\
-		}																		\
-		int64_t __center = __begin;												\
-		if (__begin != __end and (__VA_ARGS__)) {								\
-			__end = __center;													\
-		}																		\
-		__end;																	\
-	})
+#include <bits/stdc++.h>
+using namespace std;
 
+template<uint64_t n>
+struct sized{
+	char a[n];
+};
+
+int a[20][20];
 
 int main(){
-	bisect((1,2),1,2);
+
+	for (uint64_t w=0;w<20;++w){
+		for (uint64_t e=0;e<20;++e){
+			a[w][e]=rand()%10;
+		}
+	}
+
+	for (uint64_t w=0;w<20;++w){
+		for (uint64_t e=0;e<20;++e){
+			write(a[w][e]);
+		}
+		putchar('\n');
+	}
+
+		putchar('\n');
+
+	// ic(a,a+1,&a[0][0],&a[0][1],&a[1][0],&a[1][1])
+	sort((sized<80>*)(a),(sized<80>*)(a+20),
+		[&](const sized<80>&q,const sized<80>&e){
+			int*qp=(int*)(&q);
+			int*ep=(int*)(&e);
+			if (memcmp(qp,ep,80)<0){
+				return 1;
+			}
+			return 0;
+		}
+		);
+
+	for (uint64_t w=0;w<20;++w){
+		for (uint64_t e=0;e<20;++e){
+			write(a[w][e]);
+		}
+		putchar('\n');
+	}
+
 }
