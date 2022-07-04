3d715831ff1844766f1d56d8890f20b4c715a7c6
sudo-gera <gera.tatarinov.999@gmail.com>
2022-07-04 15:04:00 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,8 +1,21 @@
+template<typename T>
+auto _run(const T&f){
+	auto [q,w,e,r]=f;
+	return q;
+}
 
 
+template<typename T>
+auto run(const T&g)->decltype(auto[q,w,e,r]=g){
+	return 0;
+}
 
+struct a{
+	int a,s,d,f;
+};
 
 
 int main(){
+	run(a());
 
 }
