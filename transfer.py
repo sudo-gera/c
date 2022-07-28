92f23391ac5ce7781feb73805dec7475df1aa416
sudo-gera <gera.tatarinov.999@gmail.com>
2022-07-28 10:00:45 +0300

    *interesting commit message*

--- transfer.py
+++ transfer.py
@@ -1,161 +1,142 @@
-import json as json
-import inspect as inspect
-from fractions import Fraction as Fraction
-from functools import cache as cache
-import base64 as base64
-from queue import Queue
-from functools import reduce
-from random import shuffle
-
-@cache
-def pickledumps(t):
-    import pickle
-    return (t.__name__,base64.b64encode(pickle.dumps(t)).decode())
 
 
-@cache
-def pickleloads(a):
-    n,t=a
-    for w in loaders:
-        if pickledumps(w)==a:
-            return w
-    raise TypeError\
-        (f'no loader for {n}.')
+def main():
+    import json
+    import inspect
+    from fractions import Fraction
+    from functools import cache
+    import base64
+    from functools import reduce
 
-standart_types=[int,str,type(None),bool]
-standart_containers=[list,set,frozenset,tuple]
-loaders={w:None for w in standart_containers+standart_types}
-dumpers={w:None for w in standart_containers+standart_types}
+    # @cache
+    def pickledumps(t):
+        n=t.__name__
+        if t not in dumpers:
+            raise TypeError\
+                (f'no dumper for type \'{n}\'.')
+        import pickle
+        r=list(pickle.dumps(t))
+        s=list(pickle.dumps(int))
+        s+=[0]*(len(r)-len(s))
+        s=s[:len(r)]
+        r=[(q-w)%256 for q,w in zip(r,s)]
+        e=[]
+        for w in r:
+            if e and w==e[-1][0]:
+                e[-1][1]+=1
+            else:
+                e.append([w,1])
+        r=e
+        return (n,r)
 
-def dumps(x):
-    ts=[]
-    d={}
-    q=Queue()
-    q.put(x)
-    r=id(x)
-    while not q.empty():
-        a=q.get()
+
+    # @cache
+    def pickleloads(a):
+        n,t=a
+        for w in loaders:
+            if pickledumps(w)==a:
+                return w
+        raise TypeError\
+            (f'no loader for type \'{n}\'.')
+
+    standart_types=[int,str,type(None),bool]
+    standart_containers=[list,set,frozenset,tuple]
+    loaders={w:None for w in standart_containers+standart_types}
+    dumpers={w:None for w in standart_containers+standart_types}
+
+    def jdumps(a):
         t=type(a)
-        ts.append(t)
-        if id(a) in d:
-            continue
-        d[id(a)]=[a]
+        p=pickledumps(t)
         if t in standart_containers:
-            for w in a:
-                q.put(w)
-            d[id(a)]+=[t]+[id(w) for w in a]
-        elif t in standart_types:
-            d[id(a)]+=[t,a]
-        elif t not in dumpers:
-                raise TypeError\
-                    (f'no dumper for type {t._name__}.')
-        else:
-            _d=dumpers[t](a)
-            d[id(a)]+=[t,id(_d)]
-            q.put(_d)
-    ts=list(set(ts))
-    td=dict(zip(*list(zip(*enumerate(ts)))[::-1]))
-    dl=list(d)
-    dl=[r]+[w for w in dl if w!=r]
-    nd=dict(zip(*list(zip(*enumerate(dl)))[::-1]))
-    for w in d:
-        if d[w][1] in standart_types:
-            d[w]=[td[d[w][1]]]+d[w][2:]
-        else:
-            d[w]=[td[d[w][1]]]+[nd[e] for e in d[w][2:]]
-    d={nd[w]:d[w] for w in d}
-    d=sorted(list(d.items()))
-    d=list(list(zip(*d))[1])
-    ts=[pickledumps(w) for w in ts]
-    return json.dumps([ts,d])
+            y=[jdumps(w) for w in a]
+            return [[t]+sum([w[0] for w in y],[]),t,[w[1:] for w in y]]
+        if t in standart_types:
+            return [[t],t,a]
+        d=dumpers[t](a)
+        d=jdumps(d)
+        return [[t]+d[0],t,d[1:]]
 
-def __loads(a,d):
-    q,t,a=a[0],a[1],a[2:]
-    if q in d:
-        return a
-    d.add(q)
-    if t in standart_containers:
-        return t([__loads(w,d) for w in a])
-    a=a[0]
-    if t in standart_types:
-        return a
-    l=loaders[t]
-    if sum([[1,1,float('inf'),0,0][int(w.kind)] for w in inspect.signature(l).parameters.values()])>1:
-        return l(__loads(a,d),t)
-    return l(__loads(a,d))
+    def replace_types(a,t):
+        if a[0] in standart_containers:
+            return [t[a[0]],[replace_types(w,t) for w in a[1]]]
+        if a[0] in standart_types:
+            return [t[a[0]],a[1]]
+        return [t[a[0]],replace_types(a[1],t)]
 
-def _loads(a):
-    a=json.loads(a)
-    t,a=a
-    t=[pickleloads(tuple(w)) for w in t]
-    a=[[q,t[w[0]]]+w[1:] for q,w in enumerate(a)]
-    for w in a:
-        if w[1] not in standart_types:
-            w[2:]=[a[e] for e in w[2:]]
-    a=a[0]
-    a=__loads(a,set())
-    return a
+    def dumps(a):
+        j=jdumps(a)
+        t=j[0]
+        j=j[1:]
+        t=[t[w] for w in range(len(t)) if all([t[w]!=t[e] for e in range(w)])]
+        d=dict(zip(*list(zip(*enumerate(t)))[::-1]))
+        j=replace_types(j,d)
+        t=[pickledumps(w) for w in t]
+        t=sum(t,())
+        return json.dumps([t,j],separators=(',',':'))
 
-def jloads(a):
-    ts=a[0]
-    a=a[1]
-    t=ts[a[0]]
-    a=a[1]
-    if t in standart_containers:
-        return t([jloads([ts,w]) for w in a])
-    if t in standart_types:
-        return a
-    l=loaders[t]
-    if sum([[1,1,float('inf'),0,0][int(w.kind)] for w in inspect.signature(l).parameters.values()])>1:
-        return l(jloads([ts,a]),t)
-    return l(jloads([ts,a]))
+    def jloads(a):
+        ts=a[0]
+        a=a[1]
+        t=ts[a[0]]
+        a=a[1]
+        if t in standart_containers:
+            return t([jloads([ts,w]) for w in a])
+        if t in standart_types:
+            return a
+        l=loaders[t]
+        if sum([[1,1,float('inf'),0,0][int(w.kind)] for w in inspect.signature(l).parameters.values()])>1:
+            return l(jloads([ts,a]),t)
+        return l(jloads([ts,a]))
 
-def loads(a):
-    y=json.loads(a)
-    y[0]=[pickleloads(tuple(w)) for w in y[0]]
-    return jloads(y)
+    def loads(a):
+        y=json.loads(a)
+        ts=y[0]
+        ts=zip(ts[::2],ts[1::2])
+        ts=[pickleloads(w) for w in ts]
+        y[0]=ts
+        return jloads(y)
 
-def dumper(*t):
-    def dumper_f(f):
-        for w in t:
-            dumpers[w]=f
-        return f
-    return dumper_f
+    def dumper(*t):
+        def dumper_f(f):
+            for w in t:
+                dumpers[w]=f
+            return f
+        return dumper_f
 
-def loader(*t):
-    def loader_f(f):
-        for w in t:
-            loaders[w]=f
-        return f
-    return loader_f
+    def loader(*t):
+        def loader_f(f):
+            for w in t:
+                loaders[w]=f
+            return f
+        return loader_f
 
-@dumper(float)
-def dump_float(a):
-    return Fraction(a)
+    @dumper(float)
+    def dump_float(a):
+        return Fraction(a)
 
-@dumper(Fraction)
-def dump_frac(a):
-    return [a.numerator,a.denominator]
+    @dumper(Fraction)
+    def dump_frac(a):
+        return [a.numerator,a.denominator]
 
-@loader(Fraction)
-def load_frac(a):
-    return Fraction(*a)
+    @loader(Fraction)
+    def load_frac(a):
+        return Fraction(*a)
 
-@dumper(bytes,bytearray)
-def dump_bytes(a):
-    return base64.b64encode(a).decode()
+    @dumper(bytes,bytearray)
+    def dump_bytes(a):
+        return base64.b64encode(a).decode()
 
-@loader(bytes,bytearray)
-def load_bytes(a,t):
-    return t(base64.b64decode(a.encode()))
+    @loader(bytes,bytearray)
+    def load_bytes(a,t):
+        return t(base64.b64decode(a.encode()))
 
-@loader(dict,float)
-def load_simple(a,t):
-    return t(a)
+    @loader(dict,float)
+    def load_simple(a,t):
+        return t(a)
 
-@dumper(dict)
-def dump_dict(a):
-    return list(a.items())
+    @dumper(dict)
+    def dump_dict(a):
+        return list(a.items())
 
-a={1:2,'1':'2'}
-a[3]=a
+    return dumps,loads,dumper,loader
+dumps,loads,dumper,loader=main()
