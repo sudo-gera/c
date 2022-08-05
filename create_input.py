3874b867d7d26bf4052cec31d3e081c787609d12
sudo-gera <gera.tatarinov.999@gmail.com>
2022-08-05 11:59:13 +0300

    *interesting commit message*

--- create_input.py
+++ create_input.py
@@ -13,7 +13,7 @@ def rand(q=2**64,e=None):
 def getstr():
 	return ''.join([choice('snmrdfhg')+choice('aioue') for w in range(10)])
 
-maxlen=100+int(run_num**0.5)
+maxlen=200+int(run_num**0.5)
 
 strs=[getstr() for w in range(maxlen)]
 
@@ -21,3 +21,29 @@ n=rand(1,maxlen)
 print(n)
 a=[rand(maxlen) for w in range(n)]
 print(*a)
+f=rand(maxlen)
+print(f)
+for w in range(f):
+	q=rand(7)+1
+	if n==0:
+		q=2
+	else:
+		r=rand(n)
+		l=rand(r+1)
+	print(q,end=' ')
+	if q==1:
+		print(l,r)
+	if q==2:
+		print(rand(maxlen),rand(n+1))
+		n+=1
+	if q==3:
+		print(rand(n))
+		n-=1
+	if q==4:
+		print(rand(maxlen),l,r)
+	if q==5:
+		print(rand(maxlen),l,r)
+	if q==6:
+		print(l,r)
+	if q==7:
+		print(l,r)
