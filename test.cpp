5e87f12252e2b087f34d880ba9ec3b8bf8ffe471
sudo-gera <gera.tatarinov.999@gmail.com>
2022-08-06 18:53:58 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,10 +1,12 @@
-py_func(a)({
-    def a():
-        return '-'*99
-})
-
+#include "one_byte_ptr.cpp"
+#include <iostream>
 int main(){
-    perf();
-    ic(str(a()))
-    perf();
+    one_byte_ptr<int> a[300];
+    for (int w=0;w<300;++w){
+        a[w]=new int(w*w);
+    }
+    for (int w=0;w<300;++w){
+        std::cout<<(uint64_t)(reinterpret_cast<uint8_t&>(a[w]))<<std::endl;
+    }
+    
 }
