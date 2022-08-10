56fd46dd7ed29aae35598701b89740ae5190e8a6
sudo-gera <gera.tatarinov.999@gmail.com>
2022-08-10 14:25:13 +0300

    *interesting commit message*

--- b.py
+++ b.py
@@ -1,19 +1,201 @@
-#!/usr/bin/python3
-from sys import argv
-from subprocess import run
-ex=argv[0]
-argv=argv[1:]
-if all([w.startswith('-') for w in argv]):
-	file='-'
-else:
-	file=[w for w in argv if not w.startswith('-')][0]
-ca=argv[:argv.index(file)+1]
-ra=argv[argv.index(file)+1:]
-ca='g++ -g -fsanitize=address,undefined -std=c++17 -Wfatal-errors'.split()+ca
-ra='gdb ./a.out'.split()+ra
-if run(ca).returncode:
-	exit()
-a=run(ra).returncode
-if a:
-	print(f'proxess stopped with exit code {a}')
+from h import *
+from treeprint import *
+# data={}
+# def put(v):
+#     from random import randint
+#     k=''
+#     while not k or k in data:
+#         k=''.join(
+#             [
+#                 chr(
+#                     randint(32,126)
+#                 ) for w in range(
+#                     randint(128,1024)
+#                 )
+#             ]
+#         )
+#     data[k]=v
+#     return k
 
+# def get(k):
+#     return data[k]
+
+max_len=5
+class node:
+    def __init__(s):
+        s.data=[]
+        s.next=[None]
+    def insert(s,k):
+        if k in s.data:
+            return
+        if s.next[0]==None:
+            w=0
+            while w<len(s.data) and s.data[w]<k:
+                w+=1
+            s.data.insert(w,k)
+            s.next.append(None)
+        else:
+            w=0
+            while w<len(s.data) and s.data[w]<k:
+                w+=1
+            s.next[w].insert(k)
+            if len(s.next[w].data)>max_len:
+                assert len(s.next[w].data)==1+max_len
+                q=s.next[w]
+                s.next[w]=None
+                a=node()
+                a.data=q.data[:max_len//2]
+                a.next=q.next[:max_len//2+1]
+                d=q.data[max_len//2]
+                q.data=q.data[max_len//2+1:]
+                q.next=q.next[max_len//2+1:]
+                s.next[w:w+1]=[a,q]
+                s.data[w:w]=[d]
+    def find(s,k):
+        if k in s.data:
+            return [s]
+        if s.next[0]==None:
+            return None
+        w=0
+        while w<len(s.data) and s.data[w]<k:
+            w+=1
+        r=s.next[w].find(k)
+        if r!=None:
+            r.append(s)
+        return r
+    def erase(s,k):
+        if s.next[0]==None:
+            s.data.remove(k)
+            s.next.pop()
+        else:
+            w=0
+            while w<len(s.data) and s.data[w]<k:
+                w+=1
+            s.next[w].erase(k)
+            if len(s.next[w].data)<max_len//2:
+                assert len(s.next[w].data)==max_len//2-1
+                if w:
+                    if len(s.next[w-1].data)==max_len//2:
+                        s.next[w-1].data+=[s.data[w-1]]+s.next[w].data
+                        s.next[w-1].next+=s.next[w].next
+                        s.data[w-1:w]=[]
+                        s.next[w:w+1]=[]
+                    else:
+                        s.next[w].data+=[s.data[w-1]]
+                        s.next[w].next+=[s.next[w-1].next[-1]]
+                        s.data[w]=s.next[w-1].data[-1]
+                        s.next[w-1].data[-2:]=[]
+                        s.next[w-1].next[-2:]=[]
+                else:
+                    if len(s.next[w+1].data)==max_len//2:
+                        s.next[w].data+=[s.data[w]]+s.next[w+1].data
+                        s.next[w].next+=s.next[w+1].next
+                        s.data[w:w+1]=[]
+                        s.next[w+1:w+2]=[]
+                    else:
+                        s.next[w].data+=[s.data[w]]
+                        s.next[w].next+=[s.next[w+1].next[0]]
+                        s.data[w]=s.next[w+1].data[0]
+                        s.next[w+1].data[:1]=[]
+                        s.next[w+1].next[:1]=[]
+    def to_list(s,l):
+        if s.next[0]==None:
+            l+=s.data
+        else:
+            s.next[0].to_list(l)
+            for w in range(len(s.data)):
+                l.append(data[w])
+                s.next[0].to_list(l)
+        return l
+
+
+class b:
+    def __init__(s):
+        s.s=None
+    def check(s,r=1):
+        if s.s==None:
+            f=float('inf')
+            return [-f,f]
+        assert len(s.s.data)+1==len(s.s.next)
+        assert len(s.s.data)<=max_len
+        if r:
+            return [s.s.data[0],s.s.data[-1]]
+        assert max_len//2<=len(s.s.data)
+        if s.s.next[0]==None:
+            return [s.s.data[0],s.s.data[-1]]
+        a=[w.check(0) for w in s.s.next]
+        for w in range(len(s.s.data)):
+            assert a[w][1]<s.s.data[w]<a[w+1][0]
+        return [a[0][0],a[-1][1]]
+    def add(s,v):
+        s.check()
+        q=node()
+        q.next[0]=s.s
+        q.insert(v)
+        if q.data:
+            s.s=q
+        s.check()
+    def find(s,v):
+        s.check()
+        if s.s==None:
+            return []
+        f=s.s.find(v)
+        if f == None:
+            return []
+        f=f[0]
+        return [f.data[f.data.index(v)]]
+    def remove(s,v):
+        s.check()
+        if s.s==None:
+            return
+        a=s.s.find(v)
+        if a==None:
+            return
+        a=a[::-1]
+        if a[-1].next[0]!=None:
+            t=a[-1].next[a[-1].data.index(v)+1]
+            while t.next[0]!=None:
+                t=t.next[0]
+            d=t.data[0]
+            s.s.erase(d)
+            a=s.s.find(v)
+            a[0].data[a[0].data.index(v)]=d
+        else:
+            s.s.erase(v)
+        if len(s.s.data)==0:
+            s.s=None
+        s.check()
+    def __repr__(s):
+        treeprint(s.s)
+        return ''
+    def to_list(s):
+        if s.s==None:
+            return []
+        return s.s.to_list([])
+
+
+
+
+s=b()
+seed=rand()
+seed=2805934230260850707
+print(seed)
+random.seed(seed)
+a=set()
+for w in range(999):
+    q=choice([0]+[1]*3+[2])
+    if q==0:
+        r=rand()
+        print(q,r)
+        a.add(r)
+        s.add(r)
+    if q==1:
+        r=choice(list(a)) if a and rand(2) else rand()
+        print(q,r)
+        assert (r in a)==bool(s.find(r))
+        assert (r not in a) or s.find(r)[0]==r
+    if q==2 and a:
+        r=choice(list(a))
+        print(q,r)
+        a.remove(r)
+        s.remove(r)
--- treeprint.py
+++ treeprint.py
@@ -1,176 +1,70 @@
-def treeprint_impl(root,get_left,get_right,get_str,prev_node=None):
-	#━┃┏┓┗┛┣┫┳┻╋► 
-	if root==None{
-		return
-	}
-	left=get_left(root)
-	right=get_right(root)
-	st=get_str(root)
+def treeprint(root,l=None):
+    #━┃┏┓┗┛┣┫┳┻╋► 
+    if root==None:
+        return
+    if l==None:
+        l=[]
+    l.append(0)
+    save=0
+    if len(l)>1 and l[-2]==1:
+        save=l[-2]
+        l[-2]=0
+    l[-1]=1
+    treeprint(root.next[0],l)
+    if save:
+        l[-2]=save
+    for q in range(len(root.data)):
+        for d in range(len(l)-1):
+            if l[d]==3:
+                if d==len(l)-2:
+                    if q==len(root.data)-1:
+                        print("┗",end='')
+                    else:
+                        print("┣",end='')
+                else:
+                    print("┃",end='')
+            elif l[d]==2:
+                if d==len(l)-2:
+                    print("┣",end='')
+                else:
+                    print("┃",end='')
+            elif l[d]==1:
+                if d==len(l)-2:
+                    if q==0:
+                        print("┏",end='')
+                    else:
+                        print("┣",end='')
+                else:
+                    print("┃",end='')
+            else:
+                print(" ",end='')
 
-    node=[0]*3
-	node[0]=prev_node
-	if (prev_node){
-		prev_node[2]=(size_t)node;
-	}
+        if root.next[q]==None and q==0 and root.next[q+1]==None and q==len(root.data)-1:
+            print("━",end='')
+        elif root.next[q]==None and q==0:
+            print("┳",end='')
+        elif root.next[q+1]==None and q==len(root.data)-1:
+            print("┻",end='')
+        else:
+            print("╋",end='')
+        print("► ",end='')
+        print(root.data[q])
 
-	size_t save=0;
-	if (prev_node && prev_node[1]==2){
-		save=prev_node[1];
-		prev_node[1]=0;
-	}
-	node[1]=2;
-	treeprint_impl(left,get_left,get_right,get_str,node);
-	if (save){
-		prev_node[1]=save;
-	}
+        save=0
+        if len(l)>1 and q==len(root.data)-1 and l[-2]==3:
+            save=l[-2]
+            l[-2]=0
+        l[-1]=2+(q==len(root.data)-1)
+        treeprint(root.next[q+1],l)
+        if save:
+            l[-2]=save
+    l.pop()
+            
 
-	size_t*d=node;
-	while(d[0]){
-		d=(size_t*)d[0];
-	}
-
-	for (;d!=node;d=(size_t*)d[2]){
-		if (d[1]==1){
-			if (d==prev_node){
-				printf("┗");
-			}else{
-				printf("┃");
-			}
-		}else
-		if (d[1]==2){
-			if (d==prev_node){
-				printf("┏");
-			}else{
-				printf("┃");
-			}
-		}else{
-			printf(" ");
-		}
-	}
-
-	if (left==NULL && right==NULL){
-		printf("━");
-	}else
-	if (left==NULL){
-		printf("┳");
-	}else
-	if (right==NULL){
-		printf("┻");
-	}else{
-		printf("╋");
-	}
-	printf("► ");
-	printf("%s\n",str);
-	free(str);
-
-	save=0;
-	if (prev_node && prev_node[1]==1){
-		save=prev_node[1];
-		prev_node[1]=0;
-	}
-	node[1]=1;
-	treeprint_impl(right,get_left,get_right,get_str,node);
-	if (save){
-		prev_node[1]=save;
-	}
-}
-
-// template<typename T>
-// void treeprint(const T&root,size_t* prev_node=0){
-// 	//━┃┏┓┗┛┣┫┳┻╋► 
-// 	if (!root){
-// 		return;
-// 	}
-
-// 	size_t node[3];
-// 	node[2]=(size_t)NULL;
-// 	node[0]=(size_t)prev_node;
-// 	if (prev_node){
-// 		prev_node[2]=(size_t)node;
-// 	}
-
-// 	size_t save=0;
-// 	if (prev_node && prev_node[1]==1){
-// 		save=prev_node[1];
-// 		prev_node[1]=0;
-// 	}
-// 	node[1]=1;
-// 	treeprint(root->next[0],node);
-// 	if (save){
-// 		prev_node[1]=save;
-// 	}
-
-// 	for (uint64_t q=0;q<root->len;++q){
-
-// 		size_t*d=node;
-// 		while(d[0]){
-// 			d=(size_t*)d[0];
-// 		}
-
-// 		for (;d!=node;d=(size_t*)d[2]){
-// 			// printf("%li",d[1]);
-// 			if (d[1]==3){
-// 				if (d==prev_node){
-// 					if (q==root->len-1){
-// 						printf("┗");
-// 					}else{
-// 						printf("┣");
-// 					}
-// 				}else{
-// 					printf("┃");
-// 				}
-// 			}else
-// 			if (d[1]==2){
-// 				if (d==prev_node){
-// 					printf("┣");
-// 				}else{
-// 					printf("┃");
-// 				}
-// 			}else
-// 			if (d[1]==1){
-// 				if (d==prev_node){
-// 					if (q==0){
-// 						printf("┏");
-// 					}else{
-// 						printf("┣");
-// 					}
-// 				}else{
-// 					printf("┃");
-// 				}
-// 			}else{
-// 				printf(" ");
-// 			}
-// 		}
-
-// 		if (root->next[q]==NULL and q==0 and root->next[q+1]==NULL and q==root->len-1){
-// 			printf("━");
-// 		}else
-// 		if (root->next[q]==NULL and q==0){
-// 			printf("┳");
-// 		}else
-// 		if (root->next[q+1]==NULL and q==root->len-1){
-// 			printf("┻");
-// 		}else{
-// 			printf("╋");
-// 		}
-// 		printf("► ");
-// #define _put(s) std::cout<<#s<<" = "<<root->s<<'\t';
-
-// 		_put(data[q])
-// 		_put(depth)
-// 		_put(longlen)
-// 		std::cout<<std::endl;
-// 		// printf("%li %li %li\n",root->data[q],root->depth,root->longlen);
-
-// 		save=0;
-// 		if (prev_node and q==root->len-1 and prev_node[1]==3){
-// 			save=prev_node[1];
-// 			prev_node[1]=0;
-// 		}
-// 		node[1]=2+(q==root->len-1);
-// 		treeprint(root->next[q+1],node);
-// 		if (save){
-// 			prev_node[1]=save;
-// 		}
-// 	}
-// }
+def treeprint_(root,prev_node=0):
+    if root in [0,None]:
+        return
+    treeprint_(root.next[0],prev_node+1)
+    for w in range(len(root.data)):
+        print(prev_node*'>',root.data[w])
+        treeprint_(root.next[w+1],prev_node+1)
