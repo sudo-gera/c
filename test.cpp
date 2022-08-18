ee413dc5b79c5bf9f1c0f30ca80b02583d44970b
sudo-gera <gera.tatarinov.999@gmail.com>
2022-08-18 15:25:22 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,12 +1,10 @@
-#include "one_byte_ptr.cpp"
-#include <iostream>
-int main(){
-    one_byte_ptr<int> a[300];
-    for (int w=0;w<300;++w){
-        a[w]=new int(w*w);
-    }
-    for (int w=0;w<300;++w){
-        std::cout<<(uint64_t)(reinterpret_cast<uint8_t&>(a[w]))<<std::endl;
-    }
+template<typename T>
+int _1(){
     
+}
+
+
+
+int main(){
+
 }
