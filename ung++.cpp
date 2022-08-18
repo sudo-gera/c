8850a716d5eb6fbf4ca9ede1ee84cfe306489c8f
sudo-gera <gera.tatarinov.999@gmail.com>
2022-08-18 14:09:50 +0300

    *interesting commit message*

--- ung++.cpp
+++ ung++.cpp
@@ -1,4 +1,5 @@
 #include <type_traits>
+#include <iostream>
 
 template<typename T>
 auto f(T q)->std::enable_if_t<!std::is_void_v<decltype(q)>,int>;
@@ -8,6 +9,26 @@ auto f(T q)->std::enable_if_t<!std::is_void_v<decltype(q)>,int>{
 	return q;
 }
 
+template<typename T>
+auto g(T o)->decltype(f(o)){
+    return f(o);
+}
+
+char g(...){
+    return char(0);
+}
+
+template<typename T>
+const char*compiler_te="";
+
+template<>
+const char*compiler_te<int> ="clang++";
+
+template<>
+const char*compiler_te<char> ="g++";
+
+const char*compiler=compiler_te<decltype(g(0))>;
+
 int main(){
-    f(0);
+    std::cout<<compiler<<std::endl;
 }
