bace1ccf381c3f738e57fffa60cf15c4cb750233
sudo-gera <gera.tatarinov.999@gmail.com>
2022-09-05 16:55:59 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
-a=open('/Users/gera/Downloads/yandex.ru_09-05-2022-07-04-07.csv')
-from asyncore import read
-import csv
-csvfile=a
-dialect = csv.Sniffer().sniff(csvfile.read())
-csvfile.seek(0)
-reader = csv.reader(csvfile, dialect)
-a=list(reader)[1:]
-a=[w[10] for w in a]
-a=[w.split(':')[0] for w in a]
-for w in a:
-    print('    '+repr(w)+',')
+# a=open('/Users/gera/Downloads/yandex.ru_09-05-2022-07-04-07.csv')
+# from asyncore import read
+# import csv
+# csvfile=a
+# dialect = csv.Sniffer().sniff(csvfile.read())
+# csvfile.seek(0)
+# reader = csv.reader(csvfile, dialect)
+# a=list(reader)[1:]
+# a=[w[10] for w in a]
+# a=[w.split(':')[0] for w in a]
+# for w in a:
+#     print('    '+repr(w)+',')
