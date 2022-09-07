c9a75d47424a963532024b4a169efba8c23c2436
sudo-gera <gera.tatarinov.999@gmail.com>
2022-09-07 19:52:32 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
-# a=open('/Users/gera/Downloads/yandex.ru_09-05-2022-07-04-07.csv')
-# from asyncore import read
-# import csv
-# csvfile=a
-# dialect = csv.Sniffer().sniff(csvfile.read())
-# csvfile.seek(0)
-# reader = csv.reader(csvfile, dialect)
-# a=list(reader)[1:]
-# a=[w[10] for w in a]
-# a=[w.split(':')[0] for w in a]
-# for w in a:
-#     print('    '+repr(w)+',')
+from h import *
+# @cache
+def f(b:int,n:int):
+    if b.bit_count()==3:
+        return 1
+    c=0
+    for w in range(n):
+        if b&(1<<w):
+            c+=f(b^(1<<w),n)
+    return c
+
