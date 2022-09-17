6dbeb8c33ddbca3fe364e9e3e5fe410bfad2e0c7
sudo-gera <gera.tatarinov.999@gmail.com>
2022-09-17 14:17:59 +0300

    *interesting commit message*

--- tech01-0.sh
+++ tech01-0.sh
@@ -1,37 +1,28 @@
+#!/bin/bash
 
-
-while read cf
+IFS=":"
+while read -r func
 do
-    for w in $(echo $MANPATH | sed "s/:/\n/g")
-    {
-        w="$w/man3"
-        if [ -d $w ]
+    for mpath in $MANPATH
+    do
+        fpath=$(find $mpath -name "$func.3*" | head -1)
+        if [ -n "$fpath" ]
         then
-            for e in $(find $w | grep "$cf")
-            {
-                echo "$e"
-
-
-                # if [ "$e" == "$w/$cf"* ]
-                # then
-                #     if [ -f $e ]
-                #     then
-                #         if [[ $e == *".gz" ]]
-                #         then
-                #             echo "1 $e"
-                #             rf=$(cat $e | gunzip)
-                #         else
-                #             echo "2 $e"
-                #             rf=$(cat $e)
-                #         fi
-                #         echo "$rf"
-                #         echo ""
-                #         echo ""
-                #         echo ""
-                #         echo ""
-                #     fi
-                # fi
-            }
+            break
         fi
-    }
+    done
+
+    if [ -n "$fpath" ]
+    then
+        if [ $(echo $fpath | tail -c -3) = "gz" ]
+        then
+            include_string=$(gunzip -c $fpath | grep '\#include' | head -1)
+        else
+            include_string=$(cat $fpath | grep '\#include' | head -1)
+        fi
+        include_file=$(echo $include_string | sed -r 's/.*include <(.*)>. */\1/g')
+        echo $include_file
+        echo "---"
+    fi
 done
+
