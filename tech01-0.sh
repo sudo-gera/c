4c085b0c3c3685aca088103cc861e5b8c7fec3e5
sudo-gera <gera.tatarinov.999@gmail.com>
2022-09-17 13:06:18 +0300

    *interesting commit message*

--- tech01-0.sh
+++ tech01-0.sh
@@ -1,25 +1,29 @@
-#!/bin/bash
-# function readman(){
-#     if [ $0 == *.gz ]
-#     then
-#         echo $0 "has gz"
-#     else
-#         echo $0 "has no gz"
-#     fi
-# }
 
 
-for w in $(echo $MANPATH | sed "s/:/\n/g")
-{
-    w="$w/man3"
-    if [ -d $w ]
-    then
-        for e in $(find $w | grep memcmp)
-        {
-            if [ -f $e ]
-            then
-                echo $e
-            fi
-        }
-    fi
-}
+while read cf
+do
+    for w in $(echo $MANPATH | sed "s/:/\n/g")
+    {
+        w="$w/man3"
+        if [ -d $w ]
+        then
+            for e in $(find $w | grep $cf)
+            {
+                if [ -f $e ]
+                then
+                    if [[ $e == *".gz" ]]
+                    then
+                        rf=$(cat $e | gunzip)
+                    else
+                        rf=$(cat $e)
+                    fi
+                    echo $rf
+                    echo ""
+                    echo ""
+                    echo ""
+                    echo ""
+                fi
+            }
+        fi
+    }
+done
