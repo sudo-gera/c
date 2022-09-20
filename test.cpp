109a949f3dbdd644687c6e002cb84a18a7787552
gera <gera.tatarinov.999@gmail.com>
2022-09-20 12:55:28 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-#include <bits/stdc++.h>
-=======
-// #include <boost/mpl/has_xxx.hpp>
-// BOOST_MPL_HAS_XXX_TRAIT_DEF(Juju)
+#include <iostream>
 
 
 
->>>>>>> c6e601f69e97b524bcee840dc49a0654106a1368
 
 int main(){
 
