d537871da370b85fb8d786896a5079e587f93397
sudo-gera <gera.tatarinov.999@gmail.com>
2022-09-26 22:51:18 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,5 +1,39 @@
 int main(){
-	if (0){
+				ic()
+	int jmp=0;
+				ic()
+	while(1){
+				ic()
+		switch (jmp){
+				ic()
+			case 0:
+				ic()
+			jmp=1;
+				ic()
+			continue;
+				ic()
 
+				ic()
+			if (0){
+				ic()
+				case 1:
+				ic()
+				ic()
+				ic()
+				break;
+				ic()
+			}
+				ic()
+			continue;
+				ic()
+
+				ic()
+
+				ic()
+		}
+				ic()
+		break;
+				ic()
 	}
+				ic()
 }
