d158e8457ae3f77dabe254375211a08ab56d05f0
sudo-gera <gera.tatarinov.999@gmail.com>
2022-09-28 22:36:39 +0000

    *interesting commit message*

--- test.c
+++ test.c
@@ -3,22 +3,4 @@
 
 int main ()
 {
-    int y=260;
-    asm(
-        "mov x1,%[y]\n\t"
-
-        "and w1,w1,255          \n\t"
-        "mov w2,#1          \n\t"
-        "loop:              \n\t"
-        "cbz w1,skip        \n\t"
-        "mul w2,w2,w1       \n\t"
-        "sub w1,w1,#1       \n\t"
-        "b loop             \n\t"
-        "skip:              \n\t"
-
-        "mov %[y],x2\n\t"
-    :
-        [y] "+r" (y)
-    ::);
-    printf("%i\n",y);
 }
