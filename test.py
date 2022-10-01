432d1eaa952c45d0a345d555da60687dcf45ff0e
sudo-gera <gera.tatarinov.999@gmail.com>
2022-10-01 14:01:47 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,23 +1,46 @@
-class a:
-    def __init__(self):
-        print('a')
-        self.a=[]
-
-class b(a):
-    def __init__(self):
-        # super().__init__()
-        print('b')
-        self.a.append(0)
-
-class c(a):
-    def __init__(self):
-        print('c')
-
-class d(b, c):
-    pass
-
-
-
-# b()
+n,k=[int(w) for w in input().split()]
+if k==1:
+    if n%2:
+        for w in range(1,n+1,2):
+            print(w,end=' ')
+        for w in range(n-1,0,-2):
+            print(w,end=' ')
+    else:
+        for w in range(1,n+1,2):
+            print(w,end=' ')
+        for w in range(n,0,-2):
+            print(w,end=' ')
+if k==2:
+    if n%2:
+        for w in range(2,n+1,2):
+            print(w,end=' ')
+        for w in range(n,0,-2):
+            print(w,end=' ')
+    else:
+        for w in range(2,n+1,2):
+            print(w,end=' ')
+        for w in range(n-1,0,-2):
+            print(w,end=' ')
+if k==n-1:
+    if n%2:
+        for w in range(n-1,0,-2):
+            print(w,end=' ')
+        for w in range(1,n+1,2):
+            print(w,end=' ')
+    else:
+        for w in range(n-1,0,-2):
+            print(w,end=' ')
+        for w in range(2,n+1,2):
+            print(w,end=' ')
+if k==n:
+    if n%2:
+        for w in range(n,0,-2):
+            print(w,end=' ')
+        for w in range(2,n+1,2):
+            print(w,end=' ')
+    else:
+        for w in range(n,0,-2):
+            print(w,end=' ')
+        for w in range(1,n+1,2):
+            print(w,end=' ')
 print()
-d()
