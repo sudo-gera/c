48c223f8d1bb44c5c74142545bc48df90d8a04ee
sudo-gera <gera.tatarinov.999@gmail.com>
2022-10-01 20:23:17 +0300

    *interesting commit message*

--- test.c
+++ test.c
@@ -1,8 +1,1 @@
-#include <stdio.h>
-
-
-#defi
-
-int main ()
-{
-}
+int main
--- test.cpp
+++ test.cpp
@@ -1,50 +1,12 @@
-#if SELF_INCLUDE==1
+#include <iostream>
+using namespace std;
+char*a="12345";
 
-	#define CAT_(x,y) x##y
-	#define CAT(x,y) CAT_(x,y)
-	struct CAT(vector_,T){
-		T*vector;
-	};
+void run(int t=0){
+	cout<<a[t]<<endl;
+	run(t+1);
+}
 
-#endif
-#if SELF_INCLUDE==2
-
-	#define CAT_(x,y) x##y
-	#define CAT(x,y) CAT_(x,y)
-	struct CAT(set_,T){
-		T*set;
-	};
-
-#endif
-#if SELF_INCLUDE==0
-
-	#define T int
-	#define SELF_INCLUDE 1
-	#include __FILE__
-	#undef T
-	#undef SELF_INCLUDE
-
-
-	#define T double
-	#define SELF_INCLUDE 1
-	#include __FILE__
-	#undef T
-	#undef SELF_INCLUDE
-
-
-	#define T int
-	#define SELF_INCLUDE 2
-	#include __FILE__
-	#undef T
-	#undef SELF_INCLUDE
-
-
-	#define T double
-	#define SELF_INCLUDE 2
-	#include __FILE__
-	#undef T
-	#undef SELF_INCLUDE
-
-
-#endif
-
+int main(){
+	run();
+}
