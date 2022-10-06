a203767177252eef291322c921297cca5eef7aa2
sudo-gera <gera.tatarinov.999@gmail.com>
2022-10-06 20:09:16 +0000

    *interesting commit message*

---
+++ tech02-I-1.asm
@@ -0,0 +1,8 @@
+.text
+.global sum
+
+sum:
+    mov x9,0
+    mov w9,w0
+    mov x0,x9
+    
--- test.c
+++ test.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+int sum(int x0, size_t N, int *X){
+    for (size_t w=0;w<N;++w){
+        x0+=X[w];
+    }
+    return x0;
+}
+
 
 int main ()
 {
