e2c9f7002027fc0b9ccf31e9109cc11d67a06694
sudo-gera <gera.tatarinov.999@gmail.com>
2022-10-08 16:46:52 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,19 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void run1(int&&y){
-	int r=y;
-}
-
-void run2(int&&y){
-	run1(y);
-}
 
 
 
 int main(){
-	int u=0;
-	run2(std::move(u));
+	decltype(({int r=0;r+=1;r;})) r;
 }
 
 
