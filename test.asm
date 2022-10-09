345cbe45563a9b54711ab2cd93c128813b7ac886
sudo-gera <gera.tatarinov.999@gmail.com>
2022-10-10 04:20:56 +0500

    *interesting commit message*

--- test.asm
+++ test.asm
@@ -1,7 +1,32 @@
 %include "best_io.inc"
 global _f_printreg
+global _f_printstr
+
+section .text
 
 _f_printreg:
     printreg
 ret
+
+_f_printstr:
+    push rax
+    push rbx
+    mov rax,[rsp+32]
+    mov rbx,[rsp+24]
+    printstr rax,rbx
+    pop rbx
+    pop rax
+ret
+
+; start
+; jmp _2
+; _1:
+; db "--+++++++=="
+; _2:
+; push qword _1
+; push qword _2
+; call _f_printstr
+
+
+; stop
 
