9c5c0eff3d189d1f703ab53526a6b4e15add4de7
sudo-gera <gera.tatarinov.999@gmail.com>
2022-10-26 10:52:41 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,6 +1,2 @@
-from h import *
-c=100
-print(f'#define KEY{0:020} KEY')
-for w in range(1,c):
-    print(f'#define KEY{w:020} (KEY{w-1:020} {choice("*^^^+-")} '+choice([f'KEY{rand(0,w):020}',f'{rand()}LLU'])+f')')
-print(f'#define RES KEY{c-1:020}')
+for w in range(20):
+    print(f'                                                         running test...      ✅ success!      test {w+1:2} passed ✅')
