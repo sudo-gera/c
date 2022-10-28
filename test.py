4ca8dda2090f9721ff92c503226b0d96ad42f14b
sudo-gera <gera.tatarinov.999@gmail.com>
2022-10-28 11:14:21 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,3 +1,33 @@
-for w in range(6**10):
-    q,w,e,r,t,y=[int(w) for w in '%06i'%w]
-    print(q,w,e,r,t,y)
+for q in 'qwertyuiopasdfghjklzxcvbnm':
+    w=r'''                            {
+                                "conditions": [
+                                    {
+                                        "type": "variable_unless",
+                                        "name": "multitouch_extension_finger_count_total",
+                                        "value": 0
+                                    },
+                                    {
+                                        "input_sources": [
+                                            {
+                                                "language": "ru"
+                                            }
+                                        ],
+                                        "type": "input_source_if"
+                                    }
+                                ],
+                                "from": {
+                                    "key_code": "'''+q+r'''",
+                                    "modifiers": {
+                                        "optional": [
+                                            "any"
+                                        ]
+                                    }
+                                },
+                                "to": [
+                                    {
+                                        "shell_command":"/opt/homebrew/opt/python@3.10/bin/python3 -c \"__import__('pynput.keyboard').keyboard.Controller().press('''+"'"+q+"'"+r''')\""
+                                    }
+                                ],
+                                "type": "basic"
+                            },'''
+    print(w)
