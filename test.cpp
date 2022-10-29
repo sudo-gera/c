a7cd7bcfdf55f5f86f7261f3b70536ed0ecf92de
sudo-gera <gera.tatarinov.999@gmail.com>
2022-10-29 14:58:12 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -3,31 +3,20 @@ using namespace std;
 
 using llu = long long unsigned;
 
-template<llu n>
-struct fib_s{
-    static constexpr llu value=fib_s<n-1>::value+fib_s<n-2>::value;
+struct a{
+    llu t=0;
+    constexpr a(){};
+    constexpr llu get(){return t;}
+    constexpr void set(llu r){t=r;}
 };
 
-template<>
-struct fib_s<0>{
-    static constexpr llu value=0;
-};
 
-template<>
-struct fib_s<1>{
-    static constexpr llu value=1;
-};
-
-constexpr llu fib_f(llu n){
-    if (n<2){
-        return n;
-    }
-    return fib_f(n-1)+fib_f(n-2);
+constexpr a s(llu n){
+    a g;
+    g.set(g.get()+1);
+    return g;
 }
 
-// static_assert(fib_f(50)       ==12586269025);
-static_assert(fib_s<50>::value==12586269025);
+static_assert(s(0).get()==1);
 
-int main(){
-
-}
+int main(){}
