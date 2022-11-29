1392dd327d5f86eb9220b8dea69595942e3d082e
sudo-gera <gera.tatarinov.999@gmail.com>
2022-11-29 05:28:22 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 
 template<typename T>
-int print_value(){
+constexpr int print_value(){
     int unused=0;
     return 1;
 }
 
 
 int main(){
-    print_value<int>();
+    array<conditional_t<print_value<long>(),int,void>,20> a;
 }
