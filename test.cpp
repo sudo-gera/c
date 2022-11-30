6206917fc8d01c17547d79297fa414f8ab413ba8
sudo-gera <gera.tatarinov.999@gmail.com>
2022-12-01 01:11:41 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,9 +1,28 @@
-#include <vector>
+#include <iostream>
+#include <string>
 
-constexpr void f(){
-    std::vector<int> tmp;
+using namespace std::literals;
+
+std::string popen(std::string q){
+	std::string s;
+	auto pipe=popen(q.c_str(), "r");
+	int c; // note: int, not char, required to handle EOF
+	while ((c = fgetc(pipe)) != EOF) { // standard C I/O file reading loop
+		s+=static_cast<char>(c);
+	}
+	pclose(pipe);
+	return s;
 }
 
-int main(){}
+std::string python(std::string q){
+	return popen("python3 -c \""+q+"\"");
+}
+
+
+
+
+int main(){
+
+}
 
 
