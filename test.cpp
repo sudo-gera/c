870994a1ff2ba3e1c6825f62a9ff8c21174933a8
sudo-gera <gera.tatarinov.999@gmail.com>
2022-11-30 18:23:27 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,17 +1,9 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <vector>
 
-
-template<typename T>
-constexpr int print_type_f(){
-    int unused=0;
-    return 1;
+constexpr void f(){
+    std::vector<int> tmp;
 }
 
-template<typename T>
-using print_type=conditional_t<print_type_f<T>(),T,void>;
+int main(){}
 
-int main(){
-    array<print_type<long>,20> a;
-    array<print_type<string>,20> s;
-}
+
