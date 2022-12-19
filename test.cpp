557acd45a470f6a0e646c2fa455b953618fe2158
sudo-gera <gera.tatarinov.999@gmail.com>
2022-12-20 02:17:12 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -2,40 +2,22 @@
 using namespace std;
 using llu = long long unsigned;
 
-template<llu q,llu w,__int128_t s>
-concept cond=((s>>(8*(q-1)))&0xff)==w;
-
-template<llu q>
-concept _1=(bool)(q&1);
-
-template<llu q>
-concept _2=(bool)(q&2);
-
-template<llu q>
-concept _3=_1<q> and _2<q>;
-
-template<llu q>
-concept _4=(bool)(q&4);
-
-template<llu q>
-concept _8=(bool)(q&8);
-
-template<llu q>
-concept _12=_4<q> and _8<q>;
-
-template<llu q>
-requires _3<q>
-auto f()->enable_if_t<_12<q>,int>{
-    cout<<__LINE__<<endl;
-}
-
-template<llu q>
-requires _1<q>
-auto f()->enable_if_t<_4<q>,int>{
-    cout<<__LINE__<<endl;
+void f(int y){
+    vector<int> r(y);
+    iota(r.begin(),r.end(),20);
+    r[0];
+    r.data();
+    cout<<y<<endl;
+    cout<<y<<endl;
+    cout<<y<<endl;
 }
 
 
 int main(){
-    f<3+4>();
+    f(__LINE__);
+    f(__LINE__);
+    f(__LINE__);
+    f(__LINE__);
+    f(__LINE__);
 }
+
