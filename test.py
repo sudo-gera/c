abd3cc74ee78905ecc163538784454df80c80b76
sudo-gera <gera.tatarinov.999@gmail.com>
2023-01-31 22:14:32 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,27 +1,6 @@
-# class file_slice:
-#     def __init__(self,f,n) -> None:
-#         self.f=f
-#         self.n=n
-#     def read(self,l):
-#         r=min(l,self.n)
-#         self.n-=r
-#         return self.f.read(r)
-
-
 from h import *
-# urlopen('https://example.com/',data=file_slice(modules.io.BytesIO(b'123'*8888),8192))
-
-
-# proxies = {
-#     'http': 'http://localhost:1234',
-#     'https': 'http://localhost:1234',
-# }
-import urllib.request
-
-proxy_support = urllib.request.ProxyHandler({'http' : 'http://localhost:1234', 
-                                             'https': 'http://localhost:1234'})
-opener = urllib.request.build_opener(proxy_support)
-urllib.request.install_opener(opener)
-
-print(urlopen("https://google.com").read())
-
+a=loads(open('/Users/gera/Downloads/test (1).json').read())
+a=[w[0:len('https://www.youtube.com/watch?v=zcVmHs_WzXU')] for w in a]
+a=list(set(a))
+for w in a:
+    run(['youtube-dl','-f','bestaudio',w])
