53b3f0aec368fcc4a841f64d618b1b6661166225
sudo-gera <gera.tatarinov.999@gmail.com>
2023-01-31 16:37:33 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,16 +1,27 @@
+# class file_slice:
+#     def __init__(self,f,n) -> None:
+#         self.f=f
+#         self.n=n
+#     def read(self,l):
+#         r=min(l,self.n)
+#         self.n-=r
+#         return self.f.read(r)
+
+
 from h import *
-a=scan(str)
-n=scan(int)
-s=scan([str]*n)
-for q in s:
-    d=[w for w in range(len(a)) if a[w:].startswith(q)]
-    if d:
-        print(len(d),d[0])
-    else:
-        print(0,0)
+# urlopen('https://example.com/',data=file_slice(modules.io.BytesIO(b'123'*8888),8192))
 
 
+# proxies = {
+#     'http': 'http://localhost:1234',
+#     'https': 'http://localhost:1234',
+# }
+import urllib.request
 
+proxy_support = urllib.request.ProxyHandler({'http' : 'http://localhost:1234', 
+                                             'https': 'http://localhost:1234'})
+opener = urllib.request.build_opener(proxy_support)
+urllib.request.install_opener(opener)
 
-
+print(urlopen("https://google.com").read())
 
