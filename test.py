d7152dff7579fb91572be9c8bb531b104f4a751a
sudo-gera <gera.tatarinov.999@gmail.com>
2023-02-03 00:28:32 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ from h import *
 a=loads(open('/Users/gera/Downloads/test (1).json').read())
 a=[w[0:len('https://www.youtube.com/watch?v=zcVmHs_WzXU')] for w in a]
 a=list(set(a))
-for w in a:
-    run(['youtube-dl','-f','bestaudio',w])
+print(len(a))
+# for w in a:
+#     run(['youtube-dl','-f','bestaudio',w])
