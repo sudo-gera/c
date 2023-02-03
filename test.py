c60ae3d597f4e0d68e6ddd5aaa1953b9867b19a9
sudo-gera <gera.tatarinov.999@gmail.com>
2023-02-03 21:06:18 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
-# from h import *
-# a=loads(open('/Users/gera/Downloads/test (1).json').read())
-# a=[w[0:len('https://www.youtube.com/watch?v=zcVmHs_WzXU')] for w in a]
-# a=list(set(a))
-# print(len(a))
+from h import *
+a=loads(open('/Users/gera/Downloads/test (1).json').read())
+a=[w[0:len('https://www.youtube.com/watch?v=zcVmHs_WzXU')] for w in a]
+a=list(set(a))
+print(len(a))
 # # for w in a:
 # #     run(['youtube-dl','-f','bestaudio',w])
 
-from h import *
-s,p=scan([str,str])
-for q in range(len(s)):
-    if s[q:].startswith(p):
-        print(q)
+# from h import *
+# s,p=scan([str,str])
+# for q in range(len(s)):
+#     if s[q:].startswith(p):
+#         print(q)
     
 
 
