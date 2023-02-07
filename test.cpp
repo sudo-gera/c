dc84fc49448b18ded3a9abd28d338031440b3512
sudo-gera <gera.tatarinov.999@gmail.com>
2023-02-07 17:50:32 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template<size_t n>
-using sized_int=std::tuple_element_t<"-01-2---3-------4"[n]-48,tuple<int8_t,int16_t,int32_t,int64_t,__int128_t>>;
+struct BaseA
+{
+    operator uint16_t&();
+    operator const uint16_t() const;
+};
 
-static_assert(is_same_v<sized_int<1> ,int8_t  >);
-static_assert(is_same_v<sized_int<2> ,int16_t >);
-static_assert(is_same_v<sized_int<4> ,int32_t >);
-static_assert(is_same_v<sized_int<8> ,int64_t >);
-static_assert(is_same_v<sized_int<16>,__int128_t>);
-
+auto f(const uint16_t&t){
+    return 0;
+}
 
 int main(){
-
+    sizeof(f(declval<const BaseA&&>()));
 }
