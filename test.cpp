ac61359dae1610b93288ed86e0cb104c0d65c993
sudo-gera <gera.tatarinov.999@gmail.com>
2023-02-11 23:15:42 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct BaseA
-{
-    operator uint16_t&();
-    operator const uint16_t() const;
-};
+template<size_t n>
+using sized_int=tuple_element_t<8/n%5/!(16%n),tuple<__int128_t,int64_t,int32_t,int8_t,int16_t>>;
 
-auto f(const uint16_t&t){
-    return 0;
-}
+// static_assert(is_same_v<sized_int< 1>,  __int8_t>);
+// static_assert(is_same_v<sized_int< 2>, __int16_t>);
+// static_assert(is_same_v<sized_int< 4>, __int32_t>);
+// static_assert(is_same_v<sized_int< 8>, __int64_t>);
+// static_assert(is_same_v<sized_int<16>,__int128_t>);
 
-int main(){
-    sizeof(f(declval<const BaseA&&>()));
-}
+
+
+
+
--- test.py
+++ test.py
@@ -6,14 +6,23 @@
 # # for w in a:
 # #     run(['youtube-dl','-f','bestaudio',w])
 
-from h import *
-s,n=scan([str,int])
-p=scan([str]*n)
+# from h import *
+# s,n=scan([str,int])
+# p=scan([str]*n)
 
-for q in range(len(s)):
-    for w in p:
-        if s[q:].startswith(w):
-            print(q,w)
-    
+# for q in range(len(s)):
+#     for w in p:
+#         if s[q:].startswith(w):
+#             print(q,w)
 
+# for q in count():
+#     for w in range(1,q):
+#         a=[q//e%w for e in [1,2,4,8]]
+#         if len(a)==len(set(a)):
+#             print(q,w,a)
+#             exit()
+
+
+
+[[[print(bin(a[-1])[2:].zfill(40)[:40],end='\n'),a.append(a[-1]&~(a[-1]>>1)|a[-1]^(a[-1]<<1))] for w in a] for a in [[1]]]
 
