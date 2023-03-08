95cf37ba9401e981666d4db58f9c281f4f2582f9
sudo-gera <gera.tatarinov.999@gmail.com>
2023-03-08 03:22:49 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,19 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-struct s:recursion<size_t>{
-    size_t n=0;
-    size_t k=0;
-    auto f(size_t state){
-        switch (state){
-            case 0:
-            if (n<2){
-                return n;
-            }
-            
-        }
+struct test{
+    int v=48;
+    char t=49;
+    operator decltype(make_tuple(v,t))(){
+        return make_tuple(v,t);
     }
 };
 
+int main(){
+    test u;
+    tuple<int,char> t(u);
+}
 
