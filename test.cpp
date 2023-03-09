21c72ce752bf3d2ead8258da39313368fc2f04ee
sudo-gera <gera.tatarinov.999@gmail.com>
2023-03-10 01:57:41 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,16 +1,9 @@
+#include <gmpxx.h>
 #include <bits/stdc++.h>
 using namespace std;
 
-struct test{
-    int v=48;
-    char t=49;
-    operator decltype(make_tuple(v,t))(){
-        return make_tuple(v,t);
-    }
-};
-
 int main(){
-    test u;
-    tuple<int,char> t(u);
+    mpz_class a=133;
+    
 }
 
