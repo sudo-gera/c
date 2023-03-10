8c7b3155faa85b576288548563004596196d0a57
sudo-gera <gera.tatarinov.999@gmail.com>
2023-03-10 05:22:37 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,9 +1,11 @@
-#include <gmpxx.h>
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-    mpz_class a=133;
-    
+    vector<size_t> a(20);
+    while (a.capacity()==a.size()){
+        a.push_back(1);
+    }
+    cout<<a.data()[a.size()]<<endl;
 }
 
