d3093db8719d17a237b3a73efce4d0db4ec1fc04
sudo-gera <gera.tatarinov.999@gmail.com>
2023-03-11 22:32:59 +0300

    *interesting commit message*

--- qemu_ubuntu_arm64.sh
+++ qemu_ubuntu_arm64.sh
@@ -1,8 +1,13 @@
-qemu-system-aarch64                                                         \
-    -machine virt                                                           \
-    -m 64G                                                                  \
-    -cdrom "/Users/gera/Downloads/ubuntu-22.04.2-live-server-arm64.iso"     \
-
-    # -machine type=virt                                                      \
-    # -m 8G                                                                   \
-    # -cdrom "/Users/gera/Downloads/ubuntu-22.04.2-live-server-arm64.iso"     \
+qemu-system-aarch64 \                                                       \
+  -cpu cortex-a57 \                                                         \
+  -device rtl8139,netdev=net0 \                                             \
+  -device virtio-scsi-device \                                              \
+  -device scsi-cd,drive=cdrom \                                             \
+  -device virtio-blk-device,drive=hd0 \                                     \
+  -drive "file=/Users/gera/Downloads/ubuntu-22.04.2-live-server-arm64.iso,id=cdrom,if=none,media=cdrom" \                       \
+  -m 2G \                                                                   \
+  -machine virt \                                                           \
+  -netdev user,id=net0 \                                                    \
+  -nographic \                                                              \
+  -smp 2 \                                                                  \
+;                                                                           \
