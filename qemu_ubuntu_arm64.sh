16d4601817d2bfae94fb9266a16f02235a54f9e7
sudo-gera <gera.tatarinov.999@gmail.com>
2023-03-11 22:14:29 +0300

    *interesting commit message*

---
+++ qemu_ubuntu_arm64.sh
@@ -0,0 +1,8 @@
+qemu-system-aarch64                                                         \
+    -machine virt                                                           \
+    -m 64G                                                                  \
+    -cdrom "/Users/gera/Downloads/ubuntu-22.04.2-live-server-arm64.iso"     \
+
+    # -machine type=virt                                                      \
+    # -m 8G                                                                   \
+    # -cdrom "/Users/gera/Downloads/ubuntu-22.04.2-live-server-arm64.iso"     \
