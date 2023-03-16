6318671db6caa1743051b71008d69ff2ad579d74
sudo-gera <gera.tatarinov.999@gmail.com>
2023-03-16 16:41:35 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -1,11 +1,4 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 int main(){
-    vector<size_t> a(20);
-    while (a.capacity()==a.size()){
-        a.push_back(1);
-    }
-    cout<<a.data()[a.size()]<<endl;
+    int a=0;
 }
 
