a044cf32f3826bb8f51a9eacd796df8f51f7755a
sudo-gera <gera.tatarinov.999@gmail.com>
2023-04-03 16:17:55 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,9 +1,29 @@
-from h import *
-# def not_rand():
-#     return 1-bool(rand(8))
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
 
+hostName = "0.0.0.0"
+hostPort = 9000
 
-for w in range(8):
-    print(binstr(w.to_bytes(1,'little')))
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>Title goes here.</title></head>".encode()))
+        self.wfile.write(bytes("<body><p>This is a test.</p>".encode()))
+        self.wfile.write(bytes(("<p>You accessed path: %s</p>" % self.path).encode()))
+        self.wfile.write(bytes("</body></html>".encode()))
+
+myServer = HTTPServer((hostName, hostPort), MyServer)
+print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
+
+try:
+    myServer.serve_forever()
+except KeyboardInterrupt:
+    pass
+
+myServer.server_close()
+print(time.asctime(), "Server Stops - %s:%s" % (hostName, hostPort))
+
 
 
