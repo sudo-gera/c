a27cb4c5adcae4bfef9192ea18b1d8c5d1d3c90f
sudo-gera <gera.tatarinov.999@gmail.com>
2023-05-06 05:45:10 +0300

    *interesting commit message*

--- test.c
+++ test.c
@@ -1,7 +1,22 @@
-#include <stdio.h>
-int main(){
-    long n;
-    scanf("%li",&n);
-    n/=7;
-    printf("%li",n);
-}
+    #define MAXTHREAD 10000
+
+    static jmp_buf thread[MAXTHREAD];
+    static int count_ = 0;
+
+    static void comain(void *arg) {
+	int *p = arg, i = *p;
+	for (;;) {
+		printf("coroutine %d at %p arg %p\n", i, (void*)&i, arg);
+		int n = arc4random() % count_;
+		printf("jumping to %d\n", n);
+		arg = coto(thread[i], thread[n], (char*)arg + 1);
+	}
+    }
+
+    int main(void) {
+	while (++count_ < MAXTHREAD) {
+		printf("spawning %d\n", count_);
+		cogo(thread[0], comain, &count_);
+	}
+	return 0;
+    }
--- test.cpp
+++ test.cpp
@@ -1,26 +1,50 @@
-<<<<<<< HEAD
 #include <bits/stdc++.h>
-struct leak{
-    std::vector<leak> v;
-};
+using namespace std;
+
+    static void *coarg;
+
+    void *coto(jmp_buf here, jmp_buf there, void *arg) {
+	coarg = arg;
+        if (setjmp(here)) return(coarg);
+	longjmp(there, 1);
+    }
+
+    #define STACKDIR - // set to + for upwards and - for downwards
+    #define STACKSIZE (1<<12)
+
+    static char *tos; // top of stack
+
+    void *cogo(jmp_buf here, void (*fun)(void*), void *arg) {
+        if (tos == NULL) tos = (char*)&arg;
+        tos += STACKDIR STACKSIZE;
+        char n[STACKDIR (tos - (char*)&arg)];
+        printf("%p\n", n);
+        coarg = n; // ensure optimizer keeps n
+        if (setjmp(here)) return(coarg);
+	fun(arg);
+	abort();
+    }
 
 
+    #define MAXTHREAD 4
 
+    static jmp_buf thread_[MAXTHREAD];
+    static int count_ = 0;
 
-int main(){
-    leak l;
-    l.v.emplace_back();
-    l.v.back()=std::move(l);
-    assert(l.v.empty());
+    static void comain(void *arg) {
+	int *p = (int*)arg, i = *p;
+	for (;;) {
+		printf("coroutine %d at %p arg %p\n", i, (void*)&i, arg);
+		int n = arc4random() % count_;
+		printf("jumping to %d\n", n);
+		arg = coto(thread_[i], thread_[n], (char*)arg + 1);
+	}
+    }
 
-=======
-
-auto f(const int n){
-    n = 1; //mistake
-}
-
-int main(){
-    f(0);
->>>>>>> 409b23bb6e0aba34433d9b534b13fbc7cca76958
-}
-
+    int main(void) {
+	while (++count_ < MAXTHREAD) {
+		printf("spawning %d\n", count_);
+		cogo(thread_[0], comain, &count_);
+	}
+	return 0;
+    }
