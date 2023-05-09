6d2f9bb21eaf92cc22d7199105851d838d1fd7fc
sudo-gera <gera.tatarinov.999@gmail.com>
2023-05-09 04:21:45 +0300

    *interesting commit message*

--- proxy.sh
+++ proxy.sh
@@ -15,12 +15,17 @@ export http_proxy="$http_proxy"
 export HTTP_PROXY="$http_proxy"
 export https_proxy="$http_proxy"
 export HTTPS_PROXY="$http_proxy"
-export ALL_PROXY="$http_proxy"
-export all_proxy="$http_proxy"
 export socks_PROXY="$socks_proxy"
 export SOCKS_PROXY="$socks_proxy"
 export socks4_proxy="$socks_proxy"
 export SOCKS4_PROXY="$socks_proxy"
 export socks5_proxy="$socks_proxy"
 export SOCKS5_PROXY="$socks_proxy"
-
+if [ -z "$http_proxy" ]
+then
+    export ALL_PROXY="$socks_proxy"
+    export all_proxy="$socks_proxy"
+else
+    export ALL_PROXY="$http_proxy"
+    export all_proxy="$http_proxy"
+fi
