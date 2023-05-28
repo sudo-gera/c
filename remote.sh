964e448f7952f63293e2a0ae0ec9d180c5d25d5f
sudo-gera <gera.tatarinov.999@gmail.com>
2023-05-28 22:34:59 +0300

    *interesting commit message*

--- remote.sh
+++ remote.sh
@@ -2,14 +2,14 @@ tmppipe1="$(mktemp -u)"
 tmppipe2="$(mktemp -u)"
 mkfifo -m 600 "$tmppipe1"
 mkfifo -m 600 "$tmppipe2"
-base64 < "$tmppipe1" > "$tmppipe2" &
+# base64 < "$tmppipe1" > "$tmppipe2" &
 function send(){
-    while read -rn 1 text
+    while read -rn 20 text
     do
         echo "<<$read>>"
     done
 }
-send < "$tmppipe2" &
+send < "$tmppipe1" &
 script -F "$tmppipe1"
 
 
