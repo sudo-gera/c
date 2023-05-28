e5f102b84665109fbf97597e7e5cd7717151bcad
sudo-gera <gera.tatarinov.999@gmail.com>
2023-05-29 00:27:40 +0300

    *interesting commit message*

--- remote.sh
+++ remote.sh
@@ -7,13 +7,13 @@ function send1(){
     IFS=''
     while read -rn 1 text
     do
-        echo "$text" | base64 > "$tmppipe2"
+        echo "$text" | base64 | head -c 4 > "$tmppipe2"
         # tmp="$(echo "$text" | base64)"
         # buff="$buff$tmp"
         # echo "$buff" | wc -m 
         # curl --noproxy \* http://127.0.0.1:8008/ -d "$(echo "$text" | base64)"
     done
-    echo "----" > "$tmppipe2"
+    echo "123" > "$tmppipe2"
 }
 function send2(){
     IFS=''
@@ -21,18 +21,18 @@ function send2(){
     buff=''
     while [ $stop == 0 ]
     do
-        read -t 1 -n 5 text
-        curl --noproxy \* http://127.0.0.1:8008/ -d "$text"
-        # if read -t 1 -n 5 text
-        # then
-        #     if [ "$text" == $'----\n' ]
-        #     then
-        #         stop=1
-        #     else
-        #         buff="$buff$text"
-        #     fi
-        #     curl --noproxy \* http://127.0.0.1:8008/ -d "$(echo "$buff")"
-        # fi
+        read -t 1 -n 4 text
+        # curl --noproxy \* http://127.0.0.1:8008/ -d "$text"
+        if read -t 1 -n 4 text
+        then
+            if [ "$text" == $'---\n' ]
+            then
+                stop=1
+            else
+                buff="$buff$text"
+            fi
+            curl --noproxy \* http://127.0.0.1:8008/ -d "$(echo "$buff")"
+        fi
     done
 }
 send1 < "$tmppipe1" &
