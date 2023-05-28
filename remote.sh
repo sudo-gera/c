1db9811eb93018ac42050d3e9e4654c4376fcc43
sudo-gera <gera.tatarinov.999@gmail.com>
2023-05-28 22:39:06 +0300

    *interesting commit message*

--- remote.sh
+++ remote.sh
@@ -4,7 +4,7 @@ mkfifo -m 600 "$tmppipe1"
 mkfifo -m 600 "$tmppipe2"
 # base64 < "$tmppipe1" > "$tmppipe2" &
 function send(){
-    while read -rn 20 text
+    while read -rn 1 text
     do
         echo "$text" | base64
     done
