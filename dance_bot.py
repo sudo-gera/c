6b4396633ff58ce3e6ed2121fd942094ba7fda1a
sudo-gera <gera.tatarinov.999@gmail.com>
2023-08-01 13:57:27 +0300

    *interesting commit message*

--- dance_bot.py
+++ dance_bot.py
@@ -1,46 +1,268 @@
-from h import *
+from urllib.request import Request
+from urllib.request import urlopen
+import json
+from pprint import pprint
+from pathlib import Path
 
-token = open(home + '.dance_bot_token').read().strip()
-pay_token = open(home + '.dance_bot_pay_token').read().strip()
+home=str(Path.home())+'/'
+
+class open_db:
+    def __enter__(self):
+        try:
+            with open(home+'.dance_bot_db') as file:
+                db=file.read()
+            db=json.loads(db)
+        except Exception:
+            db={}
+        self.db=db
+        return db
+    def __exit__(self,*a):
+        with open(home+'.dance_bot_db','w') as file:
+            file.write(json.dumps(self.db))
+
+token = '5922670140:AAGTBGswsJ-w13ZXk9hBk4zVkLSpmjh8Tns'
+pay_token = '381764678:TEST:62741'
+
+class obj(dict):
+    def __init__(self,*a,**s):
+        super().__init__(*a,**s)
+        self.__dict__=self
+
+def convert(a):
+    if type(a)==dict:
+        return obj({q:convert(w) for q,w in a.items()})
+    if type(a)==list:
+        return [convert(q) for q in a]
+    return a
 
 def api(path,**d):
     r=Request(f'''https://api.telegram.org/bot{token}/{path}''')
     r.add_header('Content-type', 'application/json; charset=utf-8')
     r.add_header('Accept', 'text/plain')
-    a=json.loads(urlopen(r,data=json.dumps(d).encode()).read().decode())
-    if a['ok']:
-        return a['result']
+    with urlopen(r,data=json.dumps(d).encode()) as resp:
+        a=convert(json.loads(resp.read().decode()))
+    if a.ok:
+        return a.result
     else:
         pprint(a)
 
-update_id=0
-while 1:
-    try:
-        a=api('getUpdates',offset=update_id+1,timeout=60)
-    except KeyboardInterrupt:
-        print()
-        exit()
-    if not a: continue
-    a=a[0]
-    update_id=a['update_id']
-    pprint(a)
-    if 'pre_checkout_query' in a:
-        api('answerPreCheckoutQuery',pre_checkout_query_id=a['pre_checkout_query']['id'],ok=True)
-    elif 'message' in a:
-        a=a['message']
-        if 'text' in a:
-            api('sendMessage',
-                chat_id=a['chat']['id'],
-                text='''
-                [11](https://t.me/east_lion)'''.strip(),
-                disable_web_page_preview=True,
-                parse_mode='MarkdownV2',
-                reply_markup={'remove_keyboard':True}
-            )
+def make_text(t):
+    return '\n'.join([q.strip() for q in t.strip().splitlines()])
+
+def send_invoice(a,**d):
+    api('sendInvoice',
+        chat_id=a.from_id,
+        provider_token=pay_token,
+        currency='RUB',
+        **d
+    )
+
+def send_message(a,**d):
+    api('sendMessage',
+        chat_id=a.chat.id,
+        disable_web_page_preview=True,
+        **d,
+    )
+
+def on_event_0(a):
+    if a.event_type=='pre_checkout_query':
+        if a.invoice_payload=='/pay':
+            api('answerPreCheckoutQuery',
+                pre_checkout_query_id=a.id,
+                ok=True)
+        elif a.invoice_payload=='/vip':
+            api('answerPreCheckoutQuery',
+                pre_checkout_query_id=a.id,
+                ok=False,
+                error_message='Для покупки vip статуса необходимо быть участником курса.')
         else:
-            api('sendInvoice',chat_id=a['chat']['id'],title='_1',description='_2',payload='_3',provider_token=pay_token,currency='RUB',prices=[{'label':'_rub_','amount':'9900'}])
-    else:
-        continue
-        raise SyntaxError
+            api('answerPreCheckoutQuery',
+                pre_checkout_query_id=a.id,
+                ok=False,
+                error_message='Неизвестное значение payload.')
+    elif a.event_type=='message':
+        if 'successful_payment' in a:
+            a.db['pay']=1
+            del a.successful_payment
+            a.text='/start'
+            on_event_1(a)
+        else:
+            send_message(a,
+                text=make_text('''
+                    Здравствуйте! Вас приветствует бот курса танцев!
+
+                    Здесь вы можете оплатить своё участие.
+
+                    После оплаты вы получитие доступ к беседе курса и рассылке видео-уроков.
+                '''),
+                reply_markup={
+                    'inline_keyboard':[[{
+                        'text':'Оплатить участие',
+                        'callback_data':'/pay'
+                    }]]
+                })
+    elif a.event_type=='callback_query':
+        if a.data=='/pay':
+            a.chat={}
+            send_invoice(a,
+                title='Лучшие танцы!',
+                description='У нас самые лучшие танцы!',
+                payload='/pay',
+                prices=[{'label':'участие в курсе','amount':'9900'}])
+
+def on_event_1(a):
+    if a.event_type=='pre_checkout_query':
+        if a.invoice_payload=='/vip':
+            api('answerPreCheckoutQuery',
+                pre_checkout_query_id=a.id,
+                ok=True)
+        elif a.invoice_payload=='/pay':
+            api('answerPreCheckoutQuery',
+                pre_checkout_query_id=a.id,
+                ok=False,
+                error_message='Вы уже являетесь участником курса.')
+        else:
+            api('answerPreCheckoutQuery',
+                pre_checkout_query_id=a.id,
+                ok=False,
+                error_message='Неизвестное значение payload.')
+    elif a.event_type=='message':
+        if 'successful_payment' in a:
+            a.db['pay']=2
+            del a.successful_payment
+            a.text='/start'
+            on_event_2(a)
+        else:
+            send_message(a,
+                text=make_text('''
+                    Бот курса танцев рад вас видеть в числе участников курса!
+
+                    Я буду регулярно присылать вам записи видео-уроков.
+
+                    Предлагаю вам присоединиться к общей беседе.
+
+                    Так же вы можете стать VIP-участником и получить доступ к личному общению с владельцем курса.
+                    '''),
+                reply_markup={
+                    'inline_keyboard':[
+                        [{
+                            'text':'Купить VIP-статус',
+                            'callback_data':'/vip'
+                        }],
+                        [{
+                            'text':'Присоединиться к общей беседе',
+                            'url':'http://attachment.zip'
+                        }],
+                    ]
+                })
+    elif a.event_type=='callback_query':
+        if a.data=='/vip':
+            send_invoice(a,
+                title='VIP-статус',
+                description='Вы станете VIP-участником.',
+                payload='/vip',
+                prices=[{'label':'vip-статус','amount':'9900'}])
+            return
+
+def on_event_2(a):
+    if a.event_type=='pre_checkout_query':
+        if a.invoice_payload=='/vip':
+            api('answerPreCheckoutQuery',
+                pre_checkout_query_id=a.id,
+                ok=False,
+                error_message='Вы уже являетесь VIP-участником курса.')
+        elif a.invoice_payload=='/pay':
+            api('answerPreCheckoutQuery',
+                pre_checkout_query_id=a.id,
+                ok=False,
+                error_message='Вы уже являетесь VIP-участником курса.')
+        else:
+            api('answerPreCheckoutQuery',
+                pre_checkout_query_id=a.id,
+                ok=False,
+                error_message='Неизвестное значение payload.')
+    elif a.event_type=='message':
+        if 'successful_payment' in a:
+            a.db['pay']=2
+            del a.successful_payment
+            a.text='/start'
+            on_event_2(a)
+        else:
+            send_message(a,
+                text=make_text('''
+                    Бот курса танцев рад вас видеть в числе VIP-участников курса!
+
+                    Я буду регулярно присылать вам записи видео-уроков.
+
+                    Предлагаю вам присоединиться к общей беседе.
+
+                    Так же вы можете написать личное сообщение владельцу курса.
+                    '''),
+                reply_markup={
+                    'inline_keyboard':[
+                        [{
+                            'text':'написать владельцу курса',
+                            'url':'https://t.me/east_lion'
+                        }],
+                        [{
+                            'text':'Присоединиться к общей беседе',
+                            'url':'attachment.zip'
+                        }],
+                    ]
+                })
+
+def on_event_3(a):
+    if a.event_type=='message':
+        if 'forward_from' in a:
+            if a.forward_from.id not in a.all_db:
+                send_message(a,
+                    text=make_text(f'''
+                        Это пользователь {a.forwarded_from.first_name}{' '+a.forwarded_from.last_name if 'last_name' in a.forwarded_from else ''}
+                    '''),
+                    reply_markup={
+                        'inline_keyboard':[
+                        ]
+                    })
+                
+        send_message(a,
+            text=make_text('''
+                Бот рад видеть вас в числе администраторов.
+
+                Вы можете прислать мне видео-урок для рассылания всем остальным.
+                           
+                Перешлите мне сообщение любого человека, чтобы я назвал его статус.
+            '''),
+            reply_markup={
+                'inline_keyboard':[
+                ]
+            })
+        
+
+if __name__ == '__main__':
+    update_id=0
+    while 1:
+        try:
+            a=api('getUpdates',offset=update_id+1,timeout=60)
+        except KeyboardInterrupt:
+            print()
+            exit()
+        if not a: continue
+        a=a[0]
+        update_id=a.update_id
+        del a.update_id
+        event_type=[*a.keys()][0]
+        a=a[event_type]
+        a.event_type=event_type
+        a.from_id=str(a['from'].id)
+        with open_db() as db:
+            if a.from_id not in db:
+                db[a.from_id]={
+                    'pay':0
+                }
+            a.db=db[a.from_id]
+            pprint(a)
+            a.all_db=db
+            [on_event_0, on_event_1, on_event_2, on_event_3,][a.db['pay']](a)
+
 
 
--- test.py
+++ test.py
@@ -1,88 +1,9 @@
-task=7
-if task==8:
-    a=[0]*106
-    a[2]=1
-    for q in range(2,105):
-        a[q+1]+=a[q]
-        a[50]=0
-        if q*q<106:
-            a[q*q]+=a[q]
-            a[50]=0
-    print(a[105])
-    print(a[8])
-    print(a[9])
-    print(a[10])
-if task == 7:
-    g=[21,24,18,30]
-    for a in range(32):
-        if all([x<a or y<a or y>4*x+81 or x%3==0
-            for x in range(1,9)
-            for y in range(1,9)
-        ]):
-            if a in g:
-                print(a)
-            else:
-                print('error:',a)
-if task == 6:
-    z=5**2023 - 5**1010 + 25**650 - 625
-    a=[]
-    while z:
-        a.append(z%5)
-        z//=5
-    print(a.count(4))
+class test:
+    def __enter__(self):
+        return 0
+    def __exit__(self,*a):
+        print(a)
 
-    # from h import *
-    # a=to_radix(5**2023 - 5**1010 + 25**650 - 625,5)
-    # assert int(a,5)==5**2023-5**1010+25**650-625
-    # print(a.count('4'))
-if task == 3:
-    f=0
-    for a in [0,1]:
-        for b in [0,1]:
-            for c in [0,1]:
-                for d in [0,1]:
-                    if ((a==b)==c) and (a or d) and (not a or c):
-                        print(a,b,c,d)
-                        f+=1
-    print(f)
-if task == 10:
-    def encode(n):
-        s=bin(n)[2:]
-        s=('0'*8+s)[-8:]
-        a=s.count('0')
-        b=s.count('1')
-        a,b=max(a,b),min(a,b)
-        d,f=s[:a],s[a:]
-        for q in range(b):
-            d=d[1:]+d[:1]
-        for q in range(a):
-            f=f[-1:]+f[:-1]
-        s=d+f
-        s=''.join(['0' if q =='1' else '1' for q in s])
-        n=int(s,2)
-        return n
-    print(encode(243))
-    # for q in range(0,255):
-    #     if encode(q)==243:
-    #         print(q)
-if task == 5:
-    f=0
-    def make(a:list):
-        global f
-        if len(a)==6:
-            if all([
-                (a[q]&a[q+1]&1)==0
-                for q in range(5)
-            ]):
-                if [1 in a, 3 in a, 5 in a].count(True)<=2:
-                    f+=1
-        else:
-            for q in range(6):
-                a.append(q)
-                make(a)
-                a.pop()
-    make([])
-    print(f)
-
-
+with test() as t:
+    print(t)
 
