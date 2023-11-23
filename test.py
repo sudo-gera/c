fe2715a3c468d83a5a41b12bfaac5420317b2fcd
sudo-gera <gera.tatarinov.999@gmail.com>
2023-11-24 02:47:49 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,5 +1,28 @@
-import numpy as np
+import functools
 
-[
-    []
-]
+@functools.lru_cache(1)
+def process_one_word(word_arg):
+    try:
+        while 1:
+            word = yield
+            print(word, word_arg)
+    finally:
+        print('finally', word_arg)
+
+a='''
+1
+1
+1
+2
+2
+2
+3
+3
+3
+'''.strip().split()
+
+for q in a:
+    a = process_one_word(q)
+    a.send(None)
+
+
