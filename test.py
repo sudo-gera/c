99bbdc05ff53269c16c8b1850c1a55c688f94b7f
sudo-gera <gera.tatarinov.999@gmail.com>
2024-02-27 16:10:17 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,15 +1,7 @@
-import ast
+import inspect
+import pprint
 
-try:
-    ast.parse('''
-q=0
-w=0
-"
-"
-''')
-except Exception as _e:
-    e=_e
-for q in dir(e):
-    if q[0]!='_':
-        print(f'{q} = {repr(getattr(e, q))}')
+def test():
+    return inspect.stack()[1]
 
+pprint.pprint(test())
