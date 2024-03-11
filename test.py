42e1a157fecdf7a2c74e213576c0456516de8b9b
sudo-gera <gera.tatarinov.999@gmail.com>
2024-03-12 02:55:37 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,39 +1,130 @@
-import itertools
-import random
-
-def combinations(a: list[int], k: int, s: int, d: list[int], res: list[list[int]]):
-    if not len(a) >= k + s:
-        return
-
-    if k == 0:
-        res.append(d[:])
-        return
-    if s == len(a)-1:
-        d.append(a[s])
-        res.append(d[:])
-        d.pop()
-        return
-    
-    d.append(a[s])
-    combinations(a, k-1, s+1, d, res)
-    d.pop()
-    if len(a) != k + s:
-        combinations(a, k, s+1, d, res)
+from __future__ import annotations;
+from typing import TypeVar, Generic, Self, Callable, Optional as Option;
 
 
-for q in range(1000):
-    for n in range(10):
-        for k in range(n+1):
-            a=[random.randint(0, n*2) for w in range(n)]
-            # a=[w for w in range(n)]
-            res1 : list[list[int]] = []
-            combinations(a, k, 0, [], res1)
-            res2 = [*itertools.combinations(a, k)]
-            res3 = [tuple(w) for w in res1]
-            if not res3 == res2:
-                print(res1)
-                print(res2)
-                assert 0
-                
+K = TypeVar('K');
+V = TypeVar('V');
+
+
+class Item(Generic[K, V]):
+    def __init__(self: Item[K, V], v: Option[V]):
+        self.newer: Option[K] = None;
+        self.older: Option[K] = None;
+        self.val: Option[V] = v;
+
+    def add(self: Item[K, V], skey: Option[K],
+            oth: Item[K, V], okey: Option[K]) -> None:
+        self.newer = okey;
+        oth.older = skey;
+
+    def sub(self: Item[K, V], skey: Option[K],
+            oth: Item[K, V], okey: Option[K]) -> None:
+        self.newer = skey;
+        oth.older = okey;
+
+    def __repr__(self: Item[K, V]) -> str:
+        val = self.val;
+        older = self.older;
+        newer = self.newer;
+        return f'({val = } {older = } {newer = })';
+
+
+class Cache(Generic[K, V]):
+    def __init__(self, c: int):
+        self.capacity: int = c;
+        self.data: dict[Option[K], Item[K, V]] = {};
+        self.data[None] = Item(None);
+        self.check();
+
+
+    def extract(self, it: Item[K, V], key: K) -> None:
+        self.check();
+        okey, nkey = it.older, it.newer;
+        older, newer = self.data[okey], self.data[nkey];
+        older.sub(okey, it, key);
+        it.sub(key, newer, nkey);
+        older.add(okey, newer, nkey);
+        del self.data[key];
+        self.check();
+
+    def insert_as_newest(self, it: Item[K, V], key: K) -> None:
+        self.check();
+        nkey = self.data[None].newer;
+        newer = self.data[self.data[None].newer];
+        okey = None;
+        older = self.data[None];
+        older.sub(okey, newer, nkey);
+        older.add(okey, it, key);
+        it.add(key, newer, nkey);
+        self.data[key] = it;
+        self.check();
+
+    def get(self, k: K) -> Option[V]:
+        self.check();
+        it = self.data.get(k, None);
+        if it is not None:
+            self.extract(it, k);
+            self.insert_as_newest(it, k);
+            return it.val;
+        return None;
+
+    def set(self, k: K, v: V) -> Option[V]:
+        self.check();
+        it = self.data.get(k, None);
+        if it is not None:
+            self.extract(it, k);
+            self.insert_as_newest(Item(v), k);
+            self.check();
+            return it.val;
+        else:
+            if len(self.data) == self.capacity + 1:
+                older = self.data[None].older;
+                assert older is not None;
+                self.extract(self.data[self.data[None].older], older);
+            self.insert_as_newest(Item(v), k);
+            self.check();
+            return None;
+
+    def check(self: Self) -> None:
+        it: Item[K, V] = self.data[None];
+        its = [];
+        while True:
+            its.append(it);
+            assert (it != self.data[self.data[None].newer]) ^ (self.data[it.older] is self.data[None]);
+            assert (it != self.data[self.data[None].older]) ^ (self.data[it.newer] is self.data[None]);
+            assert self.data[self.data[it.older].newer] is it;
+            it = self.data[it.older];
+            if it is self.data[None]:
+                break;
+        assert len(set(map(id, its))) == len(set(map(id, its)));
+        assert self.capacity + 1 >= len(self.data) == len(its);
+        assert sorted([*self.data.values()], key=id) == sorted(its, key=id);
+
+
+c: Cache[int, int] = Cache(2);
+assert c.set(1, 11) is None;
+assert c.set(2, 21) is None;
+assert c.set(1, 12) == 11;
+assert c.get(1) == 12;
+assert c.get(2) == 21;
+assert c.get(1) == 12;
+assert c.get(2) == 21;
+assert c.set(3, 31) is None;
+assert c.get(1) is None;
+assert c.get(2) == 21;
+assert c.get(3) == 31;
+
+c = Cache(1);
+assert c.set(1, 11) is None;
+assert c.set(2, 21) is None;
+assert c.set(1, 12) is None;
+assert c.get(1) == 12;
+assert c.get(2) is None;
+assert c.get(1) == 12;
+assert c.get(2) is None;
+assert c.set(3, 31) is None;
+assert c.get(1) is None;
+assert c.get(2) is None;
+assert c.get(3) == 31;
 
 
--- test.rs
+++ test.rs
@@ -1,5 +1,45 @@
+use std::cell::{RefCell, Ref, RefMut};
+use std::collections::{BTreeMap, HashMap};
+use std::hash::Hash;
+
+#[derive(Debug)]
+struct Item<K, V>{
+    newer: Option<K>,
+    older: Option<K>,
+    val: Option<V>,
+}
+
+impl<K: Clone + Hash + Ord, V> Item<K, V> {
+    pub fn new(val : Option<V>) -> Self{
+        Self{
+            newer: None,
+            older: None,
+            val
+        }
+    }
+
+    // def add(self: item[K, V], skey: K | None,
+    //         oth: item[K, V], okey: K | None) -> None:
+    //     self.newer = okey;
+    //     oth.older = skey;
+
+    // def sub(self: item[K, V], skey: K | None,
+    //         oth: item[K, V], okey: K | None) -> None:
+    //     self.newer = skey;
+    //     oth.older = okey;
+}
+
+
+
+
+
+
+
 
 fn main(){
-    let mut q : Vec<Vec<i32>> = vec![vec![0; 2]; 3];
-    println!("{:#?}", q);
+    let q: RefCell<Vec<i32>> = RefCell::new(vec![1,2,3]);
+    let w = q.borrow_mut();
+    
+
+
 }
