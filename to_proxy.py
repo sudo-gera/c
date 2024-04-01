c0c215c2270ea6259bd82f8f9c9c766994af636b
sudo-gera <gera.tatarinov.999@gmail.com>
2024-04-01 17:46:49 +0300

    *interesting commit message*

--- to_proxy.py
+++ to_proxy.py
@@ -20,15 +20,18 @@ async def copy_stream(r_q, w_q, inject):
             if not data: break
             writer.write(data)
             await writer.drain()
-    except KeyboardInterrupt:
+    except (KeyboardInterrupt, ConnectionResetError):
         pass
     finally:
         if writer is not None:
             writer.close()
-            await writer.wait_closed()
+            try:
+                await writer.wait_closed()
+            except ConnectionResetError:
+                pass
         
 
-async def connection(host, port, inject, a_reader, a_writer):
+async def connection(host, port, inject, connect, a_reader, a_writer):
     c_w_q = asyncio.Queue()
     c_r_q = asyncio.Queue()
     a_w_q = asyncio.Queue()
@@ -44,6 +47,19 @@ async def connection(host, port, inject, a_reader, a_writer):
         c_r_q.put_nowait(None)
         c_w_q.put_nowait(None)
     else:
+        if connect:
+            c_writer.write(f'''
+CONNECT {connect} HTTP/1.1
+Host: {connect}
+User-Agent: curl/8.4.0
+Proxy-Connection: Keep-Alive
+
+
+'''[1:-1].replace('\n','\r\n').encode())
+            await c_writer.drain()
+            res = await c_reader.readuntil(b'\r\n\r\n')
+            if res != b'HTTP/1.1 200 Connection established\r\n\r\n':
+                print(res)
         c_r_q.put_nowait(c_reader)
         c_w_q.put_nowait(c_writer)
 
@@ -53,10 +69,11 @@ async def main():
     parser.add_argument('--listen-port', type=int, required=True)
     parser.add_argument('--connect-host', type=str, required=True)
     parser.add_argument('--connect-port', type=int, required=True)
-    parser.add_argument('--http-inject', type=str, required=True)
+    parser.add_argument('--http-inject', type=str)
+    parser.add_argument('--http-connect', type=str)
     args = parser.parse_args()
 
-    server = await asyncio.start_server(partial(connection, args.connect_host, args.connect_port, args.http_inject), args.listen_host, args.listen_port)
+    server = await asyncio.start_server(partial(connection, args.connect_host, args.connect_port, args.http_inject, args.http_connect), args.listen_host, args.listen_port)
 
     async with server:
         await server.serve_forever()
