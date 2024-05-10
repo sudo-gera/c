69df8daa24b1183e1b16b87320e8fd1140872447
sudo-gera <gera.tatarinov.999@gmail.com>
2024-05-10 23:01:56 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,44 +1,22 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
-import traceback
+class meta(type):
+    def __init__(self, name, bases, namespace):
+        print('__init__', name, bases, namespace)
+        super().__init__(name, bases, namespace)
+        
+    def __call__(self):
+        new_instance = super().__call__()
+        print('call', new_instance)
+        return new_instance
 
-hostName = "0.0.0.0"
-hostPort = 9000
+class a(metaclass=meta):
+    def __new__(cls):
+        obj = super().__new__(cls)
+        print('new', obj)
+        return obj
+    
+    def __init__(self) -> None:
+        print('init', self)
+    
 
-class MyServer(BaseHTTPRequestHandler):
-    def setup(self) -> None:
-        res = super().setup()
-        self.rfile.readline___ = self.rfile.readline
-        self.rfile.readline = lambda *a, **s: [[print(q),q][-1] for q in [print(a,s), traceback.print_stack(), self.rfile.readline___(*a, **s)][-1:]][0]
-        return res
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html; charset=utf-8")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>Title goes here.</title></head>".encode()))
-        self.wfile.write(bytes("<body><p>This is a test.</p>".encode()))
-        self.wfile.write(bytes(("<p>You accessed path: %s</p>" % self.path).encode()))
-        self.wfile.write(bytes("</body></html>".encode()))
-    def do_POST(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html; charset=utf-8")
-        self.end_headers()
-        # lenn=int(self.headers['Content-Length'])
-        # data=self.rfile.read(lenn)
-        self.wfile.write(bytes("<html><head><title>Title goes here.</title></head>".encode()))
-        self.wfile.write(bytes("<body><p>This is a test.</p>".encode()))
-        self.wfile.write(bytes(("<p>You accessed path: %s</p>" % self.path).encode()))
-        # self.wfile.write(bytes(("<p>data: %s</p>" % repr(data)).encode()))
-        self.wfile.write(bytes("</body></html>".encode()))
-        # self.wfile.write(bytes(data))
+s = a()
 
-myServer = HTTPServer((hostName, hostPort), MyServer)
-print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
-
-try:
-    myServer.serve_forever()
-except KeyboardInterrupt:
-    pass
-
-myServer.server_close()
-print(time.asctime(), "Server Stops - %s:%s" % (hostName, hostPort))
