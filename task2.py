a0a7202de24195f4a91ededa04a353da36cf76c6
sudo-gera <gera.tatarinov.999@gmail.com>
2024-05-12 22:18:08 +0300

    *interesting commit message*

--- task2.py
+++ task2.py
@@ -7,7 +7,6 @@ def solve(a: np.ndarray, b: np.ndarray):
     n = a.shape[0]
     assert a.shape == (n,n)
 
-    ps = [*map(np.eye, [n]*(n-1))]
     ls = [*map(np.eye, [n]*(n-1))]
     pjs = np.zeros((n-1), int)
 
@@ -15,10 +14,6 @@ def solve(a: np.ndarray, b: np.ndarray):
     for i in range(n-1):
         j = np.argmax([*map(abs, u[i:n, i])]) + i
         pjs[i]=j
-        ps[i][i, i]=0
-        ps[i][j, j]=0
-        ps[i][j, i]=1
-        ps[i][i, j]=1
         u[[i,j], :] = u[[j,i], :]
         for j in range(i+1, n):
             t = -u[j, i] / u[i, i]
@@ -37,37 +32,12 @@ def solve(a: np.ndarray, b: np.ndarray):
         j = pjs[i]
         p[[i,j], :] = p[[j,i], :]
 
-    rs = [*map(np.linalg.inv, ks)]
-
-    for i in range(n-1):
-        assert (rs[i]+ks[i] == np.eye(n)*2).all()
-
     r = np.eye(n)
     for i in range(n-1):
         for b in range(i+1, n):
+            assert isinstance(ks[i][b, i], float)
             r[:, i] -= r[:, b] * ks[i][b, i]
 
-        # r = r @ rs[i]
-
-    import operator
-    import functools
-
-    assert np.allclose(
-        p, functools.reduce(operator.matmul, ps[::-1])
-    )
-
-    assert np.allclose(
-        r, functools.reduce(operator.matmul, rs)
-    )
-
-    assert np.allclose(
-        functools.reduce(operator.matmul, map(functools.partial(functools.reduce, operator.matmul), zip(ls[::-1], ps[::-1]))),
-        functools.reduce(operator.matmul, ks[::-1]) @ functools.reduce(operator.matmul, ps[::-1])
-    )
-
-    assert np.allclose(functools.reduce(operator.matmul, ks[::-1]) @ functools.reduce(operator.matmul, ps[::-1]) @ a, u)
-    assert np.allclose(functools.reduce(operator.matmul, ps[::-1]) @ a, functools.reduce(operator.matmul, rs) @ u)
-    assert np.allclose(p @ a, functools.reduce(operator.matmul, rs) @ u)
     assert np.allclose(p @ a, r @ u)
     for z in range(n):
         for x in range(n):
