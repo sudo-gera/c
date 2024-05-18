5321ce34e115c2447cab91860c00e4c9155cf00c
sudo-gera <gera.tatarinov.999@gmail.com>
2024-05-18 21:09:18 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,22 +1,26 @@
-class meta(type):
-    def __init__(self, name, bases, namespace):
-        print('__init__', name, bases, namespace)
-        super().__init__(name, bases, namespace)
-        
-    def __call__(self):
-        new_instance = super().__call__()
-        print('call', new_instance)
-        return new_instance
+import numpy as np
+from scipy.special import roots_jacobi
 
-class a(metaclass=meta):
-    def __new__(cls):
-        obj = super().__new__(cls)
-        print('new', obj)
-        return obj
-    
-    def __init__(self) -> None:
-        print('init', self)
-    
+def f(x):
+    return np.exp(-1/x)*(np.sin(1/x))
 
-s = a()
+def weight(x):
+    return x
 
+def gauss_kristoffel(n):
+    nodes, weights = roots_jacobi(n, alpha=0, beta=0) 
+    integral = 0
+    for i in range(n):
+        integral += weights[i] * f(nodes[i]) * weight(nodes[i])
+    return integral
+
+# Вычисляем интеграл с заданной погрешностью
+n = 10
+integral_prev = 0
+integral_curr = gauss_kristoffel(n)
+while abs(integral_curr - integral_prev) > 1e-6:
+    integral_prev = integral_curr
+    n += 1
+    integral_curr = gauss_kristoffel(n)
+
+print("Integral value:", integral_curr)
