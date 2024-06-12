d3edee0d7abc93455b83d88d65726b51082bc71b
sudo-gera <gera.tatarinov.999@gmail.com>
2024-06-12 03:39:32 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,10 +1,31 @@
-from h import *
+import sys
 
-def ft(n, b):
-    return to_radix_list(n, b).count(4)
+def wide_rec(f, last=True, level=1, b=1, e=2):
+    if level == 0:
+        assert b + 1 == e
+        f(b-1)
+    elif last:
+        wide_rec(f, False, level-1, b, e)
+        wide_rec(f, last, level+1, e, e*2)
+    else:
+        assert (b + e) % 2 == 0
+        c = (b+e)//2
+        wide_rec(f, last, level-1, b, c)
+        wide_rec(f, last, level-1, c, e)
 
-for n in range(2, 10000):
-    print(max([
-        [ft(n, b), n, b, n-b]
-        for b in range(2, n+2)
-    ]))
+a = [0]
+
+def call(n):
+    assert a[0] == n
+    a[0] += 1
+
+for n in range(8, 60):
+    a[0] = 0
+    sys.setrecursionlimit(n)
+    try:
+        wide_rec(call)
+    except RecursionError:
+        pass
+    print(n, bin(a[0]))
+
+
