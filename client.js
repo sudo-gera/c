41ffd4a91c10e8523b760399f3c59c6646f8a476
sudo-gera <gera.tatarinov.999@gmail.com>
2024-06-19 03:54:10 +0300

    *interesting commit message*

--- client.js
+++ client.js
@@ -1,19 +1,61 @@
-//const TUNNEL_URL = 'wss://user225847803-5y4xxqyz.wormhole.vk-apps.com/'  // url, который дал вам туннель
-const TUNNEL_URL = 'wss://192.168.64.1:9999'  // url, который дал вам туннель
+const TUNNEL_URL = 'wss://user225847803-m4jenobz.wormhole.vk-apps.com/'  // url, который дал вам туннель
+// const TUNNEL_URL = 'ws://127.0.0.1:3000'  // url, который дал вам туннель
 
 const io = require('socket.io-client')
 
-const socket = io(TUNNEL_URL, { transports: ['websocket'], rejectUnauthorized: false })
+var net = require('net');
 
-socket.on('connect', () => {
-  console.log('Connected to server')
-  setInterval(() => socket.emit('message', 'Hello from client'), 2000)
-})
+function base64ToArrayBuffer(base64) {
+    var binaryString = atob(base64);
+    var bytes = new Uint8Array(binaryString.length);
+    for (var i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes;
+}
 
-socket.on('message', (msg) => {
-  console.log('Message from server:', msg)
-})
+var server = net.createServer(socket => {
+  const wsocket = io(TUNNEL_URL, { transports: ['websocket'], rejectUnauthorized: false });
+  socket.on('data', data=>{
+      data = data.toString('base64');
+      // console.log(data);
+      wsocket.send(data);
+  });
 
-socket.on('disconnect', () => {
-  console.log('Disconnected from server')
-})
+  wsocket.on('message', message => {
+    // console.log(atob(message));
+    socket.write(base64ToArrayBuffer(message));
+  });
+
+  // socket.on('close', ()=>{
+  //   wsocket.
+  // })
+  wsocket.on('disconnect', ()=>{
+    socket.destroy();
+  })
+
+
+  // socket.pipe(wsocket);
+
+	// socket.write('Echo server\r\n');
+	// socket.pipe(socket);
+});
+
+server.listen(1337, '127.0.0.1');
+
+
+// const socket = io(TUNNEL_URL, { transports: ['websocket'], rejectUnauthorized: false })
+
+// socket.on('connect', () => {
+//   console.log('Connected to server')
+//   setInterval(() => socket.emit('message', 'Hello from client'), 2000)
+// })
+
+// socket.on('message', (msg) => {
+//   console.log('Message from server:', msg)
+// })
+
+// socket.on('disconnect', () => {
+//   console.log('Disconnected from server')
+// })
+
--- server.js
+++ server.js
@@ -11,17 +11,58 @@ app.get('/', (_, res) => {
   res.send("HELLO FROM SERVER - HTTP");
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+function base64ToArrayBuffer(base64) {
+    var binaryString = atob(base64);
+    var bytes = new Uint8Array(binaryString.length);
+    for (var i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes;
+}
 
-  socket.on('message', (msg) => {
-    console.log('message: ' + msg);
-    socket.emit('message', "HELLO FROM SERVER - WS");
+io.on('connection', (wsocket) => {
+  // console.log('a user connected');
+
+  // socket.on('message', (msg) => {
+  //   console.log('message: ' + msg);
+  //   socket.emit('message', "HELLO FROM SERVER - WS");
+  // });
+
+  // socket.on('disconnect', () => {
+  //   console.log('user disconnected');
+  // });
+
+  var net = require('net');
+
+  var socket = new net.Socket();
+  socket.connect(22, '127.0.0.1', function() {
+    // console.log('Connected');
+    // client.write('Hello, server! Love, Client.');
+    // client.pipe(socket);
+    socket.on('data', data => {
+      data = data.toString('base64');
+      // console.log(data);
+      wsocket.send(data);
+    });
+
+    wsocket.on('message', message=>{
+    // console.log(atob(message));
+    socket.write(base64ToArrayBuffer(message));
+    });
+    wsocket.on('disconnect', ()=>{
+      socket.destroy();
+    })
   });
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
+  // client.on('data', function(data) {
+  //   console.log('Received: ' + data);
+  //   client.destroy(); // kill client after server's response
+  // });
+
+  // client.on('close', function() {
+  //   console.log('Connection closed');
+  // });
+
 });
 
 server.listen(3000, () => {
