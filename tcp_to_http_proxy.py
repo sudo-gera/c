c0f36c4614ae81d7a771614c029a1bcb4946852e
sudo-gera <gera.tatarinov.999@gmail.com>
2024-08-27 20:59:59 +0300

    *interesting commit message*

---
+++ tcp_to_http_proxy.py
@@ -0,0 +1,34 @@
+import asyncio
+import argparse
+
+import stream
+import forwarding_parser
+
+async def copy(reader: stream.Stream, writer: stream.Stream):
+    while (data := await reader.read(2**16)):
+        writer.write(data)
+        await writer.drain()
+
+@stream.streamify
+async def connection(server_socket: stream.Stream):
+    async with stream.Stream(await asyncio.open_connection(*args.proxy[0])) as client_socket:
+        client_socket.write(f"CONNECT {args.connect[0][0]}:{args.connect[0][1]} HTTP/1.1\r\nProxy-Connection: Keep-Alive\r\n\r\n".encode())
+        await client_socket.drain()
+        await client_socket.readuntil(b'\r\n\r\n')
+        await asyncio.gather(
+            copy(client_socket, server_socket),
+            copy(server_socket, client_socket),
+        )
+
+async def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--listen', type=forwarding_parser.ColonSeparatedSocketSequence(1), required=True)
+    parser.add_argument('--proxy', type=forwarding_parser.ColonSeparatedSocketSequence(1), required=True)
+    parser.add_argument('--connect', type=forwarding_parser.ColonSeparatedSocketSequence(1), required=True)
+    global args
+    args = parser.parse_args()
+
+    async with await asyncio.start_server(connection, *args.listen[0]) as server:
+        await server.serve_forever()
+
+asyncio.run(main())
