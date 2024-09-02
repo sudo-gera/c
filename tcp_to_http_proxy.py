b01f70e0e46b3e5a115c15abc6aa1287dc923716
sudo-gera <gera.tatarinov.999@gmail.com>
2024-09-02 13:46:13 +0300

    *interesting commit message*

--- tcp_to_http_proxy.py
+++ tcp_to_http_proxy.py
@@ -7,17 +7,26 @@ import forwarding_parser
 
 async def copy(reader: stream.Stream, writer: stream.Stream):
     while (data := await reader.read(2**16)):
-        await asyncio.sleep(5)
-        print(time.asctime(), data)
+        print(data)
+        print('\n')
+        print(time.asctime(), 'sending...')
+        # await asyncio.sleep(3)
+        # for chunk in data.splitlines(keepends=True):
+        #     writer.write(chunk)
+        #     await writer.drain()
+        #     await asyncio.sleep(0.1)
         writer.write(data)
         await writer.drain()
+        print(time.asctime(), 'done')
+        print('\n')
 
 @stream.streamify
 async def connection(server_socket: stream.Stream):
     async with stream.Stream(await asyncio.open_connection(*args.proxy[0])) as client_socket:
-        client_socket.write(f"CONNECT {args.connect[0][0]}:{args.connect[0][1]} HTTP/1.1\r\nProxy-Connection: Keep-Alive\r\n\r\n".encode())
-        await client_socket.drain()
-        await client_socket.readuntil(b'\r\n\r\n')
+        if args.connect is not None:
+            client_socket.write(f"CONNECT {args.connect[0][0]}:{args.connect[0][1]} HTTP/1.1\r\nProxy-Connection: Keep-Alive\r\n\r\n".encode())
+            await client_socket.drain()
+            await client_socket.readuntil(b'\r\n\r\n')
         await asyncio.gather(
             copy(client_socket, server_socket),
             copy(server_socket, client_socket),
@@ -27,7 +36,7 @@ async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--listen', type=forwarding_parser.ColonSeparatedSocketSequence(1), required=True)
     parser.add_argument('--proxy', type=forwarding_parser.ColonSeparatedSocketSequence(1), required=True)
-    parser.add_argument('--connect', type=forwarding_parser.ColonSeparatedSocketSequence(1), required=True)
+    parser.add_argument('--connect', type=forwarding_parser.ColonSeparatedSocketSequence(1))
     global args
     args = parser.parse_args()
 
