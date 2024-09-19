3578f32a9dc9345dd5210465a02ee1ef24993f8f
sudo-gera <gera.tatarinov.999@gmail.com>
2024-09-19 12:16:01 +0300

    *interesting commit message*

--- test.cpp
+++ test.cpp
@@ -5,19 +5,16 @@
 #include <cassert>
 using namespace std;
 
-int l(){
-    std::ifstream in("");
-}
+constexpr void f(int){};
 
-constexpr int c(int x){
-    assert(x);
-    return x;
-    // return x < 10 ? x : x * l();
-}
-
-static_assert(c(1));
+template<int y>
+concept a = f(y);
 
 int main(){
-    array<int, 1> a;
-    cout << get<0>(a) << endl;;
+    // int n = 7;
+    // int a[n];
+    // cout << strtype<decltype(a)*>() << endl;
+    // static_assert(requires{
+    //     (decltype(a)*){}
+    // ;});
 }
