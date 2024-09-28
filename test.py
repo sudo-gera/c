f3515cfcf46b09a1fe10c6c816b1ca1ea5985363
sudo-gera <gera.tatarinov.999@gmail.com>
2024-09-28 22:44:11 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -6,18 +6,15 @@ import matplotlib.pyplot as plt
 import time
 
 data = {k:v for k,v in sorted(data.items())}
+ct = int(time.time())
+data[ct]=float('inf')
 keys = np.array([*data.keys()])
 values = np.array([*data.values()])
 
-fig = plt.figure()
-
 plt.plot(keys, keys)
 plt.plot(keys, keys + 300)
-plt.plot(keys, keys * 0 + int(time.time()))
-plt.plot(keys, values, 'ob')
-
-timer = fig.canvas.new_timer(interval = 5000)
-timer.add_callback(plt.close)
+plt.plot(keys, keys * 0 + ct)
+plt.scatter(keys, values, marker='o', s=0.1)
 
 plt.show()
 
