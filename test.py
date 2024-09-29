10330027758b685d851c86f34187fd9d34448c58
sudo-gera <gera.tatarinov.999@gmail.com>
2024-09-29 14:34:18 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,20 +1,38 @@
-data : dict[int, int] = {}
-exec(open('test.txt').read())
+# 6
+# from fractions import Fraction
+# print(
+# (
+# 2 * (33**132 - 5) + 3 * (33**66 + 3)**2 - (33**66 + 9)**2
+# )*Fraction(1)/(
+# (33**66 - 4) * ((33**66 - 1) ** 2 - (33**66 + 1) * (33**66 - 2) - 7)
+# )
+# )
 
-import numpy as np
-import matplotlib.pyplot as plt
-import time
+# 7
+# for q in range(0, 999):
+#     for w in range(0, 999):
+#         if q * w == 252:
+#             if q * (w - 6) == 168:
+#                 print( (q - 5) * (w - 6) )
 
-data = {k:v for k,v in sorted(data.items())}
-ct = int(time.time())
-data[ct]=float('inf')
-keys = np.array([*data.keys()])
-values = np.array([*data.values()])
+# 8
+# for q in range(10**3, 10**4):
+#     if q % 9 == 0:
+#         for w in range(10**3, 10**4):
+#             if q * 3 - 2658 == w:
+#                 if str(w)[:-1] in str(q):
+#                     print(q, w)
 
-plt.plot(keys, keys)
-plt.plot(keys, keys + 300)
-plt.plot(keys, keys * 0 + ct)
-plt.scatter(keys, values, marker='o', s=0.1)
+#         # for w in range(10):
+#         #     e = int(str(q)[1:] + str(w))
+#         #     if q * 3 - 2658 == w:
+#         #         if q % 9 == 0:
+#         #             print(q)
 
-plt.show()
-
+# 4
+print(
+sum([
+q * 12 + 6
+for q in range(6)
+])
+)
