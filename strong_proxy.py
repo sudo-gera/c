2843cc2d859fdb67daada037cac8f7b765331e6b
sudo-gera <gera.tatarinov.999@gmail.com>
2024-10-13 23:06:51 +0300

    *interesting commit message*

--- strong_proxy.py
+++ strong_proxy.py
@@ -128,8 +128,8 @@ types.coroutine
 async def server_connection(sock_: stream.Stream) -> None:
     sock = InnerStream(sock_)
     try:
-        assert await sock.recv_msg() == b'hello'
         await sock.send_msg(b'hello')
+        assert await sock.recv_msg() == b'hello'
         data = await sock.recv_msg()
     except Exception as e:
         logging.debug(f'inner client failed {type(e) = } {e = }')
@@ -148,10 +148,13 @@ async def server_connection(sock_: stream.Stream) -> None:
 async def connect_one(out_q: asyncio.Queue[InnerStream], kill_prev: asyncio.Queue[None], kill_this: asyncio.Queue[None]) -> None:
     while 1:
         try:
+            logging.debug(f'_ opening internal connection...')
             async with stream.Stream(await timeout.run_with_timeout(asyncio.open_connection(*args.connect[0]), 2)) as sock_:
+                logging.debug(f'_ internal connection made.')
                 sock = InnerStream(sock_)
                 await sock.send_msg(b'hello')
                 assert await sock.recv_msg() == b'hello'
+                logging.debug(f'_ header exchange successfull.')
                 out_q.put_nowait(sock)
                 kill_prev.put_nowait(None)
                 await kill_this.get()
@@ -181,7 +184,7 @@ async def client_connection(sock_: stream.Stream) -> None:
                 sock = await out_q.get()
                 # async with stream.Stream(await timeout.run_with_timeout(asyncio.open_connection(*args.connect[0]), 2)) as sock_:
                 logging.debug(f'internal connection made. {con_id.hex() = }')
-                sock = InnerStream(sock_)
+                # sock = InnerStream(sock_)
                 logging.debug(f'senging header... {con_id.hex() = }')
                 await sock.send_msg(con_id + outer_connection.outer_send_count.to_bytes(8, 'big'))
                 logging.debug(f'header sent. recving headers... {con_id.hex() = }')
