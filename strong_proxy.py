803a80e570077a1a4f34153eaaaae2043db5cc81
sudo-gera <gera.tatarinov.999@gmail.com>
2024-10-15 07:21:22 +0300

    *interesting commit message*

--- strong_proxy.py
+++ strong_proxy.py
@@ -30,7 +30,7 @@ class Retry:
 
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG, format='%(levelname)8s %(asctime)s %(filename)s:%(lineno)d %(funcName)s %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)8s %(asctime)s %(filename)s:%(lineno)d %(message)s')
 
 args = argparse.Namespace()
 
