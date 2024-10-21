2e6e9db31fe4c3b029ade30bccb4e9d84e3006b4
sudo-gera <gera.tatarinov.999@gmail.com>
2024-10-21 22:35:41 +0300

    *interesting commit message*

--- strong_proxy.py
+++ strong_proxy.py
@@ -224,7 +224,7 @@ class OuterConnection:
             chunk = num, data = int.from_bytes(data[:8], 'big'), data[8:]
             logger.debug(f'{con_id.hex()!r} Got data to send outside: {num = } {get_part(chunk[1])}')
             if num == 2**64 - 1:
-	        contunue
+                contunue
                 # self.inner_send_count = int.from_bytes(data[:8], 'big')
 
             try:
