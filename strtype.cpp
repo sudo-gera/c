2a7e1939b4d37b4d41fe518b6824173e050b84b9
sudo-gera <gera.tatarinov.999@gmail.com>
2024-12-24 16:14:01 +0300

    *interesting commit message*

--- strtype.cpp
+++ strtype.cpp
@@ -1,29 +1,31 @@
-#include <cassert>
-#include <iostream>
-#include <string>
-template <typename T>
-std::string strtype() {
-    std::string a=__PRETTY_FUNCTION__;
-    #ifdef __clang__
-        assert(a.size() >= 28);
-        return std::string(a.begin()+27,a.end()-1);
-    #else
-        assert(a.size() >= 32);
-        a = std::string(a.begin() + 32, a.end());
-        for (size_t i = 0; i < a.size(); ++i){
-            if (a[i] == ';'){
-                a = std::string(a.begin(), a.begin() + i);
-                return a;
-            }
-        }
-        assert(false);
-    #endif
-}
+#include <cstdio>
+#include <tuple>
+#include <array>
+
+namespace type_to_name_impl{
+
+    template<typename T>
+    consteval auto pret_func(){
+        return std::make_pair(sizeof(__PRETTY_FUNCTION__) - 5, __PRETTY_FUNCTION__);
+    }
+
+    template<typename T>
+    auto result = []()consteval{
+        std::array<char, pret_func<T>().first - pret_func<int>().first + 4> data;
+        const char* ptr = pret_func<T>().second + pret_func<int>().first;
+        for (size_t c = 0; c < data.size(); ++c){data[c] = ptr[c];}
+        data.back() = 0;
+        return data;
+    }();
+
+};
+
+template<typename T>
+const char* type_to_name = type_to_name_impl::result<T>.data();
 
 int main(){
-    std::cout << strtype<std::string>() << std::endl;
-    std::cout << strtype<int>() << std::endl;
-    std::cout << strtype<void>() << std::endl;
-    std::cout << strtype<void*>() << std::endl;
-    std::cout << strtype<decltype(main)>() << std::endl;
+    printf("%s\n",type_to_name<void>);
+    printf("%s\n",type_to_name<int>);
+    printf("%s\n",type_to_name<char>);
+    printf("%s\n",type_to_name<std::tuple<int>>);
 }
--- test.cpp
+++ test.cpp
@@ -1,8 +1,38 @@
-//#include <atomic>
-//#include <memory>
-#include <vector>
+auto flag(int);     // E1
 
-int main(){
-    std::vector<int> a;
-    a.push_back(a);
+
+template<bool B> requires (!B)
+struct setter {
+    friend auto flag(int) {}        // E2
+
+    static constexpr bool b = B;
+};
+
+
+// E3
+template<bool FlagVal>
+[[nodiscard]]
+consteval auto nonconstant_constant_impl() {
+    if constexpr (FlagVal) {
+        return true;
+    }
+    else {
+        // E3.1
+        setter<FlagVal> s;
+        return s.b;
+    }
 }
+
+
+// E4
+template<
+    auto Arg = 0,
+    bool FlagVal = requires { flag(Arg); },             // E4.1
+    auto Val = nonconstant_constant_impl<FlagVal>()     // E4.2
+>
+constexpr auto nonconstant_constant = Val;
+
+constexpr bool a = nonconstant_constant<>;      // First evaluation in this translation unit
+constexpr bool b = nonconstant_constant<>;
+// This assertion passes.
+static_assert(a != b);
