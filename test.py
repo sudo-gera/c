e126f84742b75fdc22a2ecfba7132998882f05d2
sudo-gera <gera.tatarinov.999@gmail.com>
2025-01-06 04:39:45 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,28 +1,36 @@
-from pprint import pprint
-import aiohttp
-import asyncio
-import urllib.parse
-from pathlib import Path
-home = Path().home()
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
 
-with open(home / 'vk_token.txt') as token_file:
-    token_url = token_file.read()
-    token_fragment = urllib.parse.urlsplit(token_url).fragment
-    token_args = urllib.parse.parse_qs(token_fragment)
-    token = token_args['access_token'][0]
+# Get cpu, gpu or mps device for training.
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
 
-session : aiohttp.ClientSession
+# Define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
 
-async def main():
-    async with aiohttp.ClientSession() as ses:
-        global session
-        session = ses
-        # async with session.get(f'https://api.vk.com/method/video.liveGetCategories?access_token={token}&v=5.199') as resp:
-        #     data = await resp.json()
-        async with session.get(f'https://api.vk.com/method/video.get?access_token={token}&v=5.199') as resp:
-            data = await resp.json()
-        pprint(data)
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
 
-
-if __name__ == '__main__':
-    asyncio.run(main())
+model = NeuralNetwork().to(device)
+print(model)
