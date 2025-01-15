4052fe4f235b8909045339d808149927b86dc281
sudo-gera <gera.tatarinov.999@gmail.com>
2025-01-15 22:41:12 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,36 +1,16 @@
-import torch
-from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor
+def ack(n, m):
+    for n in range(n, 0, -1):
+        m = ack(n, m - 1) if m else 1
+    return m + 1
 
-# Get cpu, gpu or mps device for training.
-device = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "mps"
-    if torch.backends.mps.is_available()
-    else "cpu"
-)
-print(f"Using {device} device")
+for q in range(20):
+    for w in range(20):
+        try:
+            a=ack(q,w)
+        except RecursionError:
+            a = -1
+        print(f'{a:8d}',end='')
+        import sys
+        sys.stdout.flush()
+    print()
 
-# Define model
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10)
-        )
-
-    def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
-
-model = NeuralNetwork().to(device)
-print(model)
