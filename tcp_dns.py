933209be6f1ea66f78acd5336bef07a474fc9bfe
sudo-gera <gera.tatarinov.999@gmail.com>
2025-01-31 05:09:37 +0300

    *interesting commit message*

--- tcp_dns.py
+++ tcp_dns.py
@@ -27,6 +27,18 @@ class UDPConnection(asyncio.DatagramProtocol):
     def datagram_received(self, data: bytes, addr: tuple[str, int]) -> None:
         asyncio.create_task(self.async_datagram_received(data, addr))
 
+@stream.streamify
+async def TCPConnection(sock: stream.Stream):
+    data_len = int.from_bytes(await sock.readexactly(2), 'big')
+    data = await sock.readexactly(data_len)
+    message = len(data).to_bytes(2, 'big') + data
+    async with stream.Stream(asyncio.open_connection(*args.connect[0])) as tcp_connection:
+        await tcp_connection.safe_write(message)
+        data_len = int.from_bytes(await tcp_connection.readexactly(2), 'big')
+        data = await tcp_connection.readexactly(data_len)
+    message = len(data).to_bytes(2, 'big') + data
+    await sock.safe_write(message)
+
 async def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('--listen', type=forwarding_parser.ColonSeparatedSocketSequence(1), required=True)
@@ -41,7 +53,8 @@ async def main() -> None:
     )
 
     try:
-        await asyncio.Future()
+        async with await asyncio.start_server(TCPConnection, *args.listen[0]) as server:
+            await server.serve_forever()
     finally:
         endpoint.close()
 
