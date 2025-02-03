293684d8d5fcff1c22a07b75d637913ede9b765d
sudo-gera <gera.tatarinov.999@gmail.com>
2025-02-03 14:30:01 +0300

    *interesting commit message*

--- tcp_dns.py
+++ tcp_dns.py
@@ -12,6 +12,27 @@ import random
 
 args = argparse.Namespace()
 
+popular_dns_servers=[
+    forwarding_parser.ColonSeparatedSocketSequence(1)('1.1.1.1:53'),
+    forwarding_parser.ColonSeparatedSocketSequence(1)('8.8.8.8:53'),
+    forwarding_parser.ColonSeparatedSocketSequence(1)('8.8.4.4:53'),
+    forwarding_parser.ColonSeparatedSocketSequence(1)('1.0.0.1:53'),
+    forwarding_parser.ColonSeparatedSocketSequence(1)('95.85.95.85:53'),
+    forwarding_parser.ColonSeparatedSocketSequence(1)('2.56.220.2:53'),
+    forwarding_parser.ColonSeparatedSocketSequence(1)('77.88.8.8:53'),
+    forwarding_parser.ColonSeparatedSocketSequence(1)('77.88.8.1:53'),
+    forwarding_parser.ColonSeparatedSocketSequence(1)('62.76.76.62:53'),
+    forwarding_parser.ColonSeparatedSocketSequence(1)('62.76.62.76:53'),
+    forwarding_parser.ColonSeparatedSocketSequence(1)('195.208.4.1:53'),
+    forwarding_parser.ColonSeparatedSocketSequence(1)('195.208.5.1:53'),
+    forwarding_parser.ColonSeparatedSocketSequence(1)('9.9.9.9:53'),
+    forwarding_parser.ColonSeparatedSocketSequence(1)('9.9.9.10:53'),
+    forwarding_parser.ColonSeparatedSocketSequence(1)('208.67.222.222:53'),
+    forwarding_parser.ColonSeparatedSocketSequence(1)('208.67.220.220:53'),
+    forwarding_parser.ColonSeparatedSocketSequence(1)('208.67.222.220:53'),
+    forwarding_parser.ColonSeparatedSocketSequence(1)('208.67.220.222:53'),
+]
+
 class UDPConnection(asyncio.DatagramProtocol):
 
     def connection_made(self, transport: asyncio.BaseTransport) -> None:
@@ -20,7 +41,16 @@ class UDPConnection(asyncio.DatagramProtocol):
     async def async_datagram_received(self, data: bytes, addr: tuple[str, int]) -> None:
         print('got udp client')
         message = len(data).to_bytes(2, 'big') + data
-        async with stream.Stream(asyncio.open_connection(*random.choice(args.connect)[0])) as tcp_connection:
+        dns_server = random.choice(args.connect)[0]
+        to_connect = args.proxy[0] if args.proxy else dns_server
+        print(f'selected {dns_server}')
+        async with stream.Stream(asyncio.open_connection(*to_connect)) as tcp_connection:
+            if args.proxy:
+                await tcp_connection.safe_write((
+                    f'CONNECT {dns_server[0]}:{dns_server[1]} HTTP/1.1'+
+                    '\r\nProxy-Connection: Keep-Alive\r\n\r\n'
+                ).encode())
+                await tcp_connection.readuntil(b'\r\n\r\n')
             print('connected')
             await tcp_connection.safe_write(message)
             print('sent')
@@ -54,8 +84,11 @@ async def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('--listen', type=forwarding_parser.ColonSeparatedSocketSequence(1), required=True)
     parser.add_argument('--connect', type=forwarding_parser.ColonSeparatedSocketSequence(1), nargs='*')
+    parser.add_argument('--proxy', type=forwarding_parser.ColonSeparatedSocketSequence(1), required=False)
     global args
     args = parser.parse_args()
+    if not args.connect:
+        args.connect = popular_dns_servers
 
 
     endpoint, _ = await asyncio.get_running_loop().create_datagram_endpoint(
