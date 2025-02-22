22329ecdca09b143d5b0c120d702f5781f0d13d1
sudo-gera <gera.tatarinov.999@gmail.com>
2025-02-22 21:23:49 +0300

    *interesting commit message*

--- multiproxy.py
+++ multiproxy.py
@@ -112,6 +112,7 @@ import bisect
 import collections
 import timeout
 import traceback
+import pickle
 from collections import deque
 
 args = argparse.Namespace()
@@ -119,21 +120,52 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG, format='%(levelname)8s %(asctime)s %(created)9.7f %(filename)s:%(lineno)d %(message)s')
 
 
+class connection:
+    @staticmethod
+    def get_by_id(con_id: bytes) -> connection:
+        if con_id not in connections:
+            connections[con_id] = connection(con_id)
+        return connections[con_id]
+    def __init__(self, con_id: bytes, con: stream.Stream | None = None):
+        self.con = con
+        self.con_id = con_id
+        self.outer_recv_count = 0
+    async def loop(self):
+        if self.con is None:
+            assert False
+            # self.con =
+        assert self.con is not None
+        await asyncio.gather(
+            self.reader_loop,
+            # self.writer_loop,
+        ) 
+    async def reader_loop(self):
+        assert isinstance(self.con, stream.Stream)
+        data = self.con.safe_read(2**16)
+        message()
+
+connections : dict[bytes, connection] = {}
+
 class message:
     @staticmethod
-    async def from_stream():
-        
-
+    def from_bytes(data: bytes) -> message:
+        res : message
+        res = pickle.loads(data) # type: ignore
+        return res
 
 @stream.streamify
 async def inner_callback(inner_sock: stream.Stream):
     data = await inner_sock.recv_msg()
+    msg = message.from_bytes(data)
+    con = connection.get_by_id(msg.con_id)
+    ...
 
 
 @stream.streamify
 async def outer_callback(outer_sock: stream.Stream):
-    con_id = random.randbytes(8).hex()
-    
+    con_id = random.randbytes(8)
+    con = connection(con_id, outer_sock)
+    await con.loop()
 
 
 async def main() -> None:
@@ -143,7 +175,7 @@ async def main() -> None:
     parser.add_argument('--connect', type=forwarding_parser.ColonSeparatedSocketSequence(1), required=True, nargs='*')
     parser.add_argument('--is-server', action='store_true')
     args = parser.parse_args()
-    async with await asyncio.start_server(server_connection if args.is_server else client_connection, *args.listen[0]) as server:
+    async with await asyncio.start_server(inner_callback if args.is_server else outer_callback, *args.listen[0]) as server:
         await server.serve_forever()
 
 if __name__ == '__main__':
