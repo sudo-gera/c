5a4ea3615d441cb6700a994bb07d86155bd1657b
sudo-gera <gera.tatarinov.999@gmail.com>
2025-05-24 02:35:57 +0300

    *interesting commit message*

--- paradigm/comparer.sh
+++ paradigm/comparer.sh
@@ -17,12 +17,12 @@ fi
 
 dirs_escaped_lf_joined="$(find . -mindepth 1 -maxdepth 1 -type d -exec bash -c 'echo "${0@Q}"' {} \;)"
 
-echo "$dirs_escaped_lf_joined" | \
+printf '%s\n' "$dirs_escaped_lf_joined" | \
 while read -r path_1_escaped
 do
     path_1="$(bash -c "printf '%s\n' $path_1_escaped")"
     name_1="$(basename "$path_1" | $gnu_sed -E 's?_based_on_.*??g')"
-    echo "$dirs_escaped_lf_joined" | \
+    printf '%s\n' "$dirs_escaped_lf_joined" | \
     while read -r path_2_escaped
     do
         path_2="$(bash -c "printf '%s' $path_2_escaped")"
@@ -37,7 +37,7 @@ do
                 echo
                 printf '%s\n' "$to_exec"
                 echo
-                bash -c "$to_exec"
+                bash -c "$to_exec" || :
             fi
         fi
     done
---
+++ paradigm/task_03_based_on_task_01/load_capacity_attr/src/lib.rs
@@ -0,0 +1,5 @@
+// types = { path = "../types" }
+use types::AllTypesContext;
+pub fn init(context: &mut AllTypesContext){
+    context.new_attr::<i64>(String::from("load_capacity"), String::from("plane"));
+}
