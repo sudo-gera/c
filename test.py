8e66febe8fcc721c8154e474766daa2845103dee
sudo-gera <gera.tatarinov.999@gmail.com>
2025-05-31 13:26:22 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -3,21 +3,33 @@ import signal
 import time
 
 def child_process():
+    import signal
 
     resources = set()
+    running = True
 
     class resource:
         def __enter__(self):
             resources.add(id(self))
+            return self
         def __exit__(self, *_):
             resources.remove(id(self))
 
-    try:
-        while 1:
+    def handler(signum, frame):
+        nonlocal running
+        running = False
+
+    signal.signal(signal.SIGINT, handler)
+
+    while running:
+        try:
             with resource():
                 pass
-    except KeyboardInterrupt:
-        exit(len(resources))
+        finally:
+            # Just in case __exit__ isn't reached due to a signal
+            resources.clear()
+
+    exit(len(resources))
 
 def main_process(child_pid):
     time.sleep(0.1)
