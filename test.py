08655c1b9d536245160e096775eeb1a97377875d
sudo-gera <gera.tatarinov.999@gmail.com>
2025-06-23 11:17:09 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,48 +1,63 @@
-import os
-import signal
-import time
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+import random
 
-def child_process():
-    import signal
+# Seed for reproducibility
+# np.random.seed(42)
 
-    resources = set()
-    running = True
+# --- Option 1: Mixture of Two Normal Distributions ---
 
-    class resource:
-        def __enter__(self):
-            resources.add(id(self))
-            return self
-        def __exit__(self, *_):
-            print(id(self),end='\r')
-            resources.remove(id(self))
+def generate_mixture_normals(n=66):
+    # Define parameters
+    mu1, sigma1, weight1 = 1.8, 0.2, 0.4
+    mu2, sigma2, weight2 = 3.2, 0.5, 0.6
 
-    def handler(signum, frame):
-        nonlocal running
-        running = False
+    sigma1 *= 8
+    sigma2 *= 8
 
-    signal.signal(signal.SIGINT, handler)
+    # Generate number of samples for each component
+    n1 = int(n * weight1)
+    n2 = n - n1
 
-    while running:
-        try:
-            with resource():
-                pass
-        finally:
-            # Just in case __exit__ isn't reached due to a signal
-            # resources.clear()
-            pass
+    data1 = np.random.normal(mu1, sigma1, n1)
+    data2 = np.random.normal(mu2, sigma2, n2)
 
-    exit(len(resources))
+    return np.concatenate([data1, data2])
 
-def main_process(child_pid):
-    time.sleep(0.1)
-    os.kill(child_pid, signal.SIGINT)
-    pid, rc = os.waitpid(child_pid, 0)
-    assert pid == child_pid and rc == 0, f'{child_pid = }, {pid = }, {rc = }'
+# --- Option 2: Log-Normal Distribution (alternative) ---
 
-if __name__ == '__main__':
-    for i in range(999):
-        print('iteration', i)
-        if (pid := os.fork()):
-            main_process(pid)
-        else:
-            child_process()
+def generate_lognormal(n=66):
+    mu, sigma = 1.0, 0.5  # log-space parameters
+    return np.random.lognormal(mean=mu, sigma=sigma, size=n)
+
+# --- Choose one ---
+
+# --- Plot ---
+# sns.histplot(data, kde=True, bins=20, color='skyblue', edgecolor='black')
+# plt.title("Synthetic Data Distribution")
+# plt.xlabel("Value")
+# plt.ylabel("Frequency")
+# plt.grid(True)
+# plt.show()
+
+# # --- Optional: Print basic stats ---
+# print("Min:", np.min(data))
+# print("Max:", np.max(data))
+# print("Mean:", np.mean(data))
+# print("Std Dev:", np.std(data))
+
+a = []
+
+for q in range(16):
+    data = generate_mixture_normals(1024)  # or: data = generate_lognormal()
+    s = sum(data)
+    # s = s + random.random() * 60 + random.random() * 60 + random.random() * 4
+    s = round(s)
+    a.append(s)
+    print(s)
+
+print()
+print(round(np.mean(a)))
+print(round(np.std(a)))
