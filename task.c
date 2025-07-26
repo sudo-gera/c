398b30c4ff73f7cd42524d10f64b2eddcb4c7816
sudo-gera <gera.tatarinov.999@gmail.com>
2025-07-26 21:31:50 +0300

    *interesting commit message*

--- task.c
+++ task.c
@@ -1,54 +1,54 @@
 
-#include <stdio.h>
-#include <stdint.h>
-#include <inttypes.h>
-#include <stdlib.h>
-      
-#define t uint32_t
-#define tp PRIu32
+    #include <stdio.h>
+    #include <stdint.h>
+    #include <inttypes.h>
+    #include <stdlib.h>
+        
+    #define t uint32_t
+    #define tp PRIu32
 
-void print_flag_to_stdout_without_password(){
+    void print_flag_to_stdout_without_password(){
+    
+    t result_0 = UINT32_C(2805013696) - UINT32_C(819165318);
+    t result_1 = result_0 + UINT32_C(1007744213);
+    t result_2 = result_1 | UINT32_C(878721827);
+    t result_3 = result_2 * UINT32_C(682282598);
+    t result_4 = result_1 & UINT32_C(2666283760);
+    t result_5 = UINT32_C(654672814) + result_0;
+    t result_6 = result_3 * result_5;
+    t result_7 = result_3 - result_2;
+    t result_8 = result_0 ^ UINT32_C(2314295467);
+    t result_9 = result_1 + result_0;
+    t result_10 = result_9 | result_5;
+    t result_11 = result_0 * UINT32_C(38313677);
+    t result_12 = result_11 - result_10;
+    t result_13 = result_6 ^ result_8;
+    t result_14 = UINT32_C(691536159) - result_4;
+    t result_15 = result_14 & result_8;
+    t result_16 = result_9 & result_12;
+    t result_17 = result_14 + result_2;
+    t result_18 = result_11 | result_0;
+    t result_19 = result_9 ^ result_2;
 
-    t result_0 = UINT32_C(3045143558) | UINT32_C(3481897110);
-    t result_1 = UINT32_C(2065998056) + result_0;
-    t result_2 = result_1 * result_1;
-    t result_3 = result_0 * UINT32_C(2616386405);
-    t result_4 = result_0 * result_2;
-    t result_5 = result_3 ^ result_2;
-    t result_6 = result_3 ^ result_3;
-    t result_7 = UINT32_C(2200752689) ^ result_4;
-    t result_8 = result_3 - result_4;
-    t result_9 = result_3 * result_0;
-    t result_10 = result_8 - result_9;
-    t result_11 = result_1 & result_7;
-    t result_12 = result_6 & result_0;
-    t result_13 = result_8 | result_1;
-    t result_14 = result_9 + UINT32_C(822207024);
-    t result_15 = result_6 - result_8;
-    t result_16 = result_5 ^ result_1;
-    t result_17 = result_5 & result_4;
-    t result_18 = result_4 - result_14;
-    t result_19 = result_13 - UINT32_C(258585180);
-
-    t flag = result_19;
-    printf("FLAG_%" tp "\n", flag);
-}
-
-void ask_meaningless_password(){
-    char password[1024]; // Buffer of size 50
-
-    printf("Enter password:");
-    scanf("%49s", password); 
-
-    printf("No flag for this password, sorry.\n");
-}
-
-int main(int argc, char**argv){
-    if (argc != -1) {
-        print_flag_to_stdout_without_password();
-    } else {
-        ask_meaningless_password();
+        t flag = result_19;
+        printf("FLAG_%" tp "\n", flag);
     }
-}
 
+    void ask_meaningless_password(){
+        char password[1024]; // Buffer of size 50
 
+        printf("Enter password:");
+        scanf("%49s", password); 
+
+        printf("No flag for this password, sorry.\n");
+    }
+
+    int main(int argc, char**argv){
+        if (argc == -1) {
+            print_flag_to_stdout_without_password();
+        } else {
+            ask_meaningless_password();
+        }
+    }
+
+    
--- task_gen.py
+++ task_gen.py
@@ -1,47 +1,51 @@
-operations = 20
+operations = 9999
 import random
 
-print(f'''
-#include <stdio.h>
-#include <stdint.h>
-#include <inttypes.h>
-#include <stdlib.h>
-      
-#define t uint32_t
-#define tp PRIu32
+def create_c_file(output):
+    print(f'''
+    #include <stdio.h>
+    #include <stdint.h>
+    #include <inttypes.h>
+    #include <stdlib.h>
+        
+    #define t uint32_t
+    #define tp PRIu32
 
-void print_flag_to_stdout_without_password()'''+'{'+f'''
-''')
+    void print_flag_to_stdout_without_password()'''+'{'+f'''
+    ''', file=output)
 
-for q in range(operations):
-    op = random.choice('*-+^|&')
-    args = []
-    for w in '--':
-        val = random.randint(-1, q-1)
-        operand = f'UINT32_C({random.randint(0, 2**32-1)})' if val == -1 else f'result_{val}'
-        args.append(operand)
-    print(f'    t result_{q} = {args[0]} {op} {args[1]};')
+    for q in range(operations):
+        op = random.choice('*-+^|&')
+        args = []
+        for w in '--':
+            val = random.randint(-1, q-1)
+            operand = f'UINT32_C({random.randint(0, 2**32-1)})' if val == -1 else f'result_{val}'
+            args.append(operand)
+        print(f'    t result_{q} = {args[0]} {op} {args[1]};', file=output)
 
-print(f'''
-    t flag = result_{operations-1};
-    printf("FLAG_%" tp "\\n", flag);
-'''+'}'+'''
+    print(f'''
+        t flag = result_{operations-1};
+        printf("FLAG_%" tp "\\n", flag);
+    '''+'}'+'''
 
-void ask_meaningless_password()'''+'{'+f'''
-    char password[1024]; // Buffer of size 50
+    void ask_meaningless_password()'''+'{'+f'''
+        char password[1024]; // Buffer of size 50
 
-    printf("Enter password:");
-    scanf("%49s", password); 
+        printf("Enter password:");
+        scanf("%49s", password); 
 
-    printf("No flag for this password, sorry.\\n");
-'''+'}'+f'''
-
-int main(int argc, char**argv)'''+'{'+f'''
-    if (argc == -1) '''+'{'+f'''
-        print_flag_to_stdout_without_password();
-    '''+'}'+f''' else '''+'{'+f'''
-        ask_meaningless_password();
+        printf("No flag for this password, sorry.\\n");
     '''+'}'+f'''
-'''+'}'+f'''
 
-''')
+    int main(int argc, char**argv)'''+'{'+f'''
+        if (argc == -1) '''+'{'+f'''
+            print_flag_to_stdout_without_password();
+        '''+'}'+f''' else '''+'{'+f'''
+            ask_meaningless_password();
+        '''+'}'+f'''
+    '''+'}'+f'''
+
+    ''', file=output)
+
+import sys
+create_c_file(sys.stdout)
