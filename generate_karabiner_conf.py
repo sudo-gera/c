f3d55971ecd4e2714d64502c6dd3afb41b1abefb
sudo-gera <gera.tatarinov.999@gmail.com>
2025-08-18 16:36:45 +0300

    *interesting commit message*

--- generate_karabiner_conf.py
+++ generate_karabiner_conf.py
@@ -1,18 +1,16 @@
 from __future__ import annotations
-import pathlib
-import dataclasses
-import typing
-import pathlib
-from urllib.request import urlopen
-import json
 from operator import *
 from itertools import *
 from functools import *
-from typing import *
-import dataclasses
-import typing
+from typing_extensions import *
+import pathlib
+import json
+import typing as typing_basic
+import typing_extensions
 import sys
 import random
+from dataclasses import dataclass
+from urllib.request import urlopen
 
 if sys.version_info < (3, 11):
     from collections.abc import Callable as caCallable
@@ -21,46 +19,117 @@ if sys.version_info < (3, 11):
     def call(obj: caCallable[call_P, call_R], /, *args: call_P.args, **kwargs: call_P.kwargs) -> call_R:
         return obj(*args, **kwargs)
 
+# Category = Literal[
+#     'Letter keys', 'Generic desktop keys', 'Mouse keys',
+#     'Arrow keys', 'Mouse buttons', 'Media controls',
+#     'International keys', 'Generic GUI application control keys',
+#     'Japanese', 'Others', 'Keys in pc keyboards',
+#     'Controls and symbols', 'Software function',
+#     'D-pad', 'Function keys', 'Modifier keys',
+#     'Sticky modifier keys', 'Keypad keys',
+#     'Application launch keys', 'Remote control buttons',
+#     'Disable this key', 'Number keys'
+# ]
+# Modifiers = Literal[
+#     'caps_lock', 'left_control',
+#     'left_shift', 'left_option',
+#     'left_command', 'right_control',
+#     'right_shift', 'right_option',
+#     'right_command', 'fn'
+# ]
+# ButtonTypes = Literal[
+#     'key_code',
+#     'consumer_key_code',
+#     'pointing_button',
+#     'generic_desktop',
+#     'apple_vendor_top_case_key_code'
+# ]
+
+Layouts = Literal['en', 'ru']
+PossibleFingerCountValues = Literal[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# AllButtonsButtonDescriptionData = TypedDict('AllButtonsButtonDescriptionData', {'key_code': str})
+# AllButtonsButtonDescription = TypedDict(
+#     'AllButtonsButtonDescription',
+#     {'label': str, 'category': Category, 'not_from': bool, 'unsafe_from': bool, 'not_to': bool, 'data': list[AllButtonsButtonDescriptionData]}
+# )
+
 home = pathlib.Path.home()
 conf_file_path = home / '.config' / 'karabiner' / 'assets' / 'complex_modifications' / 'auto_generated_conf.json'
 dir_with_this_file = pathlib.Path(__file__).resolve().parent
-all_possible_keys_file_path = dir_with_this_file / 'simple_modifications.json'
+buttons_list_file_path = dir_with_this_file / 'simple_modifications.json'
 
-Category = typing.Literal[
-    'Letter keys', 'Generic desktop keys', 'Mouse keys',
-    'Arrow keys', 'Mouse buttons', 'Media controls',
-    'International keys', 'Generic GUI application control keys',
-    'Japanese', 'Others', 'Keys in pc keyboards',
-    'Controls and symbols', 'Software function',
-    'D-pad', 'Function keys', 'Modifier keys',
-    'Sticky modifier keys', 'Keypad keys',
-    'Application launch keys', 'Remote control buttons',
-    'Disable this key', 'Number keys'
-]
-Modifiers = typing.Literal[
-    'caps_lock', 'left_control',
-    'left_shift', 'left_option',
-    'left_command', 'right_control',
-    'right_shift', 'right_option',
-    'right_command', 'fn'
-]
-Layouts = typing.Literal['en', 'ru']
-ButtonTypes = typing.Literal['key_code', 'consumer_key_code', 'pointing_button', 'generic_desktop', 'apple_vendor_top_case_key_code']
-PossibleFingerCountValues = typing.Literal[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-AllButtonsButtonDescriptionData = typing.TypedDict('AllButtonsButtonDescriptionData', {'key_code': str})
-AllButtonsButtonDescription = typing.TypedDict(
-    'AllButtonsButtonDescription',
-    {'label': str, 'category': Category, 'not_from': bool, 'unsafe_from': bool, 'not_to': bool, 'data': list[AllButtonsButtonDescriptionData]}
-)
+# @dataclasses.dataclass
+# class ButtonDescriptionData:
+#     key_code: str
 
-@dataclasses.dataclass
-class AllButtonsButtonDescriptionData:
-    key_code: str
+# @dataclasses.dataclass
+# class ButtonDescription:
+#     label: str
+#     category: Category
+#     not_from: bool
+#     unsafe_from: bool
+#     not_to: bool
+#     pair: ButtonPair
+
+button_list_bin : bytes | None = None
+try:
+    buttons_list_bin_data = urlopen('https://raw.githubusercontent.com/pqrs-org/Karabiner-Elements/refs/heads/main/src/apps/SettingsWindow/Resources/simple_modifications.json').read()
+    json.loads(buttons_list_bin_data)
+except Exception as e:
+    print(f'failed to load keys. {type(e).__name__}:{e}')
+    with buttons_list_file_path.open('rb') as file:
+        buttons_list_bin_data = file.read()
+
+assert buttons_list_bin_data is not None
+buttons_list_json_data = json.loads(buttons_list_bin_data)
+
+with buttons_list_file_path.open('wb') as file:
+    file.write(buttons_list_bin_data)
+
+
+true_or_missing_keys : Final = ('not_from', 'not_to', 'unsafe_from')
+
+@dataclass
+class Button:
+    ...
+#     label: str
+#     category: 
+
+buttons: list[Button] = []
+
+assert isinstance(buttons_list_json_data, list)
+
+current_category : str|None = None
+
+for button_json in buttons_list_json_data:
+    assert isinstance(button_json, dict)
+    if 'category' in button_json.keys():
+        assert set(button_json.keys()) == {'category'}
+        category = button_json['category']
+        assert isinstance(category, str)
+        current_category = category
+    else:
+        category = current_category
+        assert category is not None
+        true_or_missing_keys : Final = {'not_from', 'not_to', 'unsafe_from'}
+        must_present_keys : Final = {'label', 'data'}
+        assert must_present_keys <= button_json.keys() <= must_present_keys | true_or_missing_keys
+        
+
+        label = button_json.pop('label')
+        assert isinstance(label, str)
+        not_from = button_json.pop('not_from', False)
+        assert
+
+
+
+
 
 @call
 def all_buttons() -> list[AllButtonsButtonDescription]:
     @call
-    def data() -> tuple[typing.Any, bytes]:
+    def data() -> tuple[Any, bytes]:
         try:
             bin_data = urlopen('https://raw.githubusercontent.com/pqrs-org/Karabiner-Elements/refs/heads/main/src/apps/SettingsWindow/Resources/simple_modifications.json').read()
             return json.loads(bin_data), bin_data
@@ -74,7 +143,7 @@ def all_buttons() -> list[AllButtonsButtonDescription]:
     with all_possible_keys_file_path.open('wb') as file:
         file.write(bin_data)
 
-    all_buttons : list[AllButtonsButtonDescription] = []
+    all_buttons : list[ButtonDescription] = []
     category = j_data[0]
     assert 'category' in category
     for button in j_data:
@@ -83,7 +152,7 @@ def all_buttons() -> list[AllButtonsButtonDescription]:
         else:
             all_buttons.append(category | button)
 
-    true_or_missing_keys : typing.Final = ('not_from', 'not_to', 'unsafe_from')
+    true_or_missing_keys : Final = ('not_from', 'not_to', 'unsafe_from')
 
     for true_or_missing_key in true_or_missing_keys:
         assert all([
@@ -123,7 +192,7 @@ def all_buttons() -> list[AllButtonsButtonDescription]:
     return all_buttons
 
 category_by_button_pair : dict[ButtonPair, Category] = {
-    typing.cast(
+    cast(
         tuple[str, str],
         tuple([
             *button['data'][0].keys()
@@ -134,7 +203,7 @@ category_by_button_pair : dict[ButtonPair, Category] = {
     for button in all_buttons if not button['not_from']
 }
 
-ButtonPair = typing.Annotated[
+ButtonPair = Annotated[
     tuple[str, str],
     list(category_by_button_pair)
 ]
@@ -161,7 +230,7 @@ def all_values_impl(x: typing.Any) -> typing.Any:
         return x.__metadata__[0]
     print(repr(x))
     assert False
-def all_values(x: typing.Any) -> tuple[typing.Any]:
+def all_values(x: typing.Any) -> tuple[typing.Any, ...]:
 
     return tuple([*all_values_impl(x)])
 
@@ -188,7 +257,7 @@ class product_with_len:
         self.generator = product(*args)
     def __iter__(self) -> product_with_len:
         return self
-    def __next__(self) -> tuple[typing.Any, ...]:
+    def __next__(self) -> typing.Any:
         return self.generator.__next__()
     def __len__(self) -> int:
         return self.len
@@ -208,7 +277,8 @@ def identity(arg: T) -> T:
 
 tqdm : typing.Callable[[typing.Any], typing.Any] = identity
 try:
-    from tqdm import tqdm
+    from tqdm import tqdm as _tqdm
+    tqdm = _tqdm
 except Exception as e:
     print(f'failed to import tqdm. {type(e).__name__}:{e}')
 
