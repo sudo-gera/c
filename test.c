d7cc5bc9e7b3edb077dba8a48afc4b40bb0bab27
sudo-gera <gera.tatarinov.999@gmail.com>
2025-09-28 05:02:30 +0300

    *interesting commit message*

--- test.c
+++ test.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
-#include <stdint.h>
 
-void save_bmp32(const char*fn,unsigned char*img,int w,int h){
-    int fs=54+4*w*h,z=0,o=54,d=40;short p=1,bpp=32;
+void save_bmp32(const char *fn, void *img, int w, int h){
+    int r[52]={54+4*w*h,0,54,40,w,h,0x200001};
     FILE*f=fopen(fn,"wb");
-    fwrite("BM",1,2,f);fwrite(&fs,4,1,f);fwrite(&z,4,1,f);
-    fwrite(&o,4,1,f);fwrite(&d,4,1,f);fwrite(&w,4,1,f);fwrite(&h,4,1,f);
-    fwrite(&p,2,1,f);fwrite(&bpp,2,1,f);fwrite(&z,4,1,f);
-    for(int y=h-1;y>=0;y--)for(int x=0;x<w;x++){
-        unsigned char*p=img+(y*w+x)*3,b[4]={p[2],p[1],p[0],255};
-        fwrite(b,4,1,f);
-    }
+    fwrite("BM",1,2,f);
+    fwrite(r,52,1,f);
+    fwrite(img, w*h*4, 1, f);
     fclose(f);
 }
+
 int main() {
-    int w=64,h=64;
-    unsigned char img[w*h*3];
-    for(int y=0;y<h;y++)for(int x=0;x<w;x++){
-        img[(y*w+x)*3+0]=x*4;     // R
-        img[(y*w+x)*3+1]=y*4;     // G
-        img[(y*w+x)*3+2]=128;     // B
+    int w = 64, h = 64;
+    unsigned char img[w * h * 4];
+
+    for (int y = 0; y < h; y++) { // bottom to top
+        for (int x = 0; x < w; x++) { // left to right
+            img[(y*w+x)*4+0] = x*4; // B
+            img[(y*w+x)*4+1] = 0;   // G
+            img[(y*w+x)*4+2] = y*4; // R
+            img[(y*w+x)*4+3] = 255; // A
+        }
     }
-    save_bmp32("out.bmp",img,w,h);
+
+    save_bmp32("out.bmp", img, w, h);
 }
