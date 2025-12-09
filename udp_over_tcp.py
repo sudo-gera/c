b46a7ae3d904499dbad2fe03e2ab2204a960d8d7
sudo-gera <gera.tatarinov.999@gmail.com>
2025-12-09 06:57:04 +0300

    *interesting commit message*

--- udp_over_tcp.py
+++ udp_over_tcp.py
@@ -732,12 +732,6 @@ async def server_main(args: server_args) -> None:
 
                 async def recv_while_can() -> None:
                     while 1:
-                        # msg_batch = await read_large_chunk_from_stream_reader(reader, datagram_batch)
-
-                        # for msg in msg_batch.items:
-                        #     udp_client = await get_udp_client(msg)
-                        #     await udp_client.send_datagram(msg)
-
                         msg = await read_large_chunk_from_stream_reader(reader, datagram)
 
                         udp_client = await get_udp_client(msg)
@@ -745,8 +739,6 @@ async def server_main(args: server_args) -> None:
 
                 async def send_while_can() -> None:
                     while 1:
-                        # msg_list = await get_all(context.server_to_client_queue)
-                        # await write_large_chunk_to_stream_writer(writer, datagram_batch(tuple(msg_list)))
                         msg = await context.server_to_client_queue.get()
                         await write_large_chunk_to_stream_writer(writer, msg)
                 
@@ -879,18 +871,12 @@ async def client_main(args: client_args) -> None:
 
         async def recv_while_can() -> None:
             while 1:
-                # msg_batch = await read_large_chunk_from_stream_reader(reader, datagram_batch)
-
-                # for msg in msg_batch.items:
-                #     await protocol.send_datagram(msg)
                 msg = await read_large_chunk_from_stream_reader(reader, datagram)
 
                 await protocol.send_datagram(msg)
         
         async def send_while_can() -> None:
             while 1:
-                # msg_list = await get_all(udp_to_tcp_queue)
-                # await write_large_chunk_to_stream_writer(writer, datagram_batch(tuple(msg_list)))
                 msg = await udp_to_tcp_queue.get()
                 await write_large_chunk_to_stream_writer(writer, msg)
 
