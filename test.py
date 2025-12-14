79d82187d97077224d70014f2f8febc3ae84bd66
sudo-gera <gera.tatarinov.999@gmail.com>
2025-12-14 15:38:59 +0300

    *interesting commit message*

--- test.py
+++ test.py
@@ -1,20 +1,50 @@
+from itertools import *
+import random
+import math
 
-file = open('test.ppm', 'w')
+def save_as_bmp(width: int, height: int, pixels: dict[tuple[int, int], tuple[int, int, int]], filepath: str) -> None:
 
-print('P6', file=file)
+    with open(filepath, 'wb') as file:
+        file.write(b'BM')
+        header_ints = [
+            54 + 4 * width * height,
+            0, 54, 40,
+            width, height,
+            0x2_00_00_1
+        ]
+        header_ints += [0] * ( 13 - len(header_ints) )
+        header_bytes = b''.join([
+            c.to_bytes(4, 'little')
+            for c in header_ints
+        ])
+        file.write(header_bytes)
+        for y in range(height):
+            for x in range(width):
+                pixel = pixels[x, y][::-1] + (255,)
+                for c in pixel:
+                    file.write((c % 256).to_bytes(1, 'little'))
 
-width = 200
 
-height = 200
+def main() -> None:
+    width = 1680
 
-print(width, height, file=file)
+    height = 1050
 
-print(255, file=file)
+    display : dict[tuple[int, int], tuple[int, int, int]] = {}
 
-file.flush()
+    for x in range(width):
+        for y in range(height):
+            display[x, y] = (255, 255, 255)
 
-for i in range(height):
-    for j in range(width):
-        for a in [round(i / height * 255), round(j / height * 255), 0]:
-            file.buffer.write(a.to_bytes(1, 'big'))
+    for q in range(40):
+        col = random.choice(range(width))
+        value = random.choice(range(256))
+        for x in range(width):
+            for y in range(height):
+                if abs(x - col) < value:
+                    display[x, y] -= abs(x - col)
 
+
+    save_as_bmp(width, height, display, 'out.bmp')
+
+main()
