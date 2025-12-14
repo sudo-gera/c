cf306f4184a59f9497e18087032e7b57872ac896
sudo-gera <gera.tatarinov.999@gmail.com>
2025-12-14 23:17:08 +0300

    *interesting commit message*

--- test.c
+++ test.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
 
-struct pixel{
+typedef struct pixel {
     char blue, green, red, opacity;
-};
+} pixel;
 
-void save_as_bmp(const char* filepath, int width, int height, struct pixel* pixels){
-    int sizeof_pixels = width * height * sizeof(struct pixel);
-    int header[13] = {
-        sizeof_pixels + 54,
-        0, 54, 40,
-        width, height,
-        0x200001,
-    };
-    FILE* fp = fopen(filepath, "wb");
-    fwrite( "BM",   2,              1, fp );
-    fwrite( header, sizeof(header), 1, fp );
-    fwrite( pixels, sizeof_pixels,  1, fp );
-    fclose( fp );
+void save_as_bmp(const char* path, int cols, int rows, const pixel* pixels) {
+    FILE* fp = fopen(path, "wb");
+    if (!fp) {
+        perror(path);
+        return;
+    }
+    size_t size = cols * rows * sizeof(pixel);
+    int head[13] = {size, 0, 54, 40, cols, rows, 0x200001, 0, 0, 0, 0, 0, 0};
+    fwrite("BM", 2, 1, fp);
+    fwrite(head, sizeof(head), 1, fp);
+    fwrite(pixels, size, 1, fp);
+    fclose(fp);
 }
 
 int main() {
     int width = 120, height = 80;
     struct pixel img[height * width];
-    for (int y = 0; y < height; y++) { // bottom to top
-        for (int x = 0; x < width; x++) { // left to right
-            img[y * width + x].blue  = y * 255 / height;
+    for (int y = 0; y < height; y++) {     // bottom to top
+        for (int x = 0; x < width; x++) {  // left to right
+            img[y * width + x].blue = y * 255 / height;
             img[y * width + x].green = x * 255 / width;
-            img[y * width + x].red   = 255 - y * 255 / height;
+            img[y * width + x].red = 255 - y * 255 / height;
             img[y * width + x].opacity = -1;
         }
     }
--- test.py
+++ test.py
@@ -3,6 +3,60 @@ import random
 import math
 import io
 
+import numpy as np
+
+def bmp_to_numpy(filepath: str, data: np.ndarray):
+    assert len(data.shape) == 3 and data.shape[2] == 4
+
+    height: int
+    width: int
+    height, width, _ = data.shape
+
+    header = np.zeros(shape=(26,), dtype=np.uint16)
+    header[6] = 40
+    header[8] = width
+    header[10] = height
+    header[13] = 32
+
+
+
+    # header = np.zeros(shape=(13*4,), dtype=np.uint8)
+
+    # header[12] = 40
+    # header[16:24] = list(width.to_bytes(4, 'little') + height.to_bytes(4, 'little'))
+    # header[26] = 32
+
+    with open(filepath, 'wb') as file_:
+        file_.write(b'BM')
+        file_.write(header.tobytes())
+        file_.write(data.tobytes())
+
+
+    # b'BM' + np.
+
+    # file = io.BytesIO()
+    # file.write(b'BM')
+    # header_ints = np.array([
+    #     54 + 4 * width * height,
+    #     0, 54, 40,
+    #     width, height,
+    #     0x2_00_00_1
+    # ], dtype=np.uint32)
+    # header_ints += [0] * ( 13 - len(header_ints) )
+    # header_bytes = b''.join([
+    #     c.to_bytes(4, 'little')
+    #     for c in header_ints
+    # ])
+    # file.write(header_bytes)
+
+    # file.write(data.tobytes())
+
+    # file.seek(0)
+
+    # with open(filepath, 'wb') as file_:
+    #     file_.write(file.read())
+
+
 def save_as_bmp(width: int, height: int, pixels: dict[tuple[int, int], list[int]], filepath: str) -> None:
 
 
@@ -42,23 +96,28 @@ def main() -> None:
     width = 1680
 
     height = 1050
+    # height = width
 
-    display : dict[tuple[int, int], list[int]] = {}
+    # display : dict[tuple[int, int], list[int]] = {}
 
-    for x in range(width):
-        for y in range(height):
-            display[x, y] = [255, 255, 255]
+    display = np.ndarray(shape=(height, width, 4), dtype=np.uint8)
 
-    for q in range(64):
-        col = random.choice(range(width))
-        value = random.choice(range(1, 256))
+    for y in range(height):
         for x in range(width):
-            for y in range(height):
-                for c in range(3):
-                    display[x, y][c] -= round(norm((x - col)/3) * value)
+            display[y, x] = [round(255 * x / width), round(255 * y / height), 255, 255]
+            # display[y, x, :] = [x%256, 255, 255, 255]
+
+    # for q in range(64):
+    #     col = random.choice(range(width))
+    #     value = random.choice(range(1, 256))
+    #     for x in range(width):
+    #         for y in range(height):
+    #             for c in range(3):
+                    # display[x, y][c] -= round(norm((x - col)/3) * value)
                         
 
+    bmp_to_numpy('out.bmp', display)
 
-    save_as_bmp(width, height, display, 'out.bmp')
+    # save_as_bmp(width, height, display, 'out.bmp')
 
 main()
