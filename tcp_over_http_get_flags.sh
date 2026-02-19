596db685abfd98a12cccc1e55528d340e7c95993
sudo-gera <gera.tatarinov.999@gmail.com>
2026-02-19 12:04:03 +0300

    *interesting commit message*

---
+++ tcp_over_http_get_flags.sh
@@ -0,0 +1,20 @@
+# #!/usr/bin/env bash
+
+# set -xeuo pipefail
+
+# server_url="$1"
+
+# if [ "$#" -eq 1 ]
+# then
+#     :
+# elif [ "$2" = --proxy-command ]
+# then
+#     :
+# elif [ "$2" = --host-key-alias ]
+# then
+#     :
+# elif [ "$2" = -- ]
+# then
+#     :
+
+
--- tcp_over_http_proxy_command.sh
+++ tcp_over_http_proxy_command.sh
@@ -2,6 +2,25 @@
 
 set -xeuo pipefail
 
-server_url="$1"
+encoded_server_host="$1"
 
-ssh 'tee | ( port=$(( $RANDOM %% 20000 + 20000 )) ; python3 ~/c/tcp_over_http_client.py --http-url '"'"'%h'"'"' --tcp-host 127.0.0.1 --tcp-port $port & p=$! ; sleep 1 ; trap "kill $p" EXIT ; nc 127.0.0.1 $port ) | tee'
+mkdir ~/.ssh
+
+json="$(
+    printf '%s\n' "${encoded_server_host}" | tr '.' '\n' | head -n 1 | base64 -d | base64 -d
+)"
+
+printf '%s\n' "${json}" | jq .line_for_known_hosts | sed "s?_tcp_over_http_host_placeholder_?${encoded_server_host}?g" >> ~/.ssh/known_hosts
+
+url="$(printf '%s\n' "${json}" | jq .url)"
+
+chmod 600 ~/.ssh/known_hosts
+
+port=$(( $RANDOM % 20000 + 20000 ))
+
+python3 ~/c/tcp_over_http_client.py --http-url "${url}" --tcp-host 127.0.0.1 --tcp-port "${port}" &
+p=$!
+
+trap "kill $p" EXIT
+
+nc 127.0.0.1 "${port}"
