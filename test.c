a1859aec61e06cebe23fe7200c9bfe114166a8a7
sudo-gera <gera.tatarinov.999@gmail.com>
2026-03-04 18:52:53 +0300

    *interesting commit message*

--- test.c
+++ test.c
@@ -1,34 +1,27 @@
 #include <stdio.h>
 
-typedef struct pixel {
+struct pixel{
     char blue, green, red, opacity;
-} pixel;
+};
 
-void save_as_bmp(const char* path, int cols, int rows, const pixel* pixels) {
-    FILE* fp = fopen(path, "wb");
-    if (!fp) {
-        perror(path);
+void save_as_bmp(const char* filepath, const struct pixel* data, int cols, int rows){
+    FILE* fp = fopen(filepath, "wb");
+    if (!fp){
+        perror(filepath);
         return;
     }
-    int size = cols * rows * sizeof(pixel) + 54;
-    int head[13] = {size, 0, 54, 40, cols, rows, 0x200001, 0, 0, 0, 0, 0, 0};
     fwrite("BM", 2, 1, fp);
-    fwrite(head, sizeof(head), 1, fp);
-    fwrite(pixels, size - 54, 1, fp);
+    int size = sizeof(struct pixel) * cols * rows;
+    int header[13] = {
+        size + 54,
+        0, 54, 40,
+        cols,
+        rows,
+        0x200001,
+    };
+    fwrite(header, sizeof(header), 1, fp);
+    fwrite(data, size, 1, fp);
     fclose(fp);
 }
 
-int main() {
-    int width = 1, height = 1;
-    struct pixel img[height * width];
-    for (int y = 0; y < height; y++) {     // bottom to top
-        for (int x = 0; x < width; x++) {  // left to right
-            img[y * width + x].blue = y * 255 / height;
-            img[y * width + x].green = x * 255 / width;
-            img[y * width + x].red = 255 - y * 255 / height;
-            img[y * width + x].opacity = -1;
-        }
-    }
 
-    save_as_bmp("out.bmp", width, height, img);
-}
