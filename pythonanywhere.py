21daf0384446284f591e3a22139e685da8fc3665
sudo-gera <gera.tatarinov.999@gmail.com>
2026-06-22 22:01:48 +0300

    *interesting commit message*

--- pythonanywhere.py
+++ pythonanywhere.py
@@ -1,35 +1,56 @@
 import asyncio
-import websockets
-import websockets.asyncio
-from websockets.asyncio.client import connect
+from websockets.asyncio.client import connect, ClientConnection
 
+recv_queue: asyncio.Queue[str | bytes] = asyncio.Queue(maxsize=64)
 
-async def open_pythonanywhere_websocket():
-    return await connect(
-        "wss://consoles-12.pythonanywhere.com/sj/887/f3x5zvu_/websocket",
+async def recv_into_queue(ws: ClientConnection) -> None:
+    while 1:
+        chunk = await ws.recv()
+        await recv_queue.put(chunk)
+
+async def open_pythonanywhere_websocket(ws_url: str, first_ws_message: str) -> ClientConnection:
+    ws = await connect(
+        # "wss://consoles-12.pythonanywhere.com/sj/887/f3x5zvu_/websocket",
+        ws_url,
         origin="https://www.pythonanywhere.com",
         additional_headers={
             "Sec-GPC": "1",
             "Cache-Control": "no-cache",
-            "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+            "Accept-Language": "en-US,en;q=0.9",
             "Pragma": "no-cache",
             "User-Agent": (
                 "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
-                "AppleWebKit/537.36 (KHTML, like Gecko) "
                 "Chrome/147.0.0.0 Safari/537.36"
+                "AppleWebKit/537.36 (KHTML, like Gecko) "
             ),
         },
     )
-
-
+    await ws.send(first_ws_message)
+    
 async def main():
     ws = await open_pythonanywhere_websocket()
 
-    await ws.send("hello")
-    response = await ws.recv()
-    print(response)
+    print(await ws.recv())
+
+    await ws.send('["\\u001b[asv0x1m98qlf4fd6q9xzafcdorhv62ry;45454587;;a"]')
+
+    t = asyncio.create_task(recver(ws))
+
+    await asyncio.sleep(8)
+
+    await ws.send('["pwd\\ns"]')
+
+    await asyncio.Future()
 
     await ws.close()
+
+
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--ws-url')
+parser.add_argument('--first-ws-message')
 
 
 asyncio.run(main())
