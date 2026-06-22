a159ac134da820a8ef04877d84eb0ea2f5db9850
sudo-gera <gera.tatarinov.999@gmail.com>
2026-06-22 12:47:20 +0300

    *interesting commit message*

--- servix/reverse_tcp_forwarding.sh
+++ servix/reverse_tcp_forwarding.sh
@@ -1,4 +1,4 @@
 r=$RANDOM
 echo ___ > ~/.reverse_tcp_forwarding_first_4_bytes_$r
 mv ~/.reverse_tcp_forwarding_first_4_bytes_$r ~/.reverse_tcp_forwarding_first_4_bytes
-send '(set -x ; eval "$(tmux display-message -p "#{session_name}" | tr "_-" ".\n")" ; while sleep 1 ; do date ; ~/reverse_tcp_forwarding --mode $mode --connection_timeout 300 --max_accepted_sockets_per_server 200 --max_connected_sockets 200 --first_bytes_filename ~/.reverse_tcp_forwarding_first_4_bytes --internal_port $internal_port --internal_host $internal_host --external_port $external_port --external_host $external_host --clock_interval 1 ; done )'
+send '(set -x ; eval "$(tmux display-message -p "#{session_name}" | tr "^-" ".\n")" ; while sleep 1 ; do date ; ~/reverse_tcp_forwarding --mode $mode --connection_timeout 300 --max_accepted_sockets_per_server 200 --max_connected_sockets 200 --first_bytes_filename ~/.reverse_tcp_forwarding_first_4_bytes --internal_port $internal_port --internal_host $internal_host --external_port $external_port --external_host $external_host --clock_interval 1 ; done )'
