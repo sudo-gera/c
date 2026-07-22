bc6125a63204ffa85099d281a0e9bf084c9adf34
sudo-gera <gera.tatarinov.999@gmail.com>
2026-07-23 00:47:03 +0300

    *interesting commit message*

--- fmt.c
+++ fmt.c
@@ -66,61 +66,95 @@
 
 #define fmt_arg_to_printf_fstr_args(x) fmt_arg_to_printf_fstr(x) fmt_arg_to_printf_args(x)
 
-#define fmt_arg_into_snprintf_for_tmpbuf(x)                                    \
-    _fmt_last = snprintf(_fmt_pos, _fmt_left, fmt_arg_to_printf_fstr_args(x)); \
-    if (_fmt_last < 0) {                                                       \
-        fputs("fmt format error\n", stderr);                                   \
-        tmpbuf[0] = 0;                                                         \
-        break;                                                                 \
-    }                                                                          \
-    if (_fmt_last >= _fmt_left) {                                              \
-        fputs("fmt overflow\n", stderr);                                       \
-        tmpbuf[0] = 0;                                                         \
-        break;                                                                 \
-    }                                                                          \
-    _fmt_pos += _fmt_last;                                                     \
+///////////////////////////////////////////////////////////////////////////////////////
+
+#define _fmt_arg_into_snprintf(x)                                                      \
+    _fmt_last = snprintf(_fmt_pos, (size_t)_fmt_left, fmt_arg_to_printf_fstr_args(x)); \
+    if (_fmt_last < 0) {                                                               \
+        fputs("fmt format error\n", stderr);                                           \
+        _fmt_used = _fmt_last;                                                         \
+        break;                                                                         \
+    }                                                                                  \
+    if (_fmt_last >= _fmt_left) {                                                      \
+        fputs("fmt overflow\n", stderr);                                               \
+        _fmt_used = -1;                                                                \
+        break;                                                                         \
+    }                                                                                  \
+    _fmt_pos += _fmt_last;                                                             \
+    _fmt_used += _fmt_last;                                                            \
     _fmt_left -= _fmt_last;
 
-#define fmt_arg_into_fprintf(x)                                     \
+#define _fmt_arg_into_fprintf(x)                                    \
     _fmt_last = fprintf(_fmt_file, fmt_arg_to_printf_fstr_args(x)); \
     if (_fmt_last < 0) {                                            \
         fputs("fmt format error\n", stderr);                        \
-        _fmt_used = -1;                                             \
+        _fmt_used = _fmt_last;                                      \
         break;                                                      \
     }                                                               \
     _fmt_used += _fmt_last;
 
-#define fmt(...)                                                 \
-    tmpbuf(({                                                    \
-        char* _fmt_pos = tmpbuf;                                 \
-        int _fmt_left = sizeof(tmpbuf);                          \
-        int _fmt_last = 0;                                       \
-        macro_map(fmt_arg_into_snprintf_for_tmpbuf, __VA_ARGS__) \
-    }))
+///////////////////////////////////////////////////////////////////////////////////////
 
-#define fmtf(file, ...)                                   \
-    ({                                                    \
-        FILE* _fmt_file = file;                           \
-        int _fmt_last = 0;                                \
-        int _fmt_used = 0;                                \
-        do {                                              \
-            macro_map(fmt_arg_into_fprintf, __VA_ARGS__); \
-        } while (0);                                      \
-        _fmt_used;                                        \
+#define snfmt(buffer, size, ...)                                         \
+    ({                                                                   \
+        char* _fmt_buf = (buffer);                                       \
+        char* _fmt_pos = _fmt_buf;                                       \
+        int _fmt_left = (int)(size);                                     \
+        int _fmt_last = 0;                                               \
+        int _fmt_used = 0;                                               \
+        do {                                                             \
+            macro_map(_fmt_arg_into_snprintf __VA_OPT__(, ) __VA_ARGS__) \
+        } while (0);                                                     \
+        _fmt_used;                                                       \
     })
 
-#define outfmt(...) fmtf(stdout, __VA_ARGS__)
-#define errfmt(...) fmtf(stderr, __VA_ARGS__)
+#define ffmt(file, ...)                                                  \
+    ({                                                                   \
+        FILE* _fmt_file = file;                                          \
+        int _fmt_last = 0;                                               \
+        int _fmt_used = 0;                                               \
+        do {                                                             \
+            macro_map(_fmt_arg_into_fprintf __VA_OPT__(, ) __VA_ARGS__); \
+        } while (0);                                                     \
+        _fmt_used;                                                       \
+    })
+
+#define fmt(...) tmpbuf(snfmt(tmpbuf, sizeof(tmpbuf) __VA_OPT__(, ) __VA_ARGS__);)
+#define outfmt(...) ffmt(stdout __VA_OPT__(, ) __VA_ARGS__)
+#define errfmt(...) ffmt(stderr __VA_OPT__(, ) __VA_ARGS__)
 #define outfmtln(...) outfmt(__VA_ARGS__ __VA_OPT__(, ) "\n")
 #define errfmtln(...) errfmt(__VA_ARGS__ __VA_OPT__(, ) "\n")
 
-#define M "-"
+///////////////////////////////////////////////////////////////////////////////////////
 
+#define M "="
 int main() {
     size_t val1 = -1;
     ssize_t val2 = -1;
     uint64_t val3 = 1;
-    outfmtln("val1-1 = ", (--val1) "%zu; val2-1 = ", (--val2) "%zd; val3-1 = ", (--val3) "%" PRIu64 ";");
-    outfmtln("val1-2 = ", (--val1) "%zu; val2-2 = ", (--val2) "%zd; val3-2 = ", (--val3) "%" PRIu64 ";");
-    outfmtln("val1-3 = ", (--val1) "%zu; val2-3 = ", (--val2) "%zd; val3-3 = ", (--val3) "%" PRIu64 ";");
+    outfmtln("val1-1 = ", (--val1) "%zu; val2-1 = ", (--val2) "%zd; val3-1 = ", (8, --val3) "%0*" PRIu64 ";");
+    outfmtln("val1-2 ", (M) "%s ", (--val1) "%zu; val2-2 = ", (--val2) "%zd; val3-2 = ", (--val3) "%" PRIu64 ";");
+    puts(fmt("val1-3 = ", (--val1) "%zu; val2-3 = ", (--val2) "%zd; val3-3 = ", (--val3) "%" PRIu64 ";"));
+    char* buffers[16];
+    buffers[0] = fmt((65536, 0) "%0*d");
+    buffers[1] = fmt((65536, 1) "%0*d");
+    buffers[2] = fmt((65536, 2) "%0*d");
+    buffers[3] = fmt((65536, 3) "%0*d");
+    buffers[4] = fmt((65536, 4) "%0*d");
+    buffers[5] = fmt((65536, 5) "%0*d");
+    buffers[6] = fmt((65536, 6) "%0*d");
+    buffers[7] = fmt((65536, 7) "%0*d");
+    buffers[8] = fmt((65536, 8) "%0*d");
+    buffers[9] = fmt((65536, 9) "%0*d");
+    buffers[10] = fmt((65536, 10) "%0*d");
+    buffers[11] = fmt((65536, 11) "%0*d");
+    buffers[12] = fmt((65536, 12) "%0*d");
+    buffers[13] = fmt((65536, 13) "%0*d");
+    buffers[14] = fmt((65536, 14) "%0*d");
+    buffers[15] = fmt((65536, 15) "%0*d");
+    for (size_t i = 0; i < sizeof(buffers) / sizeof(buffers[0]); ++i) {
+        if (strcmp(buffers[i], fmt((65536, i) "%0*zu"))) {
+            outfmtln("Error: buffers[", (i) "%zu] is lost.");
+        }
+    }
 }
